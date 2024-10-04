bbc058459161c9937d7e7ea84fac54e629aadbae
Jonatas Fiorentin <jonatasfiorentin@gmail.com>
2024-10-04 19:50:55 -0300

    feat(school-matrices): adding zod validation

--- hosting/src/routes/school-matrices.tsx
+++ hosting/src/routes/school-matrices.tsx
@@ -2,28 +2,45 @@ import { firestore } from '@/services/firebase'
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import { collection, getDocs } from 'firebase/firestore'
+import { z } from 'zod'
+
+const matrixSchema = z.object({
+  name: z.string(),
+  numberOfClasses: z.number(),
+  workload: z.number(),
+})
+
+const matricesSchema = z.array(matrixSchema)
 
 export const Route = createFileRoute('/school-matrices')({
   component: SchoolMatrices,
 })
 
 function SchoolMatrices() {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, error } = useQuery({
     queryKey: ['getMatrices'],
-    queryFn: () => {
+    queryFn: async () => {
       const matricesRef = collection(firestore, 'schoolMatrices')
-      console.log(matricesRef)
+      const snapshot = await getDocs(matricesRef)
+      const matricesData = snapshot.docs.map((doc) => doc.data())
 
-      return getDocs(matricesRef).then(({ docs }) => docs.map((doc) => doc.data()))
+      const parsedData = matricesSchema.safeParse(matricesData)
+
+      if (!parsedData.success) {
+        throw new Error('Invalid data structure')
+      }
+
+      return parsedData.data
     },
   })
 
   if (isLoading) return <h1>Carregando...</h1>
+  if (error) return <h1>Erro ao carregar os dados.</h1>
 
   return (
     <div>
       {data?.map(({ name, numberOfClasses, workload }) => (
-        <div>
+        <div key={name}>
           <p>
             <strong>Nome:</strong> {name}
           </p>
