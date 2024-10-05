5ccdd6fbada3d761cca670c839df52e3d2d30dc8
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-10-05 01:27:15 -0300

    feat(input-form.tsx): creating a custom input for used in interface

--- hosting/src/routes/password-changes.tsx
+++ hosting/src/routes/password-changes.tsx
@@ -3,17 +3,18 @@ import { useQuery } from '@tanstack/react-query'
 import { collection, DocumentReference, getDocs } from 'firebase/firestore'
 import { firestore } from '@/services/firebase'
 import { z } from 'zod'
-import { Field, Form, Formik } from 'formik'
+import { Form, Formik } from 'formik'
 import { toFormikValidationSchema } from 'zod-formik-adapter'
+import { InputForm } from '@/components/custom/input-form'
 
 export const Route = createFileRoute('/password-changes')({
   component: ChangedPasswordRequests,
 })
 
 const changePasswordRequestSchema = z.object({
-  newPasswordDefault: z.string(),
-  requestStatus: z.string(),
-  studentName: z.instanceof(DocumentReference),
+  newPasswordDefault: z.string().min(6, { message: 'A string deve conter no minimo 6 caracteres.' }),
+  requestStatus: z.enum(['accepted', 'refused', 'pending']),
+  studentName: z.instanceof(DocumentReference), // ver sobre
 })
 
 type ChangeRequests = z.infer<typeof changePasswordRequestSchema>
@@ -60,7 +61,21 @@ export function ChangedPasswordRequests() {
         validationSchema={toFormikValidationSchema(changePasswordRequestSchema)}
       >
         <Form>
-          <Field></Field>
+          <InputForm
+            title="Senha padrão"
+            id="newPasswordDefault"
+            name="newPasswordDefault"
+            placeholder="Digite a senha padrão aqui"
+            label="newPasswordDefault"
+          />
+
+          <InputForm
+            title="Status da requisição"
+            id="requestStatus"
+            name="requestStatus"
+            placeholder="Digite o status da requisição de troca de senha"
+            label="requestStatus"
+          />
         </Form>
       </Formik>
     </>
