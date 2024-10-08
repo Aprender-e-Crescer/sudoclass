e7a0428ebb94f12322c31a86f5ea26c1d1da2df7
yuossefpolidorio@gmail.com <joaopedromoraesbonkoski@gmail.com>
2024-10-07 22:28:20 -0300

    feat(activities): created the ActivitySchema and useSearchActivitiesQuery functions

--- hosting/src/routes/__root.tsx
+++ hosting/src/routes/__root.tsx
@@ -1,21 +1,13 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-
+import {Activities} from '@/routes/activities'
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
-      <TanStackRouterDevtools />
+      <Activities />
+      {/* <Outlet />
+      <TanStackRouterDevtools /> */}
     </>
   ),
 })
