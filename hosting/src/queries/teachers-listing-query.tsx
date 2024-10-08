c28bfd0f1aff013768d34354a6869129b30d5af9
augustofav <augustofavetti159@gmail.com>
2024-10-08 17:09:31 -0300

    feat(teachers listing): add query and models

--- hosting/src/queries/teachers-listing-query.tsx
+++ hosting/src/queries/teachers-listing-query.tsx
@@ -1,4 +1,4 @@
-import { registerSchema } from "@/models/register-schema"; } 
+import { RegisterRequests, registerSchema } from "@/models/register-schema"; 
 import { firestore } from "@/services/firebase";
 import { useQuery } from "@tanstack/react-query";
 import { collection, getDocs } from "firebase/firestore";
@@ -8,7 +8,7 @@ export function registerSchemaQuery(){
         queryKey: ['teachers'],
         queryFn: async () => {
             const studentsRef = collection(firestore, 'teachers').withConverter({
-                toFirestore: (doc: Teachers) => doc,
+                toFirestore: (doc: RegisterRequests) => doc,
                 fromFirestore: (snapshot) => registerSchema.parse({...snapshot.data(), id: snapshot.id}),
             })
 
@@ -19,4 +19,4 @@ export function registerSchemaQuery(){
     })
 }
 
-}
+
