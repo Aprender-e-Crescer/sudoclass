ce48a4e47741c7a87e589959a7e8b9ce86c76444
yuossefpolidorio@gmail.com <joaopedromoraesbonkoski@gmail.com>
2024-10-08 21:02:17 -0300

    fix(root componet and package): fixing pr

--- hosting/src/routes/__root.tsx
+++ hosting/src/routes/__root.tsx
@@ -1,13 +1,21 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import {Activities} from '@/routes/activities'
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Activities />
-      {/* <Outlet />
-      <TanStackRouterDevtools /> */}
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>
+        {' '}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
     </>
   ),
-})
+});
