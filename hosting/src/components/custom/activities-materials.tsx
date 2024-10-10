a9299801612615bfcfc3b6905bf043253ab7a9c4
Alucsky <samuelmolinari8@gmail.com>
2024-10-09 23:59:04 -0300

    feat(activities-materials): add switch of shadcn and try ajust accordion

--- hosting/src/components/custom/activities-materials.tsx
+++ hosting/src/components/custom/activities-materials.tsx
@@ -1,37 +1,34 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+import { Switch } from '@/components/ui/switch'
 
-export function ActivitiesMaterials(){
+export function ActivitiesMaterials() {
   return (
     <>
-    <Accordion type="single" collapsible>
-  <AccordionItem value="item-1">
-    <AccordionTrigger>
-    <div>
-      <p>ass<br></br>ssss</p>
-
-    </div>
-
-
-    </AccordionTrigger>
-    <AccordionContent>
-      Yes. It adheres to the WAI-ARIA design pattern.
-    </AccordionContent>
-  </AccordionItem>
-</Accordion>
-
-
-    
-    
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+            <div className="flex">
+              <div className="bg-yellow-500 w-20 h-20"></div>
+              <div>
+                <h1 className="color:gray ml-0">Material 1</h1>
+                <p className="color:gray ml-0">Material de exemplo para atividade 1.</p>
+              </div>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent>
+            <div className="ml-4">
+              <hr />
+              <div className="flex">
+                <Switch />
+                <p className="ml-4">Aceita envios</p>
+              </div>
+              <hr />
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </>
-
-
   )
 }
-
 
 // use switch too for aceita envios
--- hosting/src/components/ui/accordion.tsx
+++ hosting/src/components/ui/accordion.tsx
@@ -12,7 +12,7 @@ const AccordionItem = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
-    className={cn("border-b", className)}
+    className={cn("", className)}
     {...props}
   />
 ))
@@ -26,7 +26,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 justify-between py-4 [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
@@ -44,10 +44,10 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    className="overflow-hidden  transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
-    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    <div className={cn("", className)}>{children}</div>
   </AccordionPrimitive.Content>
 ))
 
