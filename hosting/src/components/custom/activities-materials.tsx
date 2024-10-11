af2877a2ba2f6f3d893e2b4b7eb1b679f1b34e2c
Samuel Molinari <samuelmolinari8@gmail.com>
2024-10-10 22:19:18 -0300

    feat(activitiesmaterials): leaving it in a proportional size for PC

--- hosting/src/components/custom/activities-materials.tsx
+++ hosting/src/components/custom/activities-materials.tsx
@@ -5,54 +5,56 @@ import iconeAtividade from '@/assets/iconeAtividade.png'
 export function ActivitiesMaterials() {
   return (
     <>
-      <Accordion className="border rounded-3xl p-4 " type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger className="">
-            <div className="flex items-center">
-              <div className="flex justify-center items-center bg-yellow-500 w-20 h-20 rounded-full">
-                <img className="h-12 w-12 " src={iconeAtividade} alt="" />
-              </div>
-              <div className="ml-5">
-                <p className="flex color:gray ml-0 font-bold text-2xl text-gray-700">Titulo da atividade:</p>
-                <p className="flex color:gray ml-0 text-gray-300">data da atividade</p>
-              </div>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent className="">
-            <hr />
-            <div className="ml-4 flex p-16 justify-between">
-              <div className="flex gap-5">
-                <p className="text-lg text-gray-700">instruções</p>
-              </div>
-              <div className="flex flex-col gap-y-5">
-                <div className="flex gap-5">
-                  <div>
-                    <p className="text-6xl text-gray-700">0</p>
-                    <p className="text-gray-700">Entregue</p>
-                  </div>
-                  <div className="flex items-center">
-                    <div className="border-l-2 border-gray-300 h-24 mx-4"></div>
-                  </div>
-                  <div>
-                    <p className="text-6xl text-gray-700">0</p>
-                    <p className="text-gray-700">Pendentes</p>
-                  </div>
+      <div className="w-3/5 h-32">
+        <Accordion className="border rounded-3xl p-2" type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              <div className="flex items-center">
+                <div className="flex justify-center items-center bg-yellow-500 w-12 h-12 rounded-full">
+                  <img className="h-8 w-8" src={iconeAtividade} alt="" />
                 </div>
-
-                <div className="flex">
-                  <Switch />
-                  <p className="ml-4 text-gray-700">Aceita envios</p>
+                <div className="ml-4">
+                  <p className="flex color:gray font-bold text-lg text-gray-700">Titulo da atividade:</p>
+                  <p className="flex color:gray text-xs text-gray-300">data da atividade</p>
                 </div>
               </div>
-            </div>
-
-            <div className="flex gap-5 flex-col">
+            </AccordionTrigger>
+            <AccordionContent>
               <hr />
-              <p className="text-blue-600">Visualizar Entregues</p>
-            </div>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+              <div className="ml-2 flex p-8 justify-between">
+                <div className="flex gap-4">
+                  <p className="text-base text-gray-700">instruções</p>
+                </div>
+                <div className="flex flex-col gap-y-4">
+                  <div className="flex gap-4">
+                    <div>
+                      <p className="text-5xl text-gray-700">0</p>
+                      <p className="text-gray-700">Entregue</p>
+                    </div>
+                    <div className="flex items-center">
+                      <div className="border-l-2 border-gray-300 h-20 mx-2"></div>
+                    </div>
+                    <div>
+                      <p className="text-5xl text-gray-700">0</p>
+                      <p className="text-gray-700">Pendentes</p>
+                    </div>
+                  </div>
+
+                  <div className="flex">
+                    <Switch />
+                    <p className="ml-2 text-gray-700">Aceita envios</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex gap-4 flex-col">
+                <hr />
+                <p className="text-blue-600">Visualizar Entregues</p>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </>
   )
 }
