4c9293086a51b8476db366d531109c06b52f1f9f
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-10-11 23:01:59 -0300

    feat(generics-inputs.tsx): adding type only input e initing type checkbox

--- hosting/src/components/custom/generics-inputs.tsx
+++ hosting/src/components/custom/generics-inputs.tsx
@@ -1,15 +1,17 @@
 import { ErrorMessage, Field } from 'formik'
 import { Textarea } from '@/components/ui/textarea'
+import { Checkbox } from '@/components/ui/checkbox'
 interface InputProps {
-  title: string
+  title?: string
   label: string
-  placeholder: string
+  placeholder?: string
   id: string
   name: string
-  type: 'text' | 'file' | 'textarea'
+  type: 'text' | 'file' | 'textarea' | 'onlyInput' | 'checkbox'
   titleTextArea?: string
   customStyleLabel?: string
   customStyleButton?: string
+  icon?: JSX.Element
 }
 
 export function InputForm({
@@ -22,6 +24,7 @@ export function InputForm({
   titleTextArea,
   customStyleButton,
   customStyleLabel,
+  icon,
 }: InputProps) {
   if (type === 'text') {
     return (
@@ -58,6 +61,42 @@ export function InputForm({
         <label htmlFor={label}>{titleTextArea}</label>
         <Field placeholder={placeholder} id={id} name={name} as={Textarea} className="resize-none" />
       </div>
+    )
+  }
+
+  if (type === 'onlyInput') {
+    return (
+      <div className="relative w-full">
+        <Field placeholder={placeholder} id={id} name={name} className="w-full pr-10" />
+        <span className="absolute right-2 top-1/2 transform -translate-y-1/2">{icon}</span>
+      </div>
+    )
+  }
+
+  const vetorTeste = [
+    {
+      test1: 'Sim',
+    },
+    {
+      test2: 'Não',
+    },
+  ]
+
+  if (type === 'checkbox') {
+    return (
+      <>
+        {vetorTeste.map(({ test1, test2 }) => (
+          <div className="flex items-center space-x-2 mt-2" key={name}>
+            <Checkbox id={id} />
+            <label
+              htmlFor={label}
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              {test1}
+            </label>
+          </div>
+        ))}
+      </>
     )
   }
 }
