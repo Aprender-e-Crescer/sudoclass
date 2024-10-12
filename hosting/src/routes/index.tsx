351e1bfeb8bb899a3e2211394ccc1f0a014fe66d
7chaylon <chaylontdasilva@gmail.com>
2024-10-12 16:48:59 +0000

    feat(button component): refactor Button component: add new variants and icon support

--- hosting/src/routes/index.tsx
+++ hosting/src/routes/index.tsx
@@ -1,15 +1,33 @@
-import { Button } from '@/components/ui/button'
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button';
+import { createFileRoute } from '@tanstack/react-router';
+import { FaCheck } from 'react-icons/fa';
 
 export const Route = createFileRoute('/')({
   component: Index,
-})
+});
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-      <Button variant="destructive">Click me</Button>
+    <div className="flex gap-8">
+      <Button variant="blueButton" icon={<FaCheck />}>
+        Copiar
+      </Button>
+
+      <Button variant="copyButton" icon={<FaCheck />} size="fixed">
+        Copiar
+      </Button>
+
+      <Button variant="blueButton" icon={<FaCheck />} iconPosition='left' notificationCount={0}>
+        Blue
+      </Button>
+      <Button variant="lightTextBlack" icon={<FaCheck />} notificationCount={10}>
+        Light Text Black
+      </Button>
+      <Button variant="lightTextRed" icon={<FaCheck />} notificationCount={2}>
+        Light Text Red
+      </Button>
     </div>
-  )
+  );
 }
+
+export default Index;
