e13c255e8790024fc365b06ad0cf51f5286a3484
Alucsky <samuelmolinari8@gmail.com>
2024-10-11 23:07:08 -0300

    feat(activitiesmaterials): add vision of student for activities

--- hosting/src/components/custom/activities-materials.tsx
+++ hosting/src/components/custom/activities-materials.tsx
@@ -3,14 +3,58 @@ import { Switch } from '@/components/ui/switch'
 import iconeAtividade from '@/assets/iconeAtividade.png'
 import clsx from 'clsx'
 
-interface Props {
+interface PropsTeacher {
   title: string
   dataAtv: string
   instructions: string
   iconColor: string
 }
+interface PropsStudent {
+  title: string
+  dataAtv: string
+  instructions: string
+  iconColor: string
+  isSubmitted: boolean
+}
 
-export function ActivitiesMaterials({ title, dataAtv, instructions, iconColor }: Props) {
+export function ActivitiesMaterialsStudent({ title, dataAtv, instructions, iconColor, isSubmitted }: PropsStudent) {
+  return (
+    <div className="w-3/6 h-auto my-2.5">
+      <Accordion className="border rounded-2xl p-1" type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+            <div className="flex items-center">
+              <div className={clsx('flex justify-center items-center ', iconColor, 'w-10 h-10 rounded-full')}>
+                <img className="h-6 w-6" src={iconeAtividade} alt="" />
+              </div>
+              <div className="ml-2">
+                <p className="flex color:gray font-bold text-base text-gray-700">{title}</p>
+                <p className="flex color:gray text-xs text-gray-300">{dataAtv}</p>
+              </div>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent className="transition-all duration-300">
+            <hr className="my-2" />
+            <div className="ml-1 flex p-4 justify-between">
+              <div className="flex gap-2">
+                <p className="text-sm text-gray-700">Instruções</p>
+                <p className="text-sm mx-7">{instructions}</p>
+              </div>
+              <div className="flex flex-col gap-y-2">
+                <p className={`text-lg ${isSubmitted ? 'text-green-600' : 'text-red-600'}`}>
+                  {isSubmitted ? 'Atividade entregue' : 'Atividade pendente'}
+                </p>
+              </div>
+            </div>
+           
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  )
+}
+
+export function ActivitiesMaterialsTeacher({ title, dataAtv, instructions, iconColor }: PropsTeacher) {
   return (
     <div className="w-3/6 h-auto my-2.5">
       <Accordion className="border rounded-2xl p-1" type="single" collapsible>
--- hosting/src/routes/test-component.tsx
+++ hosting/src/routes/test-component.tsx
@@ -1,4 +1,4 @@
-import { ActivitiesMaterials } from '@/components/custom/activities-materials'
+import { ActivitiesMaterialsTeacher, ActivitiesMaterialsStudent } from '@/components/custom/activities-materials'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/test-component')({
@@ -8,23 +8,27 @@ export const Route = createFileRoute('/test-component')({
 function RouteActivitiesMaterials() {
   return (
     <div>
-      <ActivitiesMaterials
+      <h1>Visao Professor:</h1>
+      <ActivitiesMaterialsTeacher
         title="Atividade 1"
         dataAtv="11-10-2024"
         instructions='Análise de Texto: "Leia o texto indicado e faça uma análise crítica, destacando os principais argumentos do autor. Em seguida, elabore uma reflexão pessoal de, no mínimo, 300 palavras, conectando o conteúdo do texto com temas abordados em sala de aula."'
-        iconColor='bg-yellow-500'
+        iconColor="bg-yellow-500"
       />
-      <ActivitiesMaterials
-        title="Atividade 2"
-        dataAtv="12-11-2024"
-        instructions='Pesquisa em Grupo: "Formem grupos de até quatro pessoas e realizem uma pesquisa sobre o impacto das tecnologias sustentáveis no meio ambiente. Apresentem os resultados por meio de uma apresentação em PowerPoint com no máximo 10 slides."'
-        iconColor='bg-blue-500'
+      <h1>Visao Aluno:</h1>
+      <ActivitiesMaterialsStudent
+        title="Atividade 1"
+        dataAtv="11-10-2024"
+        instructions='Análise de Texto: "Leia o texto indicado e faça uma análise crítica, destacando os principais argumentos do autor. Em seguida, elabore uma reflexão pessoal de, no mínimo, 300 palavras, conectando o conteúdo do texto com temas abordados em sala de aula."'
+        iconColor="bg-yellow-500"
+        isSubmitted={false}
       />
-      <ActivitiesMaterials
-        title="Atividade 3"
-        dataAtv="22-01-2020"
-        instructions='Resolução de Problemas: "Resolva os cinco problemas matemáticos fornecidos no material da aula. Cada solução deve ser acompanhada de uma explicação detalhada do raciocínio utilizado para chegar à resposta correta."'
-        iconColor='bg-orange-500'
+      <ActivitiesMaterialsStudent
+        title="Atividade 1"
+        dataAtv="11-10-2024"
+        instructions='Análise de Texto: "Leia o texto indicado e faça uma análise crítica, destacando os principais argumentos do autor. Em seguida, elabore uma reflexão pessoal de, no mínimo, 300 palavras, conectando o conteúdo do texto com temas abordados em sala de aula."'
+        iconColor="bg-yellow-500"
+        isSubmitted={true}
       />
     </div>
   )
