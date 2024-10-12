aa85158daf185016f9e8c3f64b150089225a3ab7
Alucsky <samuelmolinari8@gmail.com>
2024-10-11 22:52:54 -0300

    feat(activitiesmaterials): add more props and ajust style

--- hosting/src/components/custom/activities-materials.tsx
+++ hosting/src/components/custom/activities-materials.tsx
@@ -1,65 +1,66 @@
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
 import { Switch } from '@/components/ui/switch'
 import iconeAtividade from '@/assets/iconeAtividade.png'
+import clsx from 'clsx'
 
-interface props {
-  title: string,
-  dataAtv: string,
+interface Props {
+  title: string
+  dataAtv: string
+  instructions: string
+  iconColor: string
 }
 
-export function ActivitiesMaterials({title, dataAtv}: props) {
+export function ActivitiesMaterials({ title, dataAtv, instructions, iconColor }: Props) {
   return (
-    <>
-      <div className="w-3/5 h-32">
-        <Accordion className="border rounded-3xl p-2" type="single" collapsible>
-          <AccordionItem value="item-1">
-            <AccordionTrigger>
-              <div className="flex items-center">
-                <div className="flex justify-center items-center bg-yellow-500 w-12 h-12 rounded-full">
-                  <img className="h-8 w-8" src={iconeAtividade} alt="" />
-                </div>
-                <div className="ml-4">
-                  <p className="flex color:gray font-bold text-lg text-gray-700">{title}</p>
-                  <p className="flex color:gray text-xs text-gray-300">{dataAtv}</p>
-                </div>
+    <div className="w-3/6 h-auto my-2.5">
+      <Accordion className="border rounded-2xl p-1" type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+            <div className="flex items-center">
+              <div className={clsx('flex justify-center items-center ', iconColor, 'w-10 h-10 rounded-full')}>
+                <img className="h-6 w-6 " src={iconeAtividade} alt="" />
               </div>
-            </AccordionTrigger>
-            <AccordionContent>
-              <hr />
-              <div className="ml-2 flex p-8 justify-between">
-                <div className="flex gap-4">
-                  <p className="text-base text-gray-700">instruções</p>
-                </div>
-                <div className="flex flex-col gap-y-4">
-                  <div className="flex gap-4">
-                    <div>
-                      <p className="text-5xl text-gray-700">0</p>
-                      <p className="text-gray-700">Entregue</p>
-                    </div>
-                    <div className="flex items-center">
-                      <div className="border-l-2 border-gray-300 h-20 mx-2"></div>
-                    </div>
-                    <div>
-                      <p className="text-5xl text-gray-700">0</p>
-                      <p className="text-gray-700">Pendentes</p>
-                    </div>
+              <div className="ml-2">
+                <p className="flex color:gray font-bold text-base text-gray-700">{title}</p>
+                <p className="flex color:gray text-xs text-gray-300">{dataAtv}</p>
+              </div>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent className="transition-all duration-300">
+            <hr className="my-2" />
+            <div className="ml-1 flex p-4 justify-between">
+              <div className="flex gap-2">
+                <p className="text-sm text-gray-700">Instruções</p>
+                <p className="text-sm mx-7">{instructions}</p>
+              </div>
+              <div className="flex flex-col gap-y-2">
+                <div className="flex gap-2">
+                  <div>
+                    <p className="text-3xl text-gray-700">0</p>
+                    <p className="text-gray-700">Entregue</p>
                   </div>
-
-                  <div className="flex">
-                    <Switch />
-                    <p className="ml-2 text-gray-700">Aceita envios</p>
+                  <div className="flex items-center">
+                    <div className="border-l-2 border-gray-300 h-10 mx-1"></div>
+                  </div>
+                  <div>
+                    <p className="text-3xl text-gray-700">0</p>
+                    <p className="text-gray-700">Pendentes</p>
                   </div>
                 </div>
-              </div>
 
-              <div className="flex gap-4 flex-col">
-                <hr />
-                <p className="text-blue-600">Visualizar Entregues</p>
+                <div className="flex items-center">
+                  <Switch />
+                  <p className="ml-3 text-gray-700">Aceita envios</p>
+                </div>
               </div>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </div>
-    </>
+            </div>
+            <div className="flex flex-col p-2">
+              <hr className="my-2" />
+              <p className="text-blue-600 text-sm">Visualizar Entregues</p>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
   )
 }
