090723a3c4b0ff274bf93d49c458e7664feed627
Ariel <arielcristiandemenezes4@gmail.com>
2024-10-14 20:55:23 -0300

    feat(dialogremuvesubject): it is a dialogue when it is possible to exclude matters

---
+++ hosting/src/components/custom/dialog-remuve-subject.tsx
@@ -0,0 +1,47 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+export function DialogRemuveSubject() {
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger>Remover</DialogTrigger>
+        <DialogContent className="p-4 rounded-lg w-full max-sm:p-3 max-sm:w-[290px]">
+          <DialogHeader className="relative">
+            <DialogTitle className="flex justify-center text-xl font-medium">
+              Deseja remover a matéria
+            </DialogTitle>
+          </DialogHeader>
+          <div className="mt-2 space-y-1 pl-5">
+            <div className="flex items-center space-x-2">
+              <div className="w-4 h-4 rounded-full bg-orange-400"></div>
+              <span className="text-xl font-semibold text-gray-700">AOO</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <div className="w-4 h-4 rounded-full bg-blue-700"></div>
+              <span className="text-xl font-semibold text-gray-700">UX/UI</span>
+            </div>
+          </div>
+          <div className="flex justify-center mt-4 space-x-2">
+            <DialogClose asChild>
+              <button className="flex-1 px-4 py-2 bg-red-500 text-white font-medium rounded-md hover:bg-red-600 transition">
+                remover
+              </button>
+            </DialogClose>
+            <DialogClose asChild>
+              <button className="flex-1 px-4 py-2 bg-blue-500 text-white font-medium rounded-md hover:bg-blue-600 transition">
+                Cancelar
+              </button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
