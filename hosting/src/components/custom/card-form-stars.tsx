5eda42d23e25b560c545b69cad3bd9e0614f163a
Jonatas Fiorentin <jonatasfiorentin@gmail.com>
2024-10-14 19:54:01 -0300

    feat(card-form-stars): adding responsiveness to the component

--- hosting/src/components/custom/card-form-stars.tsx
+++ hosting/src/components/custom/card-form-stars.tsx
@@ -10,7 +10,7 @@ export default function CardFormStars({ title }: RatingProps) {
   console.log(value)
 
   return (
-    <div className="border-[#0C408F] border-2 rounded-xl w-[652px] h-36 flex flex-col items-center px-3 py-5 justify-between">
+    <div className="border-[#0C408F] border-2 rounded-xl w-[280px] h-36 flex flex-col items-center px-3 py-5 justify-between sm:w-[652px]">
       <p className="text-2xl">{title}</p>
       <hr className="w-full border-[#0C408F]" />
       <Rating
