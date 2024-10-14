35922c1a9e85de01f6f68dca46d088a901ae8c2a
Samuel Molinari <samuelmolinari8@gmail.com>
2024-10-14 19:39:20 -0300

    feat(activitiesmaterials): ajust type(teacher and student)

--- hosting/src/components/custom/activities-materials.tsx
+++ hosting/src/components/custom/activities-materials.tsx
@@ -3,110 +3,120 @@ import { Switch } from '@/components/ui/switch'
 import iconeAtividade from '@/assets/iconeAtividade.png'
 import clsx from 'clsx'
 
-interface PropsTeacher {
+interface ActivitiesMaterialsProps {
   title: string
   dataAtv: string
   instructions: string
   iconColor: string
-  assigned: number
-  pending: number
-}
-interface PropsStudent {
-  title: string
-  dataAtv: string
-  instructions: string
-  iconColor: string
-  isSubmitted: boolean
+  assigned?: number
+  pending?: number
+  isSubmitted?: boolean
+  type: 'teacher' | 'student'
 }
 
-export function ActivitiesMaterialsStudent({ title, dataAtv, instructions, iconColor, isSubmitted  }: PropsStudent) {
-  return (
-    <div className="w-3/6 h-auto my-2.5">
-      <Accordion className="border rounded-2xl p-1" type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>
-            <div className="flex items-center">
-              <div className={clsx('flex justify-center items-center ', iconColor, 'w-10 h-10 rounded-full')}>
-                <img className="h-6 w-6" src={iconeAtividade} alt="" />
-              </div>
-              <div className="ml-2">
-                <p className="flex color:gray font-bold text-base text-gray-700">{title}</p>
-                <p className="flex color:gray text-xs text-gray-300">{dataAtv}</p>
-              </div>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent className="transition-all duration-300">
-            <hr className="my-2" />
-            <div className="ml-1 flex p-4 justify-between">
-              <div className="flex gap-2">
-                <p className="text-sm text-gray-700">Instruções</p>
-                <p className="text-sm mx-7">{instructions}</p>
-              </div>
-              <div className="flex flex-col gap-y-2">
-                <p className={`text-lg ${isSubmitted ? 'text-green-600' : 'text-red-600'}`}>
-                  {isSubmitted ? 'Atividade entregue' : 'Atividade pendente'}
-                </p>
-              </div>
-            </div>
-           
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </div>
-  )
-}
-
-export function ActivitiesMaterialsTeacher({ title, dataAtv, instructions, iconColor, assigned, pending }: PropsTeacher) {
-  return (
-    <div className="w-3/6 h-auto my-2.5">
-      <Accordion className="border rounded-2xl p-1" type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>
-            <div className="flex items-center">
-              <div className={clsx('flex justify-center items-center ', iconColor, 'w-10 h-10 rounded-full')}>
-                <img className="h-6 w-6 " src={iconeAtividade} alt="" />
-              </div>
-              <div className="ml-2">
-                <p className="flex color:gray font-bold text-base text-gray-700">{title}</p>
-                <p className="flex color:gray text-xs text-gray-300">{dataAtv}</p>
-              </div>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent className="transition-all duration-300">
-            <hr className="my-2" />
-            <div className="ml-1 flex p-4 justify-between">
-              <div className="flex gap-2">
-                <p className="text-sm text-gray-700">Instruções</p>
-                <p className="text-sm mx-7">{instructions}</p>
-              </div>
-              <div className="flex flex-col gap-y-2">
-                <div className="flex gap-2">
-                  <div>
-                    <p className="text-3xl text-gray-700">{assigned}</p>
-                    <p className="text-gray-700">Entregue</p>
-                  </div>
-                  <div className="flex items-center">
-                    <div className="border-l-2 border-gray-300 h-10 mx-1"></div>
-                  </div>
-                  <div>
-                    <p className="text-3xl text-gray-700">{pending}</p>
-                    <p className="text-gray-700">Pendentes</p>
-                  </div>
+export function ActivitiesMaterials({
+  title,
+  dataAtv,
+  instructions,
+  iconColor,
+  assigned,
+  pending,
+  isSubmitted,
+  type,
+}: ActivitiesMaterialsProps) {
+  if (type === 'teacher') {
+    return (
+      <div className="w-3/6 h-auto my-2.5">
+        <Accordion className="border rounded-2xl p-1" type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              <div className="flex items-center">
+                <div className={clsx('flex justify-center items-center ', iconColor, 'w-10 h-10 rounded-full')}>
+                  <img className="h-6 w-6 " src={iconeAtividade} />
                 </div>
-
-                <div className="flex items-center">
-                  <Switch />
-                  <p className="ml-3 text-gray-700">Aceita envios</p>
+                <div className="ml-2">
+                  <p className="flex color:gray font-bold text-base text-gray-700">{title}</p>
+                  <p className="flex color:gray text-xs text-gray-300">{dataAtv}</p>
                 </div>
               </div>
-            </div>
-            <div className="flex flex-col p-2">
+            </AccordionTrigger>
+            <AccordionContent className="transition-all duration-300">
               <hr className="my-2" />
-              <p className="text-blue-600 text-sm">Visualizar Entregues</p>
-            </div>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+              <div className="flex flex-col ml-1 md:flex p-4 justify-between  gap-5">
+                <div className="hidden md:flex gap-2">
+                  <p className="text-sm text-gray-700">Instruções</p>
+                  <p className="text-sm mx-7">{instructions}</p>
+                </div>
+                <div className="flex flex-col gap-y-2">
+                  <div className="flex gap-2">
+                    <div>
+                      <p className="text-3xl text-gray-700">{assigned}</p>
+                      <p className="text-gray-700">Entregue</p>
+                    </div>
+                    <div className="flex items-center">
+                      <div className="border-l-2 border-gray-300 h-10 mx-1"></div>
+                    </div>
+                    <div>
+                      <p className="text-3xl text-gray-700">{pending}</p>
+                      <p className="text-gray-700">Pendentes</p>
+                    </div>
+                  </div>
+
+                  <div className="flex items-center">
+                    <Switch />
+                    <p className="ml-3 text-gray-700">Aceita envios</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col p-2">
+                <hr className="my-2" />
+                <p className="text-blue-600 text-sm">Visualizar Entregues</p>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    )
+  } else if (type === 'student') {
+    return (
+      <>
+        <div className="w-3/6 h-auto my-2.5">
+          <Accordion className="border rounded-2xl p-1" type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                <div className="flex items-center">
+                  <div className={clsx('flex justify-center items-center ', iconColor, 'w-10 h-10 rounded-full')}>
+                    <img className="h-6 w-6" src={iconeAtividade} alt="" />
+                  </div>
+                  <div className="ml-2">
+                    <p className="flex color:gray font-bold text-base text-gray-700">{title}</p>
+                    <p className="flex color:gray text-xs text-gray-300">{dataAtv}</p>
+                  </div>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent className="transition-all duration-300">
+                <hr className="my-2" />
+                <div className="flex flex-col ml-1 md:flex p-4 justify-between gap-5">
+                  <div className="hidden md:flex gap-2">
+                    <p className="text-sm text-gray-700">Instruções</p>
+                    <p className="text-sm mx-7">{instructions}</p>
+                  </div>
+                  <div className="flex flex-col gap-y-2">
+                    <p className={`text-lg ${isSubmitted ? 'text-green-600' : 'text-red-600'}`}>
+                      {isSubmitted ? 'Atividade entregue' : 'Atividade pendente'}
+                    </p>
+                  </div>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </>
+    )
+  }
+  return (
+    <div className="flex flex-col gap-y-2">
+      <p className="text-lg text-red-600">Erro de tipagem, insira student ou teacher no type</p>
     </div>
   )
 }
--- hosting/src/routes/test-component.tsx
+++ hosting/src/routes/test-component.tsx
@@ -1,4 +1,4 @@
-import { ActivitiesMaterialsTeacher, ActivitiesMaterialsStudent } from '@/components/custom/activities-materials'
+import { ActivitiesMaterials } from '@/components/custom/activities-materials'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/test-component')({
@@ -9,28 +9,23 @@ function RouteActivitiesMaterials() {
   return (
     <div>
       <h1>Visao Professor:</h1>
-      <ActivitiesMaterialsTeacher
+      <ActivitiesMaterials
         title="Atividade 1"
         dataAtv="11-10-2024"
         instructions='Análise de Texto: "Leia o texto indicado e faça uma análise crítica, destacando os principais argumentos do autor. Em seguida, elabore uma reflexão pessoal de, no mínimo, 300 palavras, conectando o conteúdo do texto com temas abordados em sala de aula."'
         iconColor="bg-yellow-500"
         assigned={30}
         pending={1}
+        type="teacher"
       />
       <h1>Visao Aluno:</h1>
-      <ActivitiesMaterialsStudent
+      <ActivitiesMaterials
         title="Atividade 1"
         dataAtv="11-10-2024"
         instructions='Análise de Texto: "Leia o texto indicado e faça uma análise crítica, destacando os principais argumentos do autor. Em seguida, elabore uma reflexão pessoal de, no mínimo, 300 palavras, conectando o conteúdo do texto com temas abordados em sala de aula."'
         iconColor="bg-yellow-500"
         isSubmitted={false}
-      />
-      <ActivitiesMaterialsStudent
-        title="Atividade 1"
-        dataAtv="11-10-2024"
-        instructions='Análise de Texto: "Leia o texto indicado e faça uma análise crítica, destacando os principais argumentos do autor. Em seguida, elabore uma reflexão pessoal de, no mínimo, 300 palavras, conectando o conteúdo do texto com temas abordados em sala de aula."'
-        iconColor="bg-yellow-500"
-        isSubmitted={true}
+        type="student"
       />
     </div>
   )
