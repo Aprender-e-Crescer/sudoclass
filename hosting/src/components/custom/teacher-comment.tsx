63ba632fa71b327f67ff153d0d4261822a1caf40
BrunoLeblein2 <brunoleblein@gmail.com>
2024-10-13 22:05:23 -0300

    feat: doing teachers comment component and adding image to assets

--- hosting/src/components/custom/teacher-comment.tsx
+++ hosting/src/components/custom/teacher-comment.tsx
@@ -1,7 +1,25 @@
+import React from 'react'
+import * as Avatar from '@radix-ui/react-avatar'
+
 export function TeacherComment() {
   return (
     <div>
-      <p></p>
+      <div className="w-[993px] h-[83px] mx-auto p-4 bg-white shadow-lg rounded-lg flex space-x-4">
+        <Avatar.Root className="inline-flex items-center justify-center w-12 h-12 rounded-full bg-gray-100">
+          <Avatar.Image
+            className="w-full h-full rounded-full object-cover"
+            src=".../assets/teachercommentavatarphoto.png"
+            alt="Alexandre Martinek"
+          />
+          <Avatar.Fallback className="text-xl text-gray-500">AM</Avatar.Fallback>
+        </Avatar.Root>
+
+        <div className="flex flex-col justify-center">
+          <span className="text-sm font-medium text-gray-800">Alexandre Martinek</span>
+          <span className="text-xs text-gray-500">Ontem</span>
+          <p className="mt-2 text-sm text-gray-700">Prova amanhã pessoal! Não faltem</p>
+        </div>
+      </div>
     </div>
   )
 }
