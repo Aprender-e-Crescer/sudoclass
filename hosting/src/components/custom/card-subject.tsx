be169d57c9979901fce53fafac987de950f71c73
Rafael <Rafael>
2024-10-14 18:12:33 -0300

    feat(card-subject): making the component responsive

--- hosting/src/components/custom/card-subject.tsx
+++ hosting/src/components/custom/card-subject.tsx
@@ -1,7 +1,13 @@
 export function CardSubject() {
   return (
-    <div>
-      <p>teste</p>
+    <div className="w-full max-w-[384px] sm:w-[443px] h-44 sm:h-80 bg-[#DE9E2F] rounded-lg shadow-lg flex flex-col justify-between">
+      <div className="flex flex-col m-4">
+        <div className="text-white font-bold text-xl">Matéria</div>
+        <div className="text-black text-sm sm:text-base">descrição</div>
+      </div>
+      <div className="bg-white w-full rounded-b-lg flex items-center justify-end p-2">
+        <p className="text-black text-sm sm:text-base">notas</p>
+      </div>
     </div>
   )
 }
