acadbc26bf2c21196b0990bd4c2c767823108057
Jonatas Fiorentin <jonatasfiorentin@gmail.com>
2024-10-14 21:50:17 -0300

    fix(card-form-stars): fixing component responsiveness

--- hosting/src/components/custom/card-form-stars.tsx
+++ hosting/src/components/custom/card-form-stars.tsx
@@ -10,15 +10,15 @@ export default function CardFormStars({ title }: RatingProps) {
   console.log(value)
 
   return (
-    <div className="border-[#0C408F] border-2 rounded-xl w-[280px] h-36 flex flex-col items-center px-3 py-5 justify-between sm:w-[652px]">
-      <p className="text-2xl">{title}</p>
-      <hr className="w-full border-[#0C408F]" />
+    <div className="border-[#0C408F] border-2 rounded-xl w-[280px] h-auto flex flex-col items-center px-3 py-5 justify-between sm:w-[652px]">
+      <p className="text-2xl mb-2 text-center">{title}</p>
+      <hr className="w-full border-[#0C408F]  mb-10" />
       <Rating
         value={value}
         onChange={(_event, newValue) => {
           setValue(newValue ?? 0)
         }}
-        className="scale-[2]"
+        className="scale-[2] pb-2"
       />
     </div>
   )
