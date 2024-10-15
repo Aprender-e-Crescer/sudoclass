9408183dd02ee43372461a40b3d1ecf0d4c5980b
Samuel Molinari <samuelmolinari8@gmail.com>
2024-10-14 22:26:54 -0300

    feat(activitiesmaterials): add props to links

--- hosting/src/components/custom/activities-materials.tsx
+++ hosting/src/components/custom/activities-materials.tsx
@@ -2,7 +2,10 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/
 import { Switch } from '@/components/ui/switch'
 import iconeAtividade from '@/assets/iconeAtividade.png'
 import clsx from 'clsx'
+import { Link } from '@tanstack/react-router'
 interface ActivitiesMaterialsProps {
+  id: string
+  to: string
   title: string
   dateActivity: string
   instructions: string
@@ -13,6 +16,8 @@ interface ActivitiesMaterialsProps {
 }
 
 export function ActivitiesMaterials({
+  id,
+  to,
   title,
   dateActivity,
   instructions,
@@ -80,7 +85,9 @@ export function ActivitiesMaterials({
               </div>
               <div className="flex flex-col p-2">
                 <hr className="my-2" />
-                <p className="text-blue-600 text-sm">Visualizar Entregues</p>
+                <Link to={to} search={{ id }}>
+                  <p className="text-blue-600 text-sm">Visualizar Entregues</p>
+                </Link>
               </div>
             </AccordionContent>
           </AccordionItem>
@@ -90,15 +97,17 @@ export function ActivitiesMaterials({
   } else if (type === 'student') {
     return (
       <>
-        <div className="flex items-center w-3/6 md:hidden border p-4 rounded-2xl my-2.5">
-          <div className={clsx('flex justify-center items-center', iconColor, 'w-10 h-10 rounded-full')}>
-            <img className="h-6 w-6" src={iconeAtividade} alt="" />
+        <Link to={to} search={{ id }}>
+          <div className="flex items-center w-3/6 md:hidden border p-4 rounded-2xl my-2.5">
+            <div className={clsx('flex justify-center items-center', iconColor, 'w-10 h-10 rounded-full')}>
+              <img className="h-6 w-6" src={iconeAtividade} alt="" />
+            </div>
+            <div className="ml-2">
+              <p className="flex color:gray font-bold text-base text-gray-700">{title}</p>
+              <p className="flex color:gray text-xs text-gray-300">{dateActivity}</p>
+            </div>
           </div>
-          <div className="ml-2">
-            <p className="flex color:gray font-bold text-base text-gray-700">{title}</p>
-            <p className="flex color:gray text-xs text-gray-300">{dateActivity}</p>
-          </div>
-        </div>
+        </Link>
 
         <div className="hidden md:flex md:w-3/6 h-auto my-2.5">
           <Accordion className="border rounded-2xl px-4 w-full" type="single" collapsible>
