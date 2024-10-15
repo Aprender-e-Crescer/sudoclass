e1ce3463dedbb139e2bc763b4f7689b6ac8cdb83
Yuosssef <yuossefpolidorio@gmial.com>
2024-10-14 21:47:22 -0300

    fix: i added the env. environment

--- hosting/src/routes/__root.tsx
+++ hosting/src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { Subheader } from '@/components/custom/subheader';
 
 export const Route = createRootRoute({
   component: () => (
@@ -17,6 +18,7 @@ export const Route = createRootRoute({
       </div>
       <hr />
       <Outlet />
+      <Subheader classroomType="student"/>
       <TanStackRouterDevtools />
     </>
   ),
