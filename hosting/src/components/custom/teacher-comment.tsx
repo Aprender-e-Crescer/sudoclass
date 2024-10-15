08f6cb53d22e42bc3965f1ff829363c25943692b
BrunoLeblein2 <brunoleblein@gmail.com>
2024-10-14 21:03:59 -0300

    feat(teachercomment): adjusting screen responsiveness

--- hosting/src/components/custom/teacher-comment.tsx
+++ hosting/src/components/custom/teacher-comment.tsx
@@ -9,18 +9,18 @@ interface TeacherCommentProps {
 
 export function TeacherComment({ name, date, comment, textAvatar }: TeacherCommentProps) {
   return (
-    <div>
-      <div className="w-[993px] h-[83px] mx-auto p-4 bg-white shadow-lg rounded-lg flex space-x-4">
+    <div className="flex">
+      <div className="w-full max-w-[993px] mx-auto p-4 bg-white shadow-lg rounded-lg flex gap-x-3">
         <Avatar.Root className="inline-flex items-center justify-center w-12 h-12 rounded-full bg-gray-100">
           <Avatar.Image
             className="w-full h-full rounded-full object-cover"
             src=".../assets/teachercommentavatarphoto.png"
-            alt="Alexandre Martinek"
+            alt={name}
           />
           <Avatar.Fallback className="text-xl text-gray-500">{textAvatar}</Avatar.Fallback>
         </Avatar.Root>
 
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col sm:text-left">
           <span className="text-sm font-medium text-gray-800">{name}</span>
           <span className="text-xs text-gray-500">{date}</span>
           <p className="mt-2 text-sm text-gray-700">{comment}</p>
