ae5941c80ab45fe751cb7a9335dcc9fcb87b94ea
enzo-savaris <savarisenzo2@gmail.com>
2024-10-14 21:02:06 -0300

    feat(confirmation-screen.tsx): desenvolvimento do campo de confirmação de exclusão de plano de aula

--- hosting/src/routes/__root.tsx
+++ hosting/src/routes/__root.tsx
@@ -1,5 +1,5 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
@@ -14,10 +14,13 @@ export const Route = createRootRoute({
         <Link to="/password-changes" className="[&.active]:font-bold">
           password changes
         </Link>
+        <Link to="/confirmation-screen" className="[&.active]:font-bold">
+          confirmation screen
+        </Link>
       </div>
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
-});
+})
