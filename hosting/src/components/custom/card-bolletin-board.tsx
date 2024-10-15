763d24edf9f77718d85eae7ad5317cfbfecbcf69
kozikoski <danielkozikoski17012007@gmail.com>
2024-10-14 21:26:48 -0300

    feat(card-bolletin): completing Card-bolletin component

---
+++ hosting/src/components/custom/card-bolletin-board.tsx
@@ -0,0 +1,15 @@
+// src/components/custom/card-bolletin-board.tsx
+
+interface CardComponentProps {
+  materia: string
+  descricao: string
+}
+
+export function CardComponent({ materia, descricao }: CardComponentProps) {
+  return (
+    <div className="bg-[#D89022] text-white w-full max-w-[1068px] h-[258px] rounded-lg p-8 flex flex-col justify-end">
+      <h1 className="text-3xl sm:text-4xl font-bold">{materia}</h1>
+      <p className="text-base sm:text-lg text-gray-300">{descricao}</p>
+    </div>
+  )
+}
---
+++ hosting/src/routes/card-bolletin.tsx
@@ -0,0 +1,15 @@
+// src/routes/card-bolletin.tsx
+
+import { createFileRoute } from '@tanstack/react-router';
+import { CardComponent } from '../components/custom/card-bolletin-board'; // Ajuste o caminho se necessário
+
+export const Route = createFileRoute('/card-bolletin')({
+  component: () => (
+    <div>
+      <CardComponent 
+        materia="Sua Matéria" 
+        descricao="Descrição da sua matéria." 
+      />
+    </div>
+  ),
+});
