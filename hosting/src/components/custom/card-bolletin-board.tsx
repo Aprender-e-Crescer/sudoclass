4721a068649ce5b07ef6b4abcc939565f2037253
kozikoski <danielkozikoski17012007@gmail.com>
2024-10-15 21:19:46 -0300

    refactor(card-boletin): changed name of props

--- hosting/src/components/custom/card-bolletin-board.tsx
+++ hosting/src/components/custom/card-bolletin-board.tsx
@@ -1,12 +1,12 @@
 interface CardComponentProps {
-  matter: string
+  name: string
   description: string
 }
 
-export function CardComponent({ matter, description }: CardComponentProps) {
+export function CardComponent({ name, description }: CardComponentProps) {
   return (
     <div className="bg-[#D89022] text-white w-full max-w-[1068px] h-[258px] rounded-lg p-8 flex flex-col justify-end">
-      <h1 className="text-3xl sm:text-4xl font-bold">{matter}</h1>
+      <h1 className="text-3xl sm:text-4xl font-bold">{name}</h1>
       <p className="text-base sm:text-lg text-gray-300">{description}</p>
     </div>
   )
