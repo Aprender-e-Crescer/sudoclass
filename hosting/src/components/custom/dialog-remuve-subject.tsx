b64454eddea74cc71b91a515f1e2a491cbb473ae
Ariel <arielcristiandemenezes4@gmail.com>
2024-10-15 21:34:09 -0300

    refactor(dialogremovesubject): i just switched to props

--- hosting/src/components/custom/dialog-remuve-subject.tsx
+++ hosting/src/components/custom/dialog-remuve-subject.tsx
@@ -7,7 +7,16 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
-export function DialogRemoveSubject() {
+interface Subject {
+  name: string;
+  color: string;
+}
+
+interface Props {
+  subjects: Subject[]
+}
+
+export function DialogRemoveSubject({ subjects }: Props) {
   return (
     <>
       <Dialog>
@@ -19,23 +28,30 @@ export function DialogRemoveSubject() {
             </DialogTitle>
           </DialogHeader>
           <div className="mt-2 space-y-1 pl-5">
-            <div className="flex items-center space-x-2">
-              <div className="w-4 h-4 rounded-full bg-orange-400"></div>
-              <span className="text-xl font-semibold text-gray-700">AOO</span>
-            </div>
-            <div className="flex items-center space-x-2">
-              <div className="w-4 h-4 rounded-full bg-blue-700"></div>
-              <span className="text-xl font-semibold text-gray-700">UX/UI</span>
-            </div>
+            {subjects && subjects.length > 0 ? (
+              subjects.map((subject, index) => (
+                <div key={index} className="flex items-center space-x-2">
+                  <div
+                    className="w-4 h-4 rounded-full bg-"
+                    style={{ backgroundColor: subject.color }}
+                  ></div>
+                  <span className="text-xl font-semibold text-gray-700">
+                    {subject.name}
+                  </span>
+                </div>
+              ))
+            ) : (
+              <p className="text-gray-500">Nenhuma matéria disponível.</p>
+            )}
           </div>
           <div className="flex justify-center mt-4 space-x-2">
             <DialogClose asChild>
-              <button className="flex-1 px-4 py-2 bg-red-500 text-white font-medium rounded-md hover:bg-red-600 transition">
+              <button className="flex-1 px-4 py-2 bg-[#e6e9eb] text-black font-medium rounded-md hover:bg-[#d8dbdd] transition">
                 Remover
               </button>
             </DialogClose>
             <DialogClose asChild>
-              <button className="flex-1 px-4 py-2 bg-blue-500 text-white font-medium rounded-md hover:bg-blue-600 transition">
+              <button className="flex-1 px-4 py-2 bg-[#1A73E8] text-white font-medium rounded-md hover:bg-blue-700 transition">
                 Cancelar
               </button>
             </DialogClose>
