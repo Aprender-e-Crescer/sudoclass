dad11a995d5a5b09fb8d998c0117acc0e309530e
Pablo Gabirel Bertholdo <pablobertholdo2008@gmail.com>
2024-10-15 21:07:29 -0300

    style(header): correcting margin

--- hosting/src/components/custom/header.tsx
+++ hosting/src/components/custom/header.tsx
@@ -9,11 +9,11 @@ interface headerProps {
 }
 export function Header ({ logo = sudotecLogo, avatarImage, avatarFallBack}: headerProps) {
     return(
-        <div className='flex justify-between items-center border'>
-            <div className='flex h-24 w-40 ml-10 '>
+        <div className='flex justify-between items-center border p-4'>
+            <div className='flex h-24 w-40'>
                 <img src= {logo} alt="sudotecLogo" />
             </div>
-            <div className='flex h-11 w-11 mr-10'>
+            <div className='flex h-11 w-11'>
             <Avatar>
                 <img src={avatarImage} />
                 <AvatarFallback>{avatarFallBack}</AvatarFallback>
