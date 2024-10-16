9111d908210ca51adfcda3f1c5d26b5e4a1f58de
Beatriz Defrein <beatrizdefrein67@gmail.com>
2024-10-15 21:57:27 -0300

    feat(class-user-registration.tsx): adding checkbox input

--- hosting/src/routes/class-user-resgistration.tsx
+++ hosting/src/routes/class-user-resgistration.tsx
@@ -1,4 +1,5 @@
-import { InputForm } from '@/components/custom/generics-inputs'
+import { InputCheckbox } from '@/components/custom/checkbox-input'
+import { InputForm } from '@/components/custom/text-input'
 import { changePasswordRequestSchema } from '@/models/change-password-request-schema'
 import { createFileRoute } from '@tanstack/react-router'
 import { Form, Formik } from 'formik'
@@ -14,77 +15,93 @@ const initialValues = {
     studentName: '',
 }
 
+const checkboxValues = [{
+  value: "Sim",
+  label: "yes"
+},
+{
+  value: "Não",
+  label: "no"
+}
+]
+
 export function formRegistration() {
   return (
     <>
-    <Formik 
-        onSubmit={() => {}}
-        initialValues={initialValues}
-        validationSchema={toFormikValidationSchema(changePasswordRequestSchema) }
-    >
+      <Formik 
+          onSubmit={() => {}}
+          initialValues={initialValues}
+          validationSchema={toFormikValidationSchema(changePasswordRequestSchema) }
+      >  
       <Form>
-        <div>
-            <h1 className='font-semibold'>ALUNO</h1>
-        </div>
-             
-        <InputForm
-          title="Turma" 
-          id="tumaRegistration"
-          name="tumaRegistration"
-          placeholder="Nome Da Turma" 
-          label="tumaRegistration"
-          type="text"
-        />
-        <InputForm
-          title="Turno"
-          id="shiftRecord"
-          name="shiftRecord"
-          placeholder="Turno"
-          label="shiftRecord"
-          type="text"
-        />
-        <InputForm
-          title="Previsão De Início"
-          id="startPredictionRecord"
-          name="startPredictionRecord"
-          placeholder="Previsão De Início"
-          label="startPredictionRecord"
-          type="text"
-        />
-        <InputForm
-          title="Previsão De Fim"
-          id="endPredictionRecordt"
-          name="endPredictionRecordt"
-          placeholder="Previsão De Fim"
-          label="endPredictionRecordt"
-          type="text"
-        />
-        <InputForm
-          title="Data Final Inscrição"
-          id="registrationEndDate"
-          name="registrationEndDate"
-          placeholder="Data Final Inscrição"
-          label="registrationEndDate"
-          type="text"
-        />
-        <InputForm
-          title="Qtde.Horas"
-          id="recordingNumbeOfHours"
-          name="recordingNumbeOfHours"
-          placeholder="Qtde.Horas"
-          label="recordingNumbeOfHours"
-          type="text"
-        />
-        <InputForm
-          title="Total De Vagas"
-          id="recordOfTotalVacancies"
-          name="recordOfTotalVacancies"
-          placeholder="Total De Vagas"
-          label="recordOfTotalVacancies"
-          type="text"
-        />
-    </Form>
-  </Formik>
-</>
-  )
+          <div>
+              <h1 className='font-semibold'>ALUNO</h1>
+          </div>
+          <InputForm
+            title="Turma" 
+            id="tumaRegistration"
+            name="tumaRegistration"
+            placeholder="Nome Da Turma" 
+            label="tumaRegistration"
+          />
+          <InputForm
+            title="Turno"
+            id="shiftRecord"
+            name="shiftRecord"
+            placeholder="Turno"
+            label="shiftRecord"
+          />
+          <InputForm
+            title="Previsão De Início"
+            id="startPredictionRecord"
+            name="startPredictionRecord"
+            placeholder="Previsão De Início"
+            label="startPredictionRecord"
+          />
+          <InputForm
+            title="Previsão De Fim"
+            id="endPredictionRecordt"
+            name="endPredictionRecordt"
+            placeholder="Previsão De Fim"
+            label="endPredictionRecordt"
+          />
+          <InputForm
+            title="Data Final Inscrição"
+            id="registrationEndDate"
+            name="registrationEndDate"
+            placeholder="Data Final Inscrição"
+            label="registrationEndDate"
+          />
+          <InputForm
+            title="Qtde.Horas"
+            id="recordingNumbeOfHours"
+            name="recordingNumbeOfHours"
+            placeholder="Qtde.Horas"
+            label="recordingNumbeOfHours"
+          />
+          <InputForm
+            title="Total De Vagas"
+            id="recordOfTotalVacancies"
+            name="recordOfTotalVacancies"
+            placeholder="Total De Vagas"
+            label="recordOfTotalVacancies"
+          />
+          <div>
+            <h1 className='py-2'>Concluído</h1>
+          </div>
+          <div className='flex gap-2'>
+            <InputCheckbox checkboxValues={checkboxValues}
+            />
+          </div>
+          <div>
+            <h1 className=' py-2'>Liberado</h1>
+          </div>
+          <div className='flex gap-2'>
+            <InputCheckbox checkboxValues={checkboxValues}
+            />
+          </div>
+        </Form>
+      </Formik>
+    </>
+  )
 }
