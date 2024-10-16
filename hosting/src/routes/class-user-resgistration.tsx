9883159dc09c7282272a475042fa64a0d03a01b8
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-10-15 21:37:32 -0300

    Update class-user-resgistration.tsx
    Adjusting indentation

--- hosting/src/routes/class-user-resgistration.tsx
+++ hosting/src/routes/class-user-resgistration.tsx
@@ -17,72 +17,72 @@ const initialValues = {
 export function formRegistration() {
   return (
     <>
-  <Formik 
+    <Formik 
         onSubmit={() => {}}
         initialValues={initialValues}
         validationSchema={toFormikValidationSchema(changePasswordRequestSchema) }
     >
-    <Form>
+      <Form>
         <div>
             <h1 className='font-semibold'>ALUNO</h1>
         </div>
-           
-      <InputForm
-        title="Turma" 
-        id="tumaRegistration"
-        name="tumaRegistration"
-        placeholder="Nome Da Turma" 
-        label="tumaRegistration"
-        type="text"
-      />
-      <InputForm
-        title="Turno"
-        id="shiftRecord"
-        name="shiftRecord"
-        placeholder="Turno"
-        label="shiftRecord"
-        type="text"
-      />
-      <InputForm
-        title="Previsão De Início"
-        id="startPredictionRecord"
-        name="startPredictionRecord"
-        placeholder="Previsão De Início"
-        label="startPredictionRecord"
-        type="text"
-      />
-      <InputForm
-        title="Previsão De Fim"
-        id="endPredictionRecordt"
-        name="endPredictionRecordt"
-        placeholder="Previsão De Fim"
-        label="endPredictionRecordt"
-        type="text"
-      />
-      <InputForm
-        title="Data Final Inscrição"
-        id="registrationEndDate"
-        name="registrationEndDate"
-        placeholder="Data Final Inscrição"
-        label="registrationEndDate"
-        type="text"
-      />
-      <InputForm
-        title="Qtde.Horas"
-        id="recordingNumbeOfHours"
-        name="recordingNumbeOfHours"
-        placeholder="Qtde.Horas"
-        label="recordingNumbeOfHours"
-        type="text"
-      />
-      <InputForm
-        title="Total De Vagas"
-        id="recordOfTotalVacancies"
-        name="recordOfTotalVacancies"
-        placeholder="Total De Vagas"
-        label="recordOfTotalVacancies"
-        type="text"
-      />
+             
+        <InputForm
+          title="Turma" 
+          id="tumaRegistration"
+          name="tumaRegistration"
+          placeholder="Nome Da Turma" 
+          label="tumaRegistration"
+          type="text"
+        />
+        <InputForm
+          title="Turno"
+          id="shiftRecord"
+          name="shiftRecord"
+          placeholder="Turno"
+          label="shiftRecord"
+          type="text"
+        />
+        <InputForm
+          title="Previsão De Início"
+          id="startPredictionRecord"
+          name="startPredictionRecord"
+          placeholder="Previsão De Início"
+          label="startPredictionRecord"
+          type="text"
+        />
+        <InputForm
+          title="Previsão De Fim"
+          id="endPredictionRecordt"
+          name="endPredictionRecordt"
+          placeholder="Previsão De Fim"
+          label="endPredictionRecordt"
+          type="text"
+        />
+        <InputForm
+          title="Data Final Inscrição"
+          id="registrationEndDate"
+          name="registrationEndDate"
+          placeholder="Data Final Inscrição"
+          label="registrationEndDate"
+          type="text"
+        />
+        <InputForm
+          title="Qtde.Horas"
+          id="recordingNumbeOfHours"
+          name="recordingNumbeOfHours"
+          placeholder="Qtde.Horas"
+          label="recordingNumbeOfHours"
+          type="text"
+        />
+        <InputForm
+          title="Total De Vagas"
+          id="recordOfTotalVacancies"
+          name="recordOfTotalVacancies"
+          placeholder="Total De Vagas"
+          label="recordOfTotalVacancies"
+          type="text"
+        />
     </Form>
   </Formik>
 </>
