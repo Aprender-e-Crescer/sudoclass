3d868ae9f7e166857d418cf65a8cac754958e7bf
enzoguis <enzog0315@gmail.com>
2024-10-17 11:16:44 -0300

    feat(view-activity): adding button and input for notes

--- hosting/src/routes/view-activity.tsx
+++ hosting/src/routes/view-activity.tsx
@@ -3,6 +3,7 @@ import LeftMenu from '@/components/custom/left-menu'
 import ListStudents from '@/components/custom/list-students'
 import { SubHeader } from '@/components/custom/subheader'
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { useStudentsListQuery } from '@/queries/use-students-list-query'
 import { createFileRoute } from '@tanstack/react-router'
 
@@ -17,7 +18,7 @@ function ViewActivity() {
       <Header avatarImage="" avatarFallBack="" logo="" />
       <div className="flex">
         <LeftMenu type="TeacherClassroom" />
-        <div className="flex flex-col w-full">
+        <div className="flex flex-col w-full h-full flex-1">
           <div className="flex items-center justify-between px-4 py-2">
             <SubHeader />
             <Button variant="blueButton" size="small">
@@ -25,11 +26,21 @@ function ViewActivity() {
             </Button>
           </div>
           <hr />
-
-          <div className="px-4 py-2">
-            {students?.map((student) => (
-              <ListStudents key={student.id} name={student.name} picture="" variant="corrected" />
-            ))}
+          <div className="flex">
+            <div className="px-4 py-2">
+              {students?.map((student) => (
+                <ListStudents key={student.id} name={student.name} picture="" variant="corrected" />
+              ))}
+            </div>
+            <div className="flex">
+              <hr />
+            </div>
+            <div className="flex justify-around w-full items-center mx-28">
+              <Input placeholder="Nota" className="w-48" />
+              <Button variant="blueButton" size="small">
+                Devolver
+              </Button>
+            </div>
           </div>
         </div>
       </div>
