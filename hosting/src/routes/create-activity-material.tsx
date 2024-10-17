bb65afdd0e5f91b034601f771c8bb2a3e83724b0
Samuel Molinari <samuelmolinari8@gmail.com>
2024-10-17 19:10:23 -0300

    feat(create-activity-material): basic struct

--- hosting/src/routes/create-activity-material.tsx
+++ hosting/src/routes/create-activity-material.tsx
@@ -1,4 +1,7 @@
+import { Header } from '@/components/custom/header'
 import LeftMenu from '@/components/custom/left-menu'
+import { SubHeader } from '@/components/custom/subheader'
+import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/create-activity-material')({
@@ -8,10 +11,19 @@ export const Route = createFileRoute('/create-activity-material')({
 export function CreateActivityMaterial() {
   return (
     <>
-      <div className="flex ">
-        <LeftMenu type="TeacherClassroom" />
-        <div>
-          <h1>Teste</h1>
+      <div>
+        <Header avatarFallBack="" avatarImage="" />
+        <div className="flex ">
+          <LeftMenu type="TeacherClassroom" />
+          <div className="flex items-center">
+            <div className="border-l border-gray-300 h-full mx-1"></div>
+          </div>
+          <div className="flex w-full h-10 items-center justify-around">
+            <SubHeader />
+            <Button variant="blueButton" size="small">
+              Reunião
+            </Button>
+          </div>
         </div>
       </div>
     </>
