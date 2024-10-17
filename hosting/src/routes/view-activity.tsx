7e72ca41d9741271bba0f6750836d65d65b9771a
enzoguis <enzog0315@gmail.com>
2024-10-17 11:34:43 -0300

    feat(view-activity): initing responsiveness

--- hosting/src/routes/view-activity.tsx
+++ hosting/src/routes/view-activity.tsx
@@ -13,29 +13,28 @@ export const Route = createFileRoute('/view-activity')({
 
 function ViewActivity() {
   const { data: students } = useStudentsListQuery()
+
   return (
     <div>
       <Header avatarImage="" avatarFallBack="" logo="" />
       <div className="flex">
         <LeftMenu type="TeacherClassroom" />
         <div className="flex flex-col w-full h-full flex-1">
-          <div className="flex items-center justify-between px-4 py-2">
+          <div className="flex flex-col md:flex-row items-center justify-between px-4 py-2">
             <SubHeader />
             <Button variant="blueButton" size="small">
               Reunião
             </Button>
           </div>
           <hr />
-          <div className="flex">
+          <div className="flex flex-grow">
             <div className="px-4 py-2">
               {students?.map((student) => (
                 <ListStudents key={student.id} name={student.name} picture="" variant="corrected" />
               ))}
             </div>
-            <div className="flex">
-              <hr />
-            </div>
-            <div className="flex justify-around w-full items-center mx-28">
+            <div className="max-h-screen border border-gray-300"></div>
+            <div className=" hidden lg:flex justify-center w-full items-center gap-x-10">
               <Input placeholder="Nota" className="w-48" />
               <Button variant="blueButton" size="small">
                 Devolver
