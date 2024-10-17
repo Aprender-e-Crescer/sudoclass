f654ca1b9a04c97e2599fc5bb763b22adb8cae23
Pablo Gabirel Bertholdo <pablobertholdo2008@gmail.com>
2024-10-17 19:20:05 -0300

    style(header style): changing button size

--- hosting/src/components/custom/header.tsx
+++ hosting/src/components/custom/header.tsx
@@ -7,12 +7,12 @@ interface headerProps {
 }
 export function Header ({ avatarImage, avatarFallBack}: headerProps) {
     return(
-        <div className='flex justify-between items-center border p-4'>
-            <div className='flex h-24 w-40'>
+        <div className='flex justify-between items-center border px-16 py-4'>
+            <div className='flex h-20 w-36'>
                 <img src= {sudotecLogo} alt="sudotecLogo" />
             </div>
-            <div className='flex h-11 w-11'>
-            <Avatar>
+            <div className='flex '>
+            <Avatar className='flex h-12 w-12'>
                 <img src={avatarImage} />
                 <AvatarFallback>{avatarFallBack}</AvatarFallback>
             </Avatar>
