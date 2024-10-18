5cf837dffa78f1960f81b888eda4d8ee6bbdc4e1
augustofav <augustofavetti159@gmail.com>
2024-10-17 22:30:05 -0300

    feat(list subject): add list subject

--- hosting/src/routes/list-subjects.tsx
+++ hosting/src/routes/list-subjects.tsx
@@ -1,42 +1,39 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
-import { toFormikValidate } from 'zod-formik-adapter'
-import { useListSubjectsQuery } from '@/queries/use-list-subjects-query'
-import { subjectsSchema } from '@/models/subjects-schema'
+import { createFileRoute } from '@tanstack/react-router';
+import { Formik, Form } from 'formik';
+import { useListSubjectsQuery } from '@/queries/use-list-subjects-query';
+import { InputForm } from '@/components/custom/text-input'; 
 
 export const Route = createFileRoute('/list-subjects')({
   component: ListSubjects,
-})
+});
 
 function ListSubjects() {
-  const { data } = useListSubjectsQuery('aQjvxCKlEuHc9YQEedCQ')
-
-  const validate = toFormikValidate(subjectsSchema)
+  const { data } = useListSubjectsQuery('aQjvxCKlEuHc9YQEedCQ');
 
   return (
     <>
       <Formik
         initialValues={{ name: '', description: '' }}
-        validate={validate}
         onSubmit={(values, { resetForm }) => {
-          console.log('valores do formulário:', values)
-          resetForm()
+          console.log('Valores do formulário:', values);
+          resetForm();
         }}
       >
         <Form className="flex items-center justify-center flex-col gap-4 p-6">
-          <div className="flex flex-col w-full max-w-xs">
-            <Field type="text" className="bg-gray-200 rounded-md p-2" placeholder="Nome da matéria" name="name" />
-            <ErrorMessage name="name" component="div" className="text-red-500 text-sm mt-1" />
-          </div>
-          <div className="flex flex-col w-full max-w-xs">
-            <Field
-              type="text"
-              className="bg-gray-200 rounded-md p-2"
-              placeholder="Descrição da matéria"
-              name="description"
-            />
-            <ErrorMessage name="description" component="div" className="text-red-500 text-sm mt-1" />
-          </div>
+          <InputForm
+            title="Nome da matéria"
+            label="name"
+            placeholder="Digite o nome da matéria"
+            id="name"
+            name="name"
+          />
+          <InputForm
+            title="Descrição da matéria"
+            label="description"
+            placeholder="Digite a descrição da matéria"
+            id="description"
+            name="description"
+          />
           <button type="submit" className="mt-4 bg-green-400 text-white rounded-md p-2">
             Enviar
           </button>
@@ -55,7 +52,7 @@ function ListSubjects() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default ListSubjects
+export default ListSubjects;
