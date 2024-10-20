78fe95fb8f44f6ec5dba6cbc696fd0a2091536bc
thaylaalves <thaylavitoriar@gmail.com>
2024-10-20 09:17:02 -0300

    refactor: adding validation

--- hosting/src/routes/add-new-course.tsx
+++ hosting/src/routes/add-new-course.tsx
@@ -1,74 +1,88 @@
 import { InputForm } from '@/components/custom/text-input'
-import { matriceSchema } from '@/models/matrice-schema'
+import { Button } from '@/components/ui/button'
+import { addNewCourseSchema } from '@/models/add-new-course-schema'
+import { useCreateSchoolMatriceMutation } from '@/mutations/use-create-school-matrice-mutation'
 import { createFileRoute } from '@tanstack/react-router'
 import { Formik } from 'formik'
+import { toFormikValidationSchema } from 'zod-formik-adapter'
 
 export const Route = createFileRoute('/add-new-course')({
   component: AddNewCourseForm,
 })
 
-function AddNewCourseForm() {
-  const handleSubmit = () => {}
+const initialValues = {
+  name: '',
+  startDate: '',
+  endDate: '',
+  workload: '',
+  startOfRegistration: '',
+  endOfRegistration: '',
+  numberOfVacancies: '',
+}
 
+function AddNewCourseForm() {
+  const { mutate, isPending, error } = useCreateSchoolMatriceMutation()
+
+  console.log(isPending, error)
+
+  const handleSubmit = (values: typeof initialValues) => mutate(values)
   return (
     <Formik
-      initialValues={{
-        name: '',
-        startDate: '',
-        endDate: '',
-        workload: '',
-        startOfRegistration: '',
-        endOfRegistration: '',
-        numberOfVacancies: '',
-      }}
-      validationSchema={matriceSchema}
+      initialValues={initialValues}
+      validationSchema={toFormikValidationSchema(addNewCourseSchema)}
       onSubmit={handleSubmit}
     >
-      <form>
-        <InputForm title="Nome" label="Nome" name="name" placeholder="Nome" id="name" />
-        <InputForm
-          title="Data de inicio"
-          label="Data de inicio"
-          name="startDate"
-          placeholder="00/00/0000"
-          id="startDate"
-        />
-        <InputForm
-          title="Data de termino"
-          label="Data de termino"
-          name="endDate"
-          placeholder="00/00/0000"
-          id="endDate"
-        />
-        <InputForm
-          title="Carga horaria do curso"
-          label="Carga horaria do curso"
-          name="workload"
-          placeholder="500hrs"
-          id="workload"
-        />
-        <InputForm
-          title="Inicio das inscrições"
-          label="Inicio das inscrições"
-          name="startOfRegistration"
-          placeholder="00/00/0000"
-          id="startOfRegistration"
-        />
-        <InputForm
-          title="Termino das inscrições"
-          label="Termino das inscrições"
-          name="endOfRegistration"
-          placeholder="Termino das inscrições"
-          id="endOfRegistration"
-        />
-        <InputForm
-          title="Numero de vagas"
-          label="Numero de vagas"
-          name="numberOfVacancies"
-          placeholder="30"
-          id="numberOfVacancies"
-        />
-      </form>
+      {({ handleSubmit, values }) => {
+        console.log(values)
+        return (
+          <form onSubmit={handleSubmit}>
+            <InputForm title="Nome" label="Nome" name="name" placeholder="Nome" id="name" />
+            <InputForm
+              title="Data de inicio"
+              label="Data de inicio"
+              name="startDate"
+              placeholder="00/00/0000"
+              id="startDate"
+            />
+            <InputForm
+              title="Data de termino"
+              label="Data de termino"
+              name="endDate"
+              placeholder="00/00/0000"
+              id="endDate"
+            />
+            <InputForm
+              title="Carga horaria do curso"
+              label="Carga horaria do curso"
+              name="workload"
+              placeholder="500hrs"
+              id="workload"
+            />
+            <InputForm
+              title="Inicio das inscrições"
+              label="Inicio das inscrições"
+              name="startOfRegistration"
+              placeholder="00/00/0000"
+              id="startOfRegistration"
+            />
+            <InputForm
+              title="Termino das inscrições"
+              label="Termino das inscrições"
+              name="endOfRegistration"
+              placeholder="Termino das inscrições"
+              id="endOfRegistration"
+            />
+            <InputForm
+              title="Numero de vagas"
+              label="Numero de vagas"
+              name="numberOfVacancies"
+              placeholder="30"
+              id="numberOfVacancies"
+            />
+            <Button type="submit">Adicionar curso</Button>
+          </form>
+        )
+      }}
     </Formik>
   )
 }
