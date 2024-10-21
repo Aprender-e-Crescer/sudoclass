e71589fbfa76907fa63343d5587ac0c9c905146f
Kaua Cantelli <cantellidumbo@gmail.com>
2024-10-21 18:59:18 -0300

    docs(notes-screen): modifying and finalizing the notes screen and notes table

--- hosting/src/components/custom/notes-table.tsx
+++ hosting/src/components/custom/notes-table.tsx
@@ -1,18 +1,41 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import Avatarimg from '@/assets/avatar.png';
+
 interface Props {
-  name: string
-  rating: string
-  overallRating: string
+  name?: string;
+  rating?: string;
+  overallRating?: string;
+  otherNotes?: string;
+  avatar?: string;
 }
 
-export function NotesTable({name, rating, overallRating}: Props): void {
-  <div className="flex h-screen">    
-        <tbody>
-          <tr>
-            <td className="border border-[#DBDBDB] px-20 py-6">{name}</td>
-            <td className="border border-[#DBDBDB] w-52 px-20 py-6">{rating}</td>
-            <td className="border border-[#DBDBDB] w-52 px-20 py-6">{overallRating}</td>
-          </tr>
-        </tbody>
-  </div>      
-
+export function NotesTable({ name, rating, overallRating, avatar, otherNotes }: Props) {
+  return (
+    <div className="flex">
+      <div className="flex items-center  border border-[#DBDBDB] gap-12 w-72">
+       <div className="ml-5"> {avatar && (
+          <Avatar>
+            <AvatarImage src={avatar || Avatarimg} alt={name} />
+            <AvatarFallback></AvatarFallback>
+          </Avatar>
+        )}
+        </div>
+        <div className="flex justify-center items-center">
+        <span>{name}</span>
+        </div>
+      </div>
+      <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24 text-[#292D32] text-opacity-70">
+        {rating}
+      </div>
+      <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24 text-[#292D32] text-opacity-70">
+        {overallRating}
+      </div>
+      <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24">
+        {otherNotes}
+      </div>
+      <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24">
+        {otherNotes}
+      </div>
+    </div>
+  );
 }
--- hosting/src/routes/notes-screen.tsx
+++ hosting/src/routes/notes-screen.tsx
@@ -1,19 +1,102 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { HeaderNotes } from '@/components/custom/header-notes'
+import { NotesTable } from '@/components/custom/notes-table'
+import { Avatar }  from '@/components/ui/avatar';
+import Avatarimg from '@/assets/avatar.png';
+
 
 export const Route = createFileRoute('/notes-screen')({
-  component: () =>
+  component: () => {
+    return (
+    <>
+      <div>
+        <Avatar/>
+      </div>
 
+      <div className="flex flex-col md:flex-row items-start">
+        <div className="border border-[#DBDBDB] w-72 h-24 flex flex-col items-center">
+        </div>
+          <HeaderNotes
+            subtitle="sem data de entrega"
+            title="Atividade 1"
+            totalNote="De 100"
+          />
+          <HeaderNotes
+            subtitle="Para cada aluno"
+            title="Média Geral"
+            totalNote="De 100"
+          />
+          <div className="border border-[#DBDBDB] w-52 h-24 flex flex-col items-center">
+          </div>
+          <div className="border border-[#DBDBDB] w-52 h-24 flex flex-col items-center">
+          </div>
 
-<div className="flex">
-    <HeaderNotes
-    subtitle='sem data de entrega'
-    title='Atividade 1'
-    totalNote='De 100'/>
-    <HeaderNotes
-    subtitle='Para cada aluno'
-    title='Média Geral'
-    totalNote='De 100'/>
-</div>
-    
-})
+        </div>
+
+        <div className="flex flex-col md:flex-col font-medium">
+          <NotesTable 
+          />
+        <div className='flex items-center'>  
+          <NotesTable
+          name='Média Geral'
+          />
+        </div>
+          <NotesTable
+            name="Ronald Richards"
+            overallRating="_/100"
+            rating="_/100"
+            avatar={Avatarimg}
+          />
+          <NotesTable
+            name="Marvin McKinney"
+            overallRating="_/100"
+            rating="_/100"
+            avatar={Avatarimg}
+          />
+          <NotesTable
+            name="Jerome Bell"
+            overallRating="_/100"
+            rating="_/100"
+            avatar={Avatarimg}
+          />
+          <NotesTable
+            name="Kathryn Murphy"
+            overallRating="_/100"
+            rating="_/100"
+            avatar={Avatarimg}
+          />
+          <NotesTable
+            name="Jacob Jones"
+            overallRating="_/100"
+            rating="_/100"
+            avatar={Avatarimg}
+          />
+          <NotesTable
+            name="Kristin Watson"
+            overallRating="_/100"
+            rating="_/100"
+            avatar={Avatarimg}
+          />
+          <NotesTable
+            name="Kristin Watson"
+            overallRating="_/100"
+            rating="_/100"
+            avatar={Avatarimg}
+          />
+          <NotesTable
+            name="Kristin Watson"
+            overallRating="_/100"
+            rating="_/100"
+            avatar={Avatarimg}
+          />
+          <NotesTable
+            name="Kristin Watson"
+            overallRating="_/100"
+            rating="_/100"
+            avatar={Avatarimg}
+          />
+        </div>
+    </>
+    );
+  }
+});
