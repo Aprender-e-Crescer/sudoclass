3f55c8d972a8e0df29e03c49b752d8e732467724
João <joaopedromoraesbonkoski@gmail.com>
2024-10-20 21:40:04 -0300

    fix(request-sent): size corrections

--- hosting/src/routes/request-sent.tsx
+++ hosting/src/routes/request-sent.tsx
@@ -1,30 +1,29 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Button } from '@/components/ui/button'
+import { createFileRoute } from '@tanstack/react-router';
+import { Button } from '@/components/ui/button';
 
 export const Route = createFileRoute('/request-sent')({
   component: RequestSent,
-})
+});
 
 export default function RequestSent() {
   return (
-    <div className="flex flex-col lg:flex-row w-full max-w-6xl h-full md:h-[600px] bg-[#1A73E8] shadow-lg rounded-[27px] mx-4 lg:mx-0">
-      <div className="flex justify-center">
+    <div className="flex flex-col lg:flex-row w-full max-w-6xl h-full md:h-[600px] bg-[#1A73E8] shadow-lg rounded-[27px] mb-4 lg:mx-0">
+      <div className="flex justify-center items-center flex-grow">
         <img
           src="https://i.imgur.com/H0PBQy2.png"
-          alt="foi n"
-          className="w-[500px] h-auto lg:w-[577px] lg:rounded-r-[27px]"
+          alt="imagem"
+          className="w-[500px] h-auto lg:w-[577px] lg:rounded-r-[27px] md:h-[500px]"
         />
       </div>
-      <div className="flex flex-col justify-center items-center border w-full lg:w-[697px] bg-[#FFFFFF] lg:rounded-r-[27px] p-4">
+      <div className="flex flex-col justify-center items-center border w-full lg:w-[697px] bg-[#FFFFFF] lg:rounded-r-[27px]">
         <h1 className="text-[40px] text-[#0C408F] font-bold">
           Solicitação Enviada!
         </h1>
-        <p className="text-[26px] text-[#1A73E8] font-semibold text-center mt-4 px-4">
-          Sua solicitação foi enviada com sucesso. Espere o retorno da
-          coordenação.
+        <p className="text-[26px] text-[#1A73E8] font-semibold text-center p-10">
+          Sua solicitação foi enviada com sucesso. Espere o retorno da coordenação.
         </p>
         <Button
-          className="w-full max-w-[400px] h-[67px] mt-[20px] flex justify-center px-4"
+          className="w-full max-w-[400px] h-[67px] mt-[20px] flex justify-center"
           variant="blueButton"
           size="large"
         >
@@ -32,5 +31,5 @@ export default function RequestSent() {
         </Button>
       </div>
     </div>
-  )
+  );
 }
