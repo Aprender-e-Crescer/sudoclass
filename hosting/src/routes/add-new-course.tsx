373f4624b52c12550d7ecc07008656408e1e7cca
thaylaalves <thaylavitoriar@gmail.com>
2024-10-21 18:41:53 -0300

    refactor(add-new-course): styling the screen

--- hosting/src/routes/add-new-course.tsx
+++ hosting/src/routes/add-new-course.tsx
@@ -36,51 +36,55 @@ function AddNewCourseForm() {
         console.log(values)
         return (
           <form onSubmit={handleSubmit}>
-            <InputForm title="Nome" label="Nome" name="name" placeholder="Nome" id="name" />
-            <InputForm
-              title="Data de inicio"
-              label="Data de inicio"
-              name="startDate"
-              placeholder="00/00/0000"
-              id="startDate"
-            />
-            <InputForm
-              title="Data de termino"
-              label="Data de termino"
-              name="endDate"
-              placeholder="00/00/0000"
-              id="endDate"
-            />
-            <InputForm
-              title="Carga horaria do curso"
-              label="Carga horaria do curso"
-              name="workload"
-              placeholder="500hrs"
-              id="workload"
-            />
-            <InputForm
-              title="Inicio das inscrições"
-              label="Inicio das inscrições"
-              name="startOfRegistration"
-              placeholder="00/00/0000"
-              id="startOfRegistration"
-            />
-            <InputForm
-              title="Termino das inscrições"
-              label="Termino das inscrições"
-              name="endOfRegistration"
-              placeholder="00/00/0000"
-              id="endOfRegistration"
-            />
-            <InputForm
-              title="Numero de vagas"
-              label="Numero de vagas"
-              name="numberOfVacancies"
-              placeholder="30"
-              id="numberOfVacancies"
-            />
-            <Button variant="lightTextBlack">Cancelar</Button>
-            <Button variant="blueButton">Criar</Button>
+            <div className="p-5">
+              <InputForm title="Nome" label="Nome" name="name" placeholder="Nome" id="name" />
+              <InputForm
+                title="Data de inicio"
+                label="Data de inicio"
+                name="startDate"
+                placeholder="00/00/0000"
+                id="startDate"
+              />
+              <InputForm
+                title="Data de termino"
+                label="Data de termino"
+                name="endDate"
+                placeholder="00/00/0000"
+                id="endDate"
+              />
+              <InputForm
+                title="Carga horaria do curso"
+                label="Carga horaria do curso"
+                name="workload"
+                placeholder="500hrs"
+                id="workload"
+              />
+              <InputForm
+                title="Inicio das inscrições"
+                label="Inicio das inscrições"
+                name="startOfRegistration"
+                placeholder="00/00/0000"
+                id="startOfRegistration"
+              />
+              <InputForm
+                title="Termino das inscrições"
+                label="Termino das inscrições"
+                name="endOfRegistration"
+                placeholder="00/00/0000"
+                id="endOfRegistration"
+              />
+              <InputForm
+                title="Numero de vagas"
+                label="Numero de vagas"
+                name="numberOfVacancies"
+                placeholder="30"
+                id="numberOfVacancies"
+              />
+            </div>
+            <div className="flex items-center justify-center mt-5">
+              <Button variant="lightTextBlack">Cancelar</Button>
+              <Button variant="blueButton">Criar</Button>
+            </div>
           </form>
         )
       }}
