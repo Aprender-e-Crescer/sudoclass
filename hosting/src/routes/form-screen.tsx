019178543ddb2523e883fd9f83ac4e5dc03c80d1
Jonatas Fiorentin <jonatasfiorentin@gmail.com>
2024-10-21 22:30:39 -0300

    fix(form-screen): removing component parentheses

--- hosting/src/routes/form-screen.tsx
+++ hosting/src/routes/form-screen.tsx
@@ -3,7 +3,7 @@ import { CardFormStars } from '@/components/custom/card-form-stars'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/form-screen')({
-  component: FormScreen(),
+  component: FormScreen,
 })
 
 export function FormScreen() {
