ba1f9cc7233073769065d1de9f8ae3c702e30dd0
Jonatas Fiorentin <jonatasfiorentin@gmail.com>
2024-10-21 21:10:11 -0300

    fix(get-input-schema): fix name

--- hosting/src/routes/student-documents.tsx
+++ hosting/src/routes/student-documents.tsx
@@ -1,6 +1,6 @@
 import { GenericTable } from '@/components/custom/generic-table'
 import { InputWithoutLabel } from '@/components/custom/without-label-input'
-import { getInputSchema } from '@/models/get-input=shcema'
+import { getInputSchema } from '@/models/get-input-schema'
 import { createFileRoute } from '@tanstack/react-router'
 import { Form, Formik } from 'formik'
 import { Download, Search } from 'lucide-react'
