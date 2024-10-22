f191001faae0564facbfb1f4a0ab3f06c2d62cbe
Raul <rauldondonimachado09@gmail.com>
2024-10-22 19:39:16 -0300

    feat(password-change-request.tsx): putting alert dialog in screen

--- hosting/src/routes/password-change-request.tsx
+++ hosting/src/routes/password-change-request.tsx
@@ -1,6 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { SquareCheck } from 'lucide-react'
+import { SquareCheck, Copy } from 'lucide-react'
 import { X } from 'lucide-react'
+import { Check } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { AlertDialog, AlertDialogHeader, AlertDialogContent, AlertDialogTrigger, AlertDialogTitle, AlertDialogDescription } from '@/components/ui/alert-dialog'
+
 export const Route = createFileRoute('/password-change-request')({
   component: requestChangePassword,
 })
@@ -8,17 +12,32 @@ export const Route = createFileRoute('/password-change-request')({
 export function requestChangePassword() {
   return (
   <>
-    <div className='w-11/12 p-3'>  {/* tirar padding quando terminar*/}
+    <div className='w-10/12 p-3'>  {/* tirar padding quando terminar*/}
     
-    <div className='rounded-md border-2 flex p-3'>
+    <div className='rounded-md border-2 flex p-3 items-center'>
+
         <div>
-        <h1 className='font-bold'>Ronaldinho Cleber Machado</h1>
-        <p className='text-gray-300'>Curso: Aprender e crescer</p>
+          <h1 className='font-bold font-[inter]'>Ronaldinho Cleber Machado</h1>
+          <p className='text-gray-300 font-[inter]'>Curso: Aprender e crescer</p>
         </div>
 
-        <div className='border'>
-        <X className='text-red-700'/>
-        </div>
+      <div className='gap-3 flex ml-auto'>
+        <AlertDialog>
+          <AlertDialogTrigger>
+              <div className='flex border h-8 rounded-md justify-center items-center p-1'>
+                <Check className='text-green-500 '/>
+              </div>
+            </AlertDialogTrigger>
+            <AlertDialogContent className='p-10  h-56'>
+              <AlertDialogTitle className='text-2xl'>Nova senha - Ronaldinho Cleber...</AlertDialogTitle>
+            <Button iconPosition='right' variant='ghostBlack' icon={<Copy/>} className='w-full p-6 text-xl border-2 border-black'> rOnaldinho22</Button>
+            </AlertDialogContent>
+          </AlertDialog>
+          <div className='flex border h-8 rounded-md justify-center items-center p-1 '>
+          <X className='text-red-700'/>
+          </div>
+      
+      </div>
     </div>
 
 
