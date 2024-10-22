7746aedf0b5632533f3733b3e63070bacc4d7471
JoaoPMBsp <joaopedromoraesbonkoski@gmail.com>
2024-10-21 21:50:41 -0300

    fix(attachment-view): corrected responsiveness

--- hosting/src/components/custom/attachment-view.tsx
+++ hosting/src/components/custom/attachment-view.tsx
@@ -10,20 +10,17 @@ interface PropsAttachment {
 
 export function AttachmentView({ url, imageUrl, title, linkText }: PropsAttachment) {
   return (
-    <div className="border border-gray-300 rounded-lg flex items-center justify-between p-4 w-full max-w-[379.71px] h-auto sm:h-[78.14px]">
-      <Link to={url} className="flex items-center gap-2 flex-1 sm:flex-none mb-4 sm:mb-0 hover:underline">
-        <div className="w-[105px] h-[70px] sm:w-[105px] sm:h-[70px] mb-2 sm:mb-0">
-          <img src={imageUrl} alt="Imagem do anexo" className="w-full h-full object-cover rounded" />
-        </div>
+    <div className="border border-gray-300 rounded-lg flex p-4 max-w-[380px] w-full h-[78.14px]">
+      <Link to={url} className="flex items-center gap-2 hover:underline">
+          <img src={imageUrl} alt="Imagem do anexo" className='w-[105px] h-[70px]'/>
         <div>
-          <h1 className="text-[18px] sm:text-[23px]">{title}</h1>
-          <Link to={url} className="hover:underline text-xs sm:text-sm">
+          <h1 className="text-[16px] sm:text-[18px] md:text-[20px] lg:text-[23px]">{title}</h1>
+          <Link to={url} className="text-[12px]">
             {linkText}
           </Link>
         </div>
       </Link>
-
-      <button className="ml-4 p-2 sm:p-0 flex items-center justify-center hover:bg-gray-200 rounded-full">
+      <button className="ml-4 p-2 flex items-center justify-center hover:bg-gray-200 rounded-full">
         <IoMdClose className="w-6 h-6 text-gray-700" />
       </button>
     </div>
