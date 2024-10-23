8189038b7e7123ea4f37c69b9c5ed338a59d9285
Pablo Gabirel Bertholdo <pablobertholdo2008@gmail.com>
2024-10-23 16:55:34 -0300

    style(header-mobile): mobile header made

--- hosting/src/components/custom/header.tsx
+++ hosting/src/components/custom/header.tsx
@@ -1,25 +1,42 @@
 import sudotecLogo from '@/assets/sudotecLogo.svg'
 import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+import  { Menu } from 'lucide-react'
 
 interface headerProps {
-    avatarFallBack: string
-    avatarImage: string
+    avatarFallBack?: string
+    avatarImage?: string
 }
 export function Header ({ avatarImage, avatarFallBack}: headerProps) {
     return(
-        <div className='flex justify-between items-center border px-16 py-4'>
+        <>
+        <div className=' hidden sm:flex justify-between items-center border px-16 py-4 w-full'>
             <div className='flex h-20 w-36'>
                 <img src= {sudotecLogo} alt="sudotecLogo" />
             </div>
             <div className='flex '>
-            <Avatar className='flex h-12 w-12'>
+            <Avatar className='flex h-12 w-12 '>
                 <img src={avatarImage} />
                 <AvatarFallback>{avatarFallBack}</AvatarFallback>
             </Avatar>
 
             </div>
         </div>
-        
-    )   
 
+        <div className='flex justify-between items-center border px-4 py-1 w-full sm:hidden'>
+            <div>
+                <Menu/>
+            </div>
+            <div className='flex h-20 w-36'>
+                <img src= {sudotecLogo} alt="sudotecLogo" />
+            </div>
+            <div className='flex '>
+            <Avatar className='flex h-12 w-12 '>
+                <img src={avatarImage} />
+                <AvatarFallback>{avatarFallBack}</AvatarFallback>
+            </Avatar>
+
+            </div>
+        </div>
+        </>
+    )   
 }
