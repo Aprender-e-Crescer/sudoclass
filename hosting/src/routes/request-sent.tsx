c02210dbf7b41b553d8a3f219fd9e3d2cda07588
JoaoPMBsp <joaopedromoraesbonkoski@gmail.com>
2024-10-22 22:28:47 -0300

    fix(request-sent and image-request-sent): created the image file and corrections in the request-sent

--- hosting/src/routes/request-sent.tsx
+++ hosting/src/routes/request-sent.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Button } from '@/components/ui/button';
+import image from '@/assets/image-request-sent.png';
 
 export const Route = createFileRoute('/request-sent')({
   component: RequestSent,
@@ -7,32 +8,34 @@ export const Route = createFileRoute('/request-sent')({
 
 export default function RequestSent() {
   return (
-    <div className='flex justify-center'>
-    <div className="flex flex-col lg:flex-row w-full max-w-6xl h-full md:h-[600px] bg-[#1A73E8] shadow-lg rounded-[27px]">
-      <div className="flex justify-center items-center flex-grow">
+    <div className='flex items-center lg:m-5 m-2 flex-col lg:flex-row'>
+      {/* Container da Imagem */}
+      <div className="flex justify-center h-[250px] w-full lg:h-full lg:w-1/2 border bg-[#1A73E8] rounded-l-[27px] shadow-lg">
         <img
-          src="https://i.imgur.com/H0PBQy2.png"
+          src={image}
           alt="imagem"
-          className="w-[500px] h-auto lg:w-[577px] lg:rounded-r-[27px] md:h-[500px]"
+          className="w-[285px] h-[285px] lg:w-[641px] lg:h-[664px]"
         />
       </div>
-      <div className="flex-col text-center border w-full lg:w-[697px] bg-[#FFFFFF] lg:rounded-r-[27px]">
-        <h1 className="text-[40px] text-[#0C408F] font-bold mt-[100px]">
+
+      {/* Container do Texto */}
+      <div className="flex items-center flex-col lg:h-[664px] w-full lg:w-[1000px] border font-bold rounded-r-[27px] pb-5 shadow-lg">
+        <h1 className="text-[#0C408F] lg:text-[50px] text-[40px] p-4 lg:p-20">
           Solicitação Enviada!
         </h1>
-        <p className="text-[26px] text-[#1A73E8] font-semibold p-10 mt-[90px]">
-          Sua solicitação foi enviada com sucesso. Espere o retorno da
-          coordenação.
+
+        <p className="text-[#1A73E8] lg:text-[20px] text-[17px] p-4 lg:p-20">
+          Sua solicitação foi enviada com sucesso. Espere o retorno da coordenação.
         </p>
+
         <Button
-          className="w-full max-w-[400px] h-[67px] mt-[20px]"
+          className="flex justify-center w-full max-w-[500px]"
           variant="blueButton"
-          size="large"
+          size='large'
         >
           Retornar ao menu
         </Button>
       </div>
-    </div>
     </div>
   );
 }
