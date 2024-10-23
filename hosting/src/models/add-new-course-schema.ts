0d43806e01efc199b12fbe0e2d13d308cfcaa54d
thaylaalves <thaylavitoriar@gmail.com>
2024-10-22 21:50:52 -0300

    refactor(add-new-couse-schema): adjusting vakidation fields

--- hosting/src/models/add-new-course-schema.ts
+++ hosting/src/models/add-new-course-schema.ts
@@ -2,18 +2,12 @@ import { z } from 'zod'
 
 export const addNewCourseSchema = z.object({
   name: z.string({ required_error: `Insira um nome válido` }).min(1, { message: 'Insira um valor válido' }),
-  startDate: z.string({ required_error: `Insira uma data inicial` }).min(1, { message: 'Insira um valor válido' }),
-  endDate: z.string({ required_error: `Insira uma data de termino` }).min(1, { message: 'Insira um valor válido' }),
+  startDate: z.date({ message: `Insira uma data válida` }),
+  endDate: z.date({ message: `Insira uma data válida` }),
   workload: z
-    .number({ required_error: `Insira uma carga horaria total do curso` })
+    .number({ message: `Insira uma carga horaria total do curso` })
     .min(1, { message: 'Insira um valor válido' }),
-  numberOfVacancies: z
-    .number({ required_error: `Insira um número de vagas` })
-    .min(1, { message: 'Insira um valor válido' }),
-  endOfRegistration: z
-    .string({ required_error: `Insira uma data final para o curso` })
-    .min(1, { message: 'Insira um valor válido' }),
-  startOfRegistration: z
-    .string({ required_error: `Insira uma data inicial de inscrições` })
-    .min(1, { message: 'Insira um valor válido' }),
+  numberOfVacancies: z.number({ message: `Insira um número de vagas` }).min(1, { message: 'Insira um valor válido' }),
+  endOfRegistration: z.date({ message: `Insira uma data válida` }),
+  startOfRegistration: z.date({ message: `Insira uma data válida` }),
 })
