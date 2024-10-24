5dd66c20312f4b6aa373870db97ea9533b1ad13d
SoaresDv <viniciussoaresdasilva233@gmail.com>
2024-10-23 22:28:20 -0300

    fix(password-reset): adjusting the layout and spacing of the password reset component

--- hosting/src/routes/password-reset.tsx
+++ hosting/src/routes/password-reset.tsx
@@ -20,33 +20,33 @@ function PasswordReset() {
             >
                 {() => (
                     <div
-                        className="flex flex-col sm:px-0 justify-center min-h-screen items-center w-full h-screen pt-16 px-2"
+                        className="flex flex-col justify-center items-center w-full min-h-screen overflow-y-auto px-4 sm:px-8"
                         style={{ fontFamily: 'Poppins, sans-serif' }}
                     >
                         <div
-                            className="flex flex-col md:flex-row w-full max-w-6xl h-full md:h-[600px] rounded-xl shadow-lg overflow-hidden"
+                            className="flex flex-col md:flex-row w-full max-w-6xl h-auto md:h-[600px] rounded-xl shadow-lg overflow-hidden"
                             style={{ backgroundColor: 'white' }}
                         >
                             <div
-                                className="w-full md:w-1/2 flex justify-center items-center bg-blue-600 md:h-auto h-[300px] sm:h-[400px]"
+                                className="w-full md:w-1/2 flex justify-center items-center bg-blue-600 h-[250px] sm:h-[350px] md:h-auto"
                             >
                                 <img
                                     src={passwordResetImage}
                                     alt="Imagem de redefinição de senha"
-                                    className="w-[200px] h-[200px] md:w-[400px] md:h-[400px]"
+                                    className="w-[200px] h-[200px] sm:w-[285px] sm:h-[285px] md:w-[500px] md:h-[500px]"
                                 />
                             </div>
 
-                            <div className="w-full md:w-1/2 p-8 flex flex-col justify-between">
+                            <div className="w-full md:w-1/2 p-6 sm:p-8 flex flex-col justify-between">
                                 <h1
-                                    className="text-[24px] md:text-[28px] font-extrabold mb-2 text-center"
+                                    className="text-[22px] sm:text-[24px] md:text-[28px] font-extrabold mb-2 text-center"
                                     style={{ color: 'rgba(12, 64, 143, 1)' }}
                                 >
                                     Redefinição de Senha
                                 </h1>
 
                                 <div className="mt-4">
-                                    <p className="text-[16px] md:text-[18px] text-blue-600 text-left ml-9 mb-4">
+                                    <p className="text-[14px] sm:text-[16px] md:text-[18px] text-blue-600 text-left mb-4 ml-5">
                                         Informe seu CPF para a redefinição de senha
                                     </p>
 
@@ -64,23 +64,22 @@ function PasswordReset() {
                                     </div>
                                 </div>
 
-                                <div className="flex flex-col justify-center mt-4 w-full gap-3 px-4 sm:px-0">
+                                <div className="mt-6 w-full space-y-2 px-4">
                                     <Button
                                         variant="blueButton"
                                         size="Login"
-                                        className="w-full sm:w-auto py-3 px-6 text-[14px] sm:text-[16px]"
+                                        className="w-full py-2 sm:py-3 text-sm sm:text-base"
                                     >
                                         Solicitar redefinição
                                     </Button>
                                     <Button
                                         variant="blueButton"
                                         size="Login"
-                                        className="w-full sm:w-auto py-3 px-6 text-[14px] sm:text-[16px]"
+                                        className="w-full py-2 sm:py-3 text-sm sm:text-base"
                                     >
                                         Retornar
                                     </Button>
                                 </div>
-
                             </div>
                         </div>
                     </div>
