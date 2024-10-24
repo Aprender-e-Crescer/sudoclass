5175c01c318e43eaaea34115be3552ea655f64f2
JoaoPMBsp <joaopedromoraesbonkoski@gmail.com>
2024-10-23 21:21:00 -0300

    fix(request-sent): corrected the proportions

--- hosting/src/routes/request-sent.tsx
+++ hosting/src/routes/request-sent.tsx
@@ -8,9 +8,8 @@ export const Route = createFileRoute('/request-sent')({
 
 export default function RequestSent() {
   return (
-    <div className='flex items-center lg:m-5 m-2 flex-col lg:flex-row'>
-      {/* Container da Imagem */}
-      <div className="flex justify-center h-[250px] w-full lg:h-full lg:w-1/2 border bg-[#1A73E8] rounded-l-[27px] shadow-lg">
+    <div className='flex items-center justify-center lg:m-5 m-2 flex-col lg:flex-row'>
+      <div className="flex justify-center h-[250px] w-full lg:h-full lg:w-[800px] border bg-[#1A73E8] rounded-l-[27px] shadow-lg">
         <img
           src={image}
           alt="imagem"
@@ -18,13 +17,12 @@ export default function RequestSent() {
         />
       </div>
 
-      {/* Container do Texto */}
-      <div className="flex items-center flex-col lg:h-[664px] w-full lg:w-[1000px] border font-bold rounded-r-[27px] pb-5 shadow-lg">
-        <h1 className="text-[#0C408F] lg:text-[50px] text-[40px] p-4 lg:p-20">
+      <div className="flex items-center flex-col lg:h-[664px] w-full lg:w-[800px] border font-bold rounded-r-[27px] pb-5 shadow-lg">
+        <h1 className="text-[#0C408F] lg:text-[50px] text-[40px] p-8 lg:p-20">
           Solicitação Enviada!
         </h1>
 
-        <p className="text-[#1A73E8] lg:text-[20px] text-[17px] p-4 lg:p-20">
+        <p className="text-[#1A73E8] lg:text-[20px] text-[17px] p-5 lg:p-10 lg:mt-[100px] lg:w-[480px]">
           Sua solicitação foi enviada com sucesso. Espere o retorno da coordenação.
         </p>
 
