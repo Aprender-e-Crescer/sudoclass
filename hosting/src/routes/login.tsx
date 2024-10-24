c2031b74912c5e46401c48c9da72cf57d24f83f2
Yuosssef <yuossefpolidorio@gmial.com>
2024-10-23 22:35:57 -0300

    fix(login): corrected formik

--- hosting/src/routes/login.tsx
+++ hosting/src/routes/login.tsx
@@ -1,5 +1,5 @@
-import { Formik } from 'formik';
-import login from '@/assets/login.png';
+import { Formik, Form } from 'formik';
+import loginImage from '@/assets/login.png';
 import { AiOutlineIdcard } from 'react-icons/ai';
 import { InputAuth } from '@/components/custom/auth-input';
 import { FaKey } from 'react-icons/fa6';
@@ -7,48 +7,47 @@ import { InputCheckbox } from '@/components/custom/checkbox-input';
 import { Link } from '@tanstack/react-router';
 import { Button } from '@/components/ui/button';
 import { createFileRoute } from '@tanstack/react-router';
-import * as Yup from 'yup';
 import { useLoginController } from '@/controllers/use-login-controller'; // Importa o controlador
+import  { loginSchema } from '@/models/login-schema';
+import { toFormikValidationSchema } from 'zod-formik-adapter';
 
 export const Route = createFileRoute('/login')({
   component: Login,
 });
 
-export function Login() {p
-  const checkboxOptions = [
-    {
-      value: 'Lembre-me',
-      label: 'Lembre-me',
-    },
-  ];
+const checkboxOptions = [
+  {
+    value: 'Lembre-me',
+    label: 'Lembre-me',
+  },
+];
 
-  const initialValues = {
-    cpf: '',
-    password: '',
+
+
+const initialValues = {
+  cpf: '',
+  password: '',
+};
+
+
+function useLogic() {
+  const { login } = useLoginController();
+
+  return {
+    login,
   };
+}
 
-  const userSchema = Yup.object().shape({
-    cpf: Yup.string()
-      .matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, 'CPF inválido!')
-      .required('Obrigatório'),
-    password: Yup.string().min(6, 'Senha muito curta!').required('Obrigatório'),
-  });
-
-  const { handleLoginWithEmail, handleLoginAnonymously } = useLoginController(); // Chama o controlador
-
-  const handleOnSubmitForm = async (values: { cpf: string; password: string }) => {
-    console.log('Dados do formulário:', values);
-    await handleLoginWithEmail(values.cpf, values.password); // Chama a função de login com os valores do formulário
-  };
+export function Login() {
+  const { login } = useLogic();
 
   return (
     <Formik
       initialValues={initialValues}
-      validationSchema={userSchema}
-      onSubmit={handleOnSubmitForm}
+      validationSchema={toFormikValidationSchema(loginSchema)}
+      onSubmit={() => login()}
     >
-      {({ handleSubmit }) => (
-        <form onSubmit={handleSubmit}> {/* Adiciona o evento de submissão */}
+        <Form>
           <div>
             <div className="flex flex-col justify-center items-center mb-24 min-h-screen mt-20 font-poppins px-4 sm:px-0">
               <div className="w-full text-center md:hidden mb-4">
@@ -59,7 +58,7 @@ export function Login() {p
               <div className="flex w-full max-w-6xl h-full md:h-[600px] rounded-xl shadow-lg overflow-hidden bg-white flex-col md:flex-row mb-36">
                 <div className="w-full flex justify-center items-center bg-blue-600 h-[250px] md:h-full md:w-1/2">
                   <img
-                    src={login}
+                    src={loginImage}
                     alt="Login do usuário"
                     className="w-[285px] h-[285px] md:w-[500px] md:h-[500px]"
                   />
@@ -95,24 +94,16 @@ export function Login() {p
                       </div>
                     </div>
 
-                    <div className="text-blue-600 flex justify-between items-center gap-4 mr-4 ml-4">
+                    <div className="text-blue-600 flex justify-between items-center gap-4 -mt-8 mr-5 ml-4">
                       <InputCheckbox checkboxValues={checkboxOptions} />
                       <Link to="/" className="text-blue-600 underline text-sm">
                         Esqueceu sua Senha?
                       </Link>
                     </div>
 
-                    <div className="flex justify-between mt-4 w-full">
+                    <div className="flex justify-between mt-16 ml-4 w-full">
                       <Button variant="blueButton" size="Login" type="submit">
                         Login
-                      </Button>
-                      <Button
-                        variant="blueButton"
-                        size="Login"
-                        type="button" // Tipo de botão para não enviar o formulário
-                        onClick={handleLoginAnonymously} // Chamando a função de login anônimo
-                      >
-                        Login Anônimo
                       </Button>
                     </div>
                   </div>
@@ -120,8 +111,7 @@ export function Login() {p
               </div>
             </div>
           </div>
-        </form>
-      )}
+        </Form>
     </Formik>
   );
 }
