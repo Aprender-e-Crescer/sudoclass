ee808f48e5caa957aea4a99013f85a9b4f4e3de1
enzoguis <enzog0315@gmail.com>
2024-10-23 23:27:08 -0300

    feat(input-note-schema): adding min and max to the input-note

--- hosting/src/models/input-note-schema.ts
+++ hosting/src/models/input-note-schema.ts
@@ -1,7 +1,15 @@
 import { z } from 'zod'
 
 export const InputNoteSchema = z.object({
-  value: z.preprocess((value) => Number(value), z.number({ required_error: `Insira um número` })),
+  value: z.preprocess(
+    (value) => Number(value),
+    z
+      .number({
+        required_error: 'Insira um número',
+      })
+      .min(1, { message: 'O valor mínimo é 1' })
+      .max(100, { message: 'O valor máximo é 100' }),
+  ),
 })
 
 export type InputNote = z.infer<typeof InputNoteSchema>
