61d1380ed18188425ce4dfb69f3e10df2019cd9a
enzoguis <enzog0315@gmail.com>
2024-10-23 22:21:38 -0300

    refactor(view-activity-input): adding preprocess at schema for input note

--- hosting/src/models/input-note-schema.ts
+++ hosting/src/models/input-note-schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const InputNoteSchema = z.object({
-  value: z.number({ required_error: `Insira um número` }),
+  value: z.preprocess((value) => Number(value), z.number({ required_error: `Insira um número` })),
 })
 
 export type InputNote = z.infer<typeof InputNoteSchema>
