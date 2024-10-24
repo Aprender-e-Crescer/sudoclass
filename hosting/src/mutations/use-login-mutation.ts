ddc1310cfaf16b9e23b9803e867ecd63da965d38
Yuosssef <yuossefpolidorio@gmial.com>
2024-10-23 22:35:07 -0300

    fix(use-login-mutation): component update

--- hosting/src/mutations/use-login-mutation.ts
+++ hosting/src/mutations/use-login-mutation.ts
@@ -1,59 +1,17 @@
 import { auth } from "@/services/firebase";
 import { useMutation } from "@tanstack/react-query";
-import { signInWithEmailAndPassword, signInAnonymously } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-
-interface LoginData {
-  cpf?: string; // cpf é opcional para login anônimo
-  password?: string; // password é opcional para login anônimo
-}
-
+import { signInAnonymously } from "firebase/auth";
 interface LoginResponses {
   onSuccess: () => void;
-  onError: (error: any) => void; // Passa o erro ao onError para tratamento
+  onError: (error: any) => void;
 }
 
 export function useLoginMutation({ onSuccess, onError }: LoginResponses) {
   return useMutation({
-    mutationFn: async ({ cpf, password }: LoginData = {}) => { // Default para um objeto vazio
-      try {
-        if (cpf && password) {
-          return await signInWithEmailAndPassword(auth, cpf, password);
-        } else {
-          return await signInAnonymously(auth);
-        }
-      } catch (error) {
-        onError(error); // Passa o erro para a função onError
-        throw error; // Lança o erro para ser tratado pela mutação
-      }
+    mutationFn: () => {
+      return signInAnonymously(auth);
     },
     onSuccess,
     onError,
   });
-}
-
-export function useLoginController() {
-  const navigate = useNavigate();
-
-  const { mutateAsync: login } = useLoginMutation({
-    onSuccess: () => {
-      navigate('/home'); // Navega para a página inicial após login
-    },
-    onError: (error) => {
-      // Aqui você pode verificar o tipo de erro e exibir mensagens apropriadas
-      if (error.code) {
-        console.error("Erro durante a autenticação:", error.message);
-        // Por exemplo, você pode fazer um toast ou alert aqui
-      }
-    },
-  });
-
-  const handleLogin = async () => {
-    try {
-      // Passar um objeto vazio para login anônimo
-      await login({}); // Para autenticação anônima, passa um objeto vazio
-    } catch (error) {
-      console.error("Erro durante o login", error);
-    }
-  };
 }
