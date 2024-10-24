3a26e048a053c4ba7a359b562d3c173a90c762f5
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2024-10-24 18:15:53 -0300

    fix(file-input): i just fixed the input that appeared that I had changed

--- hosting/src/components/custom/file-input.tsx
+++ hosting/src/components/custom/file-input.tsx
@@ -1,13 +1,8 @@
-
-import { Field } from 'formik'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
-import { Plus, Upload } from 'lucide-react'
 import { useFormikContext } from 'formik'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { CircleX, Plus, Upload } from 'lucide-react'
 import { useState } from 'react'
 import { Input } from '@/components/ui/input'
-
 
 interface FileInputProps {
   label: string
@@ -52,11 +47,6 @@ export function InputFile({ id, label, title, placeholder, name }: FileInputProp
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
-      <label htmlFor={id} className="cursor-pointer bg-white border border-gray-200 rounded w-full py-2">
-        {placeholder}
-      </label>
-      <Field placeholder={placeholder} id={id} name={name} type="file" className="invisible" />
-
 
       <div className="flex justify-between items-center">
         <label htmlFor={id} className="cursor-pointer bg-white border border-gray-200 rounded w-full py-2 relative">
@@ -64,6 +54,7 @@ export function InputFile({ id, label, title, placeholder, name }: FileInputProp
         </label>
         {fileName && <CircleX onClick={handleOnRemoveFileClick} className="cursor-pointer absolute right-3" />}
       </div>
+
       <Input
         placeholder={placeholder}
         id={id}
