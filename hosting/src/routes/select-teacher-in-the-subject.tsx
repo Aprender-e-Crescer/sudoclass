551c15e382ddcef7e0c2487160aaf1b2ba237692
thaylaalves <thaylavitoriar@gmail.com>
2024-10-25 17:13:04 -0300

    refactor: styling screen

--- hosting/src/routes/select-teacher-in-the-subject.tsx
+++ hosting/src/routes/select-teacher-in-the-subject.tsx
@@ -5,5 +5,30 @@ export const Route = createFileRoute('/select-teacher-in-the-subject')({
 })
 
 export function SelectionTeacher() {
-  return <div></div>
+  return (
+    <div>
+      <h1 className="font-semibold p-5 border">Selecionar professor</h1>
+      <h1 className="font-semibold p-3 ml-3 text-gray-400">Professores cadastrados</h1>
+      <hr />
+      <div className="flex items-center ml-6 p-4 ">
+        <div className="w-8 h-8 rounded-full flex items-center justify-center bg-blue-500 text-white">A</div>
+        <p className="text-gray-700 ml-4">Alexandre</p>
+      </div>
+      <hr />
+      <div className="flex items-center ml-6 p-4">
+        <div className="w-8 h-8 rounded-full flex items-center justify-center bg-red-500 text-white">P</div>
+        <p className="text-gray-700 ml-4">Pardim</p>
+      </div>
+      <hr />
+      <div className="flex items-center ml-6 p-4">
+        <div className="w-8 h-8 rounded-full flex items-center justify-center bg-yellow-400 text-white">S</div>
+        <p className="text-gray-700 ml-4">Stephani</p>
+      </div>
+      <hr />
+      <div className="flex items-center ml-6 p-4">
+        <div className="w-8 h-8 rounded-full flex items-center justify-center bg-orange-500 text-white">J</div>
+        <p className="text-gray-700 ml-4">José Otavio</p>
+      </div>
+    </div>
+  )
 }
