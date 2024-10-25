c314fbf0c230613b6fb3fcb2ba8f60ec7e5c74c1
thaylaalves <thaylavitoriar@gmail.com>
2024-10-25 17:21:32 -0300

    refactor: adding button

--- hosting/src/routes/select-teacher-in-the-subject.tsx
+++ hosting/src/routes/select-teacher-in-the-subject.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/select-teacher-in-the-subject')({
   component: SelectionTeacher,
@@ -7,7 +8,7 @@ export const Route = createFileRoute('/select-teacher-in-the-subject')({
 export function SelectionTeacher() {
   return (
     <div>
-      <h1 className="font-semibold p-5 border">Selecionar professor</h1>
+      <h1 className="font-semibold p-5 border">Selecione professor</h1>
       <h1 className="font-semibold p-3 ml-3 text-gray-400">Professores cadastrados</h1>
       <hr />
       <div className="flex items-center ml-6 p-4 ">
@@ -28,6 +29,10 @@ export function SelectionTeacher() {
       <div className="flex items-center ml-6 p-4">
         <div className="w-8 h-8 rounded-full flex items-center justify-center bg-orange-500 text-white">J</div>
         <p className="text-gray-700 ml-4">José Otavio</p>
+      </div>
+      <div className="flex items-center justify-center mt-5">
+        <Button variant="lightTextBlack">Cancelar</Button>
+        <Button variant="blueButton">Adicionar</Button>
       </div>
     </div>
   )
