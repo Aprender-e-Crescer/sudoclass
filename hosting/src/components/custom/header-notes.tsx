0ab1cf34c70420e1c8c450f439097eaf0e264fd9
KauaCantelli <cantellidumbo@gmail.com>
2024-10-29 23:12:06 -0300

    fixing responsiveness notes screen

--- hosting/src/components/custom/header-notes.tsx
+++ hosting/src/components/custom/header-notes.tsx
@@ -1,20 +1,29 @@
 interface Props {
-    title: string
-    subtitle: string
-    totalNote: string
-  }
-   
-  export function HeaderNotes({ title, subtitle, totalNote }: Props) {
-    return (
-          <div className="border border-[#DBDBDB] w-52 h-24 flex gap-5 flex-col items-center ">
-            <div className="w-[185px] mt-2">
-              <h1 className="text-[18px] flex font font-semibold text-[#0C408F]">{title}</h1>
-              <p className="text-[14px] flex font-medium text-[#787486] ">{subtitle}</p>
-            </div>
-            <div className="w-48">
-              <hr />
-              <p className="text-[8px] text-[#787486] font-normal ml-1 mt-1">{totalNote}</p>
-            </div>
+  title?: string
+  subtitle?: string
+}
+
+export function HeaderNotes({ title, subtitle }: Props) {
+  return (
+    <>
+    <div className="flex md:hidden">
+      <div className="flex flex-col w-36 h-20 border border-[#DBDBDB] items-center justify-center gap-2">
+        <h1 className="flex text-[#0C408F] font-semibold">{title}</h1>
+        <p className="flex items-center text-[#787486] font-medium">{subtitle}</p>
+      </div>
+    </div>  
+
+      <div className="hidden md:flex">
+        <div className="border border-[#DBDBDB] w-52 h-24 flex gap-5 flex-col items-center ">
+          <div className="w-[185px] mt-2">
+            <h1 className="text-[18px] flex font font-semibold text-[#0C408F]">{title}</h1>
+            <p className="text-[14px] flex font-medium text-[#787486] ">{subtitle}</p>
           </div>
-    )
-  }
+          <div className="w-48">
+            <hr />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- hosting/src/components/custom/notes-table.tsx
+++ hosting/src/components/custom/notes-table.tsx
@@ -1,41 +1,65 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import Avatarimg from '@/assets/avatar.png';
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
+import Avatarimg from '@/assets/avatar.png'
 
 interface Props {
-  name?: string;
-  rating?: string;
-  overallRating?: string;
-  otherNotes?: string;
-  avatar?: string;
+  name?: string
+  rating?: string
+  overallRating?: string
+  otherNotes?: string
+  avatar?: string
 }
 
 export function NotesTable({ name, rating, overallRating, avatar, otherNotes }: Props) {
   return (
-    <div className="flex">
-      <div className="flex items-center  border border-[#DBDBDB] gap-12 w-72">
-       <div className="ml-5"> {avatar && (
-          <Avatar>
-            <AvatarImage src={avatar || Avatarimg} alt={name} />
-            <AvatarFallback></AvatarFallback>
-          </Avatar>
-        )}
+    <>
+      <div className="flex flex-row md:hidden">
+        <div className="flex flex-col w-full">
+          <div className="flex flex-row items-center border border-[#DBDBDB] w-24 p-4 h-20">
+            {avatar && (
+              <Avatar className="w-28">
+                <AvatarImage src={avatar || Avatarimg} alt={name} />
+                <AvatarFallback />
+              </Avatar>
+            )}
+            
+          </div>
+
+          <div className="flex items-center justify-center border border-[#DBDBDB] w-24 h-20 text-[#292D32] text-opacity-70">
+            {rating}
+          </div>
+
+          <div className="flex items-center justify-center border border-[#DBDBDB] w-24 h-20 text-[#292D32] text-opacity-70">
+            {overallRating}
+          </div>
+
+          <div className="flex items-center justify-center border border-[#DBDBDB] w-24  h-20">{otherNotes}</div>
         </div>
-        <div className="flex justify-center items-center">
-        <span>{name}</span>
+      </div>
+
+      <div className="hidden md:flex">
+        <div className="flex">
+          <div className="flex items-center border border-[#DBDBDB] gap-12 w-72">
+            <div className="flex flex-col">
+              {' '}
+              {avatar && (
+                <Avatar>
+                  <AvatarImage src={avatar || Avatarimg} alt={name} />
+                  <AvatarFallback></AvatarFallback>
+                </Avatar>
+              )}
+            </div>
+            <div className="flex justify-center items-center">{name}</div>
+          </div>
+          <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24 text-[#292D32] text-opacity-70">
+            {rating}
+          </div>
+          <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24 text-[#292D32] text-opacity-70">
+            {overallRating}
+          </div>
+          <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24">{otherNotes}</div>
+          <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24">{otherNotes}</div>
         </div>
       </div>
-      <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24 text-[#292D32] text-opacity-70">
-        {rating}
-      </div>
-      <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24 text-[#292D32] text-opacity-70">
-        {overallRating}
-      </div>
-      <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24">
-        {otherNotes}
-      </div>
-      <div className="flex items-center justify-center border border-[#DBDBDB] w-52 h-24">
-        {otherNotes}
-      </div>
-    </div>
-  );
+    </>
+  )
 }
