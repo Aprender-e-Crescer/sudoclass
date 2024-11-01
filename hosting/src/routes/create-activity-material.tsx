d3fb03207f82f6bb3e794f4c9aced5d72af54569
Alucsky <samuelmolinari8@gmail.com>
2024-11-01 10:05:40 -0300

    feat(create-activity-material): ajust layout

--- hosting/src/routes/create-activity-material.tsx
+++ hosting/src/routes/create-activity-material.tsx
@@ -57,7 +57,7 @@ export function CreateActivityMaterial() {
         'aQjvxCKlEuHc9YQEedCQ',
         'subjects',
         'zGTOAwnKJBjFSmayHxJo',
-        'activities'
+        'activities',
       )
       const newActivity = {
         title: values.title,
@@ -75,67 +75,45 @@ export function CreateActivityMaterial() {
 
   return (
     <>
-      <div className="flex h-full w-full mt-10 md:hidden">
+      <div className="flex h-full w-full mt-10 ">
         <div className="flex flex-col w-full">
-          <div className="flex flex-col w-full justify-around gap-4 p-5">
-            <SheetActivies />
-            <p>Pontos</p>
-            <p>Data</p>
-            <DatePickerDemo date={date} setDate={setDate} />
-          </div>
-          <div className="flex flex-col h-full w-full gap-2 justify-center items-center">
-            <div className="flex flex-col w-full h-64 border p-6">
-              <p>Título</p>
-              <input type="text" className="border p-10 rounded-sm w-full" />
-              <p>Instruções</p>
-              <input type="text" className="border p-10 rounded-sm w-full" />
-            </div>
-            <p>Anexar</p>
-            <div className="flex justify-center border w-full p-4 rounded-sm">
-              <div className="relative inline-block">
-                <div className="w-24 h-24 bg-white border rounded-full flex items-center justify-center">
-                  <Download color="black" size={50} />
-                </div>
-                <input type="file" accept="image/*" className="absolute inset-0 opacity-0 cursor-pointer" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="hidden md:flex h-full w-full mt-10">
-        <div className="flex flex-col w-full gap-10">
-          <div className="flex justify-around mx-10 gap-10">
-            <div className="flex flex-col w-full h-64 border p-6">
+          <div className="flex justify-around mx-10">
+            <div className="flex flex-col w-full h-full border p-6 ">
               <Formik initialValues={initialValues} onSubmit={addActivity}>
                 {({ setFieldValue }) => (
                   <Form>
-                    <div>
-                      <p>Título</p>
-                      <Field name="title" placeholder="Digite o título" className="border rounded-sm w-full p-2" />
+                    <div className="flex flex-col  gap-12">
+                      <div>
+                        <p>Título</p>
+                        <Field name="title" placeholder="Digite o título" className="border rounded-sm w-full p-2" />
+                      </div>
+                      <div>
+                        <p>Instruções</p>
+                        <Field
+                          name="instructions"
+                          placeholder="Digite as instruções"
+                          className="border p-7 rounded-sm w-full"
+                        />
+                      </div>
+                      <div className="w-32">
+                        <p>Peso</p>
+                        <InputWithoutLabel
+                          id="value"
+                          name="value"
+                          placeholder="Digite o valor da atividade"
+                          onChange={(e) => setFieldValue('value', e.target.value.replace(/\D/g, ''))}
+                        />
+                      </div>
+
+                      <div className="flex justify-between items-center ">
+                        <div>
+                          <p>Data de entrega</p>
+                          <DatePickerDemo date={date} setDate={setDate} />
+                        </div>
+
+                        <Button type="submit">Criar atividade</Button>
+                      </div>
                     </div>
-                    <div>
-                      <p>Instruções</p>
-                      <Field
-                        name="instructions"
-                        placeholder="Digite as instruções"
-                        className="border p-7 rounded-sm w-full"
-                      />
-                    </div>
-                    <div>
-                      <p>Pontos</p>
-                      <InputWithoutLabel
-                        id="value"
-                        name="value"
-                        placeholder="Digite o valor da atividade"
-                        onChange={(e) => setFieldValue('value', e.target.value.replace(/\D/g, ''))}
-                      />
-                    </div>
-                    <div>
-                      <p>Data de entrega</p>
-                      <DatePickerDemo date={date} setDate={setDate} />
-                    </div>
-                    <Button type="submit">Criar atividade</Button>
                   </Form>
                 )}
               </Formik>
