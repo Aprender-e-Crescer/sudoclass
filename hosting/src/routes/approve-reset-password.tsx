7c55ff21a8e507185166510729e6192bcb5695b6
Yuosssef <yuossefpolidorio@gmail.com>
2024-11-02 10:32:37 -0300

    fix(approveresetpassword): added props to the page

--- hosting/src/routes/approve-reset-password.tsx
+++ hosting/src/routes/approve-reset-password.tsx
@@ -3,19 +3,19 @@ import approveResetPassword from '@/assets/approveResetPassword.png'
 import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
 
+interface ApproveResetPasswordProps {
+  aluno : string
+}
+
+
 export const Route = createFileRoute('/approve-reset-password')({
   component: ApproveResetPassword,
 })
 
-export function ApproveResetPassword() {
+export function ApproveResetPassword({aluno = "Aluno"} : ApproveResetPasswordProps) {
   return (
     <div>
-      <div className="flex flex-col justify-center items-center mb-24 min-h-screen mt-20 font-poppins px-4 sm:px-0">
-        <div className="w-full text-center md:hidden mb-4">
-          <h1 className="text-[30px] font-bold text-blue-600">Bem-vindo a</h1>
-          <span className="text-[40px] font-bold text-blue-900">Sudotec</span>
-        </div>
-
+      <div className="flex flex-col justify-center items-center mb-24 min-h-screen font-poppins px-4 sm:px-0">
         <div className="flex w-full max-w-6xl h-full md:h-[600px] rounded-xl shadow-lg overflow-hidden bg-white flex-col md:flex-row mb-36">
           <div className="w-full flex justify-center items-center bg-blue-600 h-[250px] md:h-full md:w-1/2">
             <img
@@ -27,9 +27,9 @@ export function ApproveResetPassword() {
 
           <div className="w-full md:w-1/2 p-8 flex flex-col justify-center items-center h-full space-y-4">
             <div className="hidden md:block text-center">
-              <h1 className="text-[24px] font-bold text-blue-600">
-                O aluno(a) solicitou uma redefinição de senha. Caso aprove,
-                copie a nova senha gerada e envie ao aluno(a).
+              <h1 className="text-[24px] font-semibold text-blue-600 font-poppins">
+                O aluno {aluno} solicitou uma redefinição de senha. Caso aprove, copie a nova senha gerada e envie ao
+                aluno(a).
               </h1>
             </div>
 
@@ -40,7 +40,7 @@ export function ApproveResetPassword() {
                 </Button>
               </div>
 
-              <div className="flex justify-between mt-8 ml-4 w-full">
+              <div className="flex justify-between mt-6 ml-4 w-full">
                 <Button variant="loginoutline" size="Login" type="submit">
                   Retornar
                 </Button>
@@ -52,5 +52,3 @@ export function ApproveResetPassword() {
     </div>
   )
 }
-
-export default ApproveResetPassword
