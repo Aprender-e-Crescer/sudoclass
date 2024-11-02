18921037e67cddd03e745014f6735277cd201af0
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-11-02 09:35:47 -0300

    refactor(teacher-registration.tsx): remove unnecesary divs and adjusting namings

--- hosting/src/routes/teacher-registration-form.tsx
+++ hosting/src/routes/teacher-registration-form.tsx
@@ -3,170 +3,182 @@ import { InputFile } from '@/components/custom/file-input'
 import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
 import { Formik, Form } from 'formik'
-import { X } from 'lucide-react'
-import { Plus } from 'lucide-react'
 
 export const Route = createFileRoute('/teacher-registration-form')({
   component: TeacherRegistration,
 })
+
+const initialValues = {
+  teacherName: '',
+  email: '',
+  phoneNumber: '',
+  state: '',
+  city: '',
+  street: '',
+  neighborhood: '',
+  houseNumber: '',
+  dateOfBirth: '',
+  CPF: '',
+  RG: '',
+  shippingDate: '',
+  shippingState: '',
+  stateOfBirth: '',
+  cityOfBirth: '',
+  attachDocuments: '',
+}
+
 export function TeacherRegistration() {
   return (
     <>
-      <Formik>
-        <Form className='p-1'>
-          <div className=' p-2 w-11/12 rounded-sm border-2'>
+      <Formik initialValues={initialValues} onSubmit={() => {}}>
+        <Form className="p-1">
+          <div className=" p-2 rounded-sm border-2">
             <InputForm
-                    title="Nome completo"
-                    placeholder="nomecompleto.."
-                    id="nameProfessor"
-                    name="nameProfessor"
-                    label="nameProfessor"
-                    customStyleButton='rounded-lg border-2 p-[6px]'
-                    
-                />
-                <InputForm
-                    title="Email"
-                    placeholder="@gmail.com"
-                    id="emailProfessor"
-                    name="emailProfessor"
-                    label="emailProfessor"
-                    customStyleButton='rounded-lg border-2 p-[6px]'
-                    
-                />
-                <InputForm
-                    title="Telefone"
-                    placeholder="(99) 99999-9999"
-                    id="phoneNumberTeacher"
-                    name="phoneNumberTeacher"
-                    label="phoneNumberTeacher"
-                    customStyleButton='rounded-lg border-2 p-[6px]'
-                    
-                />
-                 <div className='flex gap-5 flex-wrap'>
-                  <InputForm
-                      title="Estado"
-                      placeholder="PR"
-                      id="state"
-                      name="state"
-                      label="state"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                      
-                 />
-                  <InputForm
-                      title="Muncipio"
-                      placeholder="Dois vizinhos"
-                      id="city"
-                      name="city"
-                      label="city"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                  />
-                </div>  
+              title="Nome completo"
+              placeholder="Nome completo"
+              id="teacherName"
+              name="teacherName"
+              label="teacherName"
+              customStyleInput="rounded-lg border-2 p-[6px]"
+            />
+            <InputForm
+              title="Email"
+              placeholder="professor@gmail.com"
+              id="email"
+              name="email"
+              label="email"
+              customStyleInput="rounded-lg border-2 p-[6px]"
+            />
+            <InputForm
+              title="Telefone"
+              placeholder="(99) 99999-9999"
+              id="phoneNumber"
+              name="phoneNumber"
+              label="phoneNumber"
+              customStyleInput="rounded-lg border-2 p-[6px]"
+            />
+            <div className="flex gap-5 flex-wrap">
+              <InputForm
+                title="Estado"
+                placeholder="PR"
+                id="state"
+                name="state"
+                label="state"
+                customStyleInput="rounded-lg border-2 p-[6px]"
+              />
+              <InputForm
+                title="Municipio"
+                placeholder="Seu Municipio"
+                id="city"
+                name="city"
+                label="city"
+                customStyleInput="rounded-lg border-2 p-[6px]"
+              />
+            </div>
 
-                <InputForm
-                      title="Rua"
-                      placeholder="Rua"
-                      id="street"
-                      name="street"
-                      label="street"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                />
+            <InputForm
+              title="Rua"
+              placeholder="Rua"
+              id="street"
+              name="street"
+              label="street"
+              customStyleInput="rounded-lg border-2 p-[6px]"
+            />
 
-                 <div className='flex gap-5'>
-                  <div className='xl:w-1/2 w-1/2 '>
-                  <InputForm
-                      title="Bairro"
-                      placeholder="Bairro"
-                      id="neighborhood"
-                      name="neighborhood"
-                      label="neighborhood"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                      
-                  />
-                  </div>
-                  <div className='xl:w-72 w-1/2 max-sm:w-1/3'>
-                  <InputForm
-                      title="Numero"
-                      placeholder="ex: 77"
-                      id="HouseNumber"
-                      name="HouseNumber"
-                      label="HouseNumber"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                  />
-                  </div>
-                </div>
+            <div className="flex sm:gap-5 sm:flex-row flex-col">
+              <InputForm
+                title="Bairro"
+                placeholder="Bairro"
+                id="neighborhood"
+                name="neighborhood"
+                label="neighborhood"
+                customStyleInput="rounded-lg border-2 p-[6px]"
+              />
 
-                <div className='flex gap-5 max-sm:gap-1 flex-wrap'>
-                <InputForm
-                      title="Data de nascimento"
-                      placeholder="00/00/0000"
-                      id="Date of birth"
-                      name="Date of birth"
-                      label="Date of birth"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                  />
-                  <InputForm
-                      title="CPF"
-                      placeholder="000.000.000-00"
-                      id="CPFadmin"
-                      name="CPFadmin"
-                      label="CPFadmin"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                  />
-                  <InputForm
-                      title="RG"
-                      placeholder="00.000.000-0"
-                      id="RGadmin"
-                      name="RGadmin"
-                      label="RGadmin"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                  />
+              <InputForm
+                title="Numero"
+                placeholder="ex: 77"
+                id="houseNumber"
+                name="houseNumber"
+                label="houseNumber"
+                customStyleInput="rounded-lg border-2 p-[6px]"
+              />
+            </div>
 
-                </div>
-                <InputForm
-                      title="Data de expedição RG"
-                      placeholder="data de expedição"
-                      id="shippingDate"
-                      name="shippingDate"
-                      label="shippingDate"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                />
-                 <InputForm
-                      title="Estado de expedição RG"
-                      placeholder="estado de expedição"
-                      id="shippingState"
-                      name="shippingState"
-                      label="shippingState"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                />
-                <InputForm
-                      title="Estado de nascimento"
-                      placeholder="Estado"
-                      id="stateOfBirth"
-                      name="stateOfBirth"
-                      label="stateOfBirth"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                />
-                <InputForm
-                      title="Cidade de nascimento"
-                      placeholder="  Cidade"
-                      id="cityOfBirth"
-                      name="cityOfBirth"
-                      label="cityOfBirth"
-                      customStyleButton='rounded-lg border-2 p-[6px]'
-                />
-                 <InputFile
-                      title='Anexar arquivos'
-                      placeholder="ImagemDocumentoAnexado.png 90kb"
-                      id="AttachDocuments"
-                      name="AttachDocuments"
-                      label="AttachDocuments"
-                />
-                <div className='flex justify-center gap-5'>
-                <Button variant='ghostBlack' size='large' className='w-64'>Cancelar</Button>
-                <Button variant='blueButton' size='large' className='w-64'>Atualizar</Button>
-                </div>
-               
-                </div>
+            <div className="flex gap-5 max-sm:gap-1 flex-wrap">
+              <InputForm
+                title="Data de nascimento"
+                placeholder="00/00/0000"
+                id="DateOfBirth"
+                name="DateOfBirth"
+                label="DateOfBirth"
+                customStyleInput="rounded-lg border-2 p-[6px]"
+              />
+              <InputForm
+                title="CPF"
+                placeholder="000.000.000-00"
+                id="CPF"
+                name="CPF"
+                label="CPF"
+                customStyleInput="rounded-lg border-2 p-[6px]"
+              />
+              <InputForm
+                title="RG"
+                placeholder="00.000.000-0"
+                id="RG"
+                name="RG"
+                label="RG"
+                customStyleInput="rounded-lg border-2 p-[6px]"
+              />
+            </div>
+            <InputForm
+              title="Data de expedição RG"
+              placeholder="data de expedição"
+              id="shippingDate"
+              name="shippingDate"
+              label="shippingDate"
+              customStyleInput="rounded-lg border-2 p-[6px]"
+            />
+            <InputForm
+              title="Estado de expedição RG"
+              placeholder="estado de expedição"
+              id="shippingState"
+              name="shippingState"
+              label="shippingState"
+              customStyleInput="rounded-lg border-2 p-[6px]"
+            />
+            <InputForm
+              title="Estado de nascimento"
+              placeholder="Estado"
+              id="stateOfBirth"
+              name="stateOfBirth"
+              label="stateOfBirth"
+              customStyleInput="rounded-lg border-2 p-[6px]"
+            />
+            <InputForm
+              title="Cidade de nascimento"
+              placeholder="  Cidade"
+              id="cityOfBirth"
+              name="cityOfBirth"
+              label="cityOfBirth"
+              customStyleInput="rounded-lg border-2 p-[6px]"
+            />
+            <InputFile
+              title="Anexar arquivos"
+              placeholder="ImagemDocumentoAnexado.png 90kb"
+              id="attachDocuments"
+              name="attachDocuments"
+              label="attachDocuments"
+            />
+            <div className="flex justify-center gap-5">
+              <Button variant="ghostBlack" size="large" className="w-64">
+                Cancelar
+              </Button>
+              <Button variant="blueButton" size="large" className="w-64">
+                Atualizar
+              </Button>
+            </div>
+          </div>
         </Form>
       </Formik>
     </>
