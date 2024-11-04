cb17862c276d6c07314931fe3da3bbac84660418
thaylaalves <thaylavitoriar@gmail.com>
2024-11-04 20:05:26 -0300

    refactor: adjusting to edit document

--- hosting/src/mutations/course-syllabus-mutation.ts
+++ hosting/src/mutations/course-syllabus-mutation.ts
@@ -1,15 +1,16 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { addDoc, collection } from 'firebase/firestore'
+import { doc, updateDoc } from 'firebase/firestore'
 import { firestore } from '@/services/firebase'
 
 export const LIST_SYLLABUS_QUERY_KEY = 'syllabus'
 
-export function useCreateOrEditSyllabusMutation() {
+export function useCreateOrEditSyllabusMutation(syllabusID: string) {
   const queryClient = useQueryClient()
+  const syllbusRef = doc(firestore, 'syllabus', syllabusID)
 
   return useMutation({
     mutationKey: ['createOrEditSyllabus'],
-    mutationFn: async (values: any) => await addDoc(collection(firestore, 'syllabus'), values),
+    mutationFn: async (values: any) => await updateDoc(syllbusRef, { description: values }),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [LIST_SYLLABUS_QUERY_KEY] })
     },
