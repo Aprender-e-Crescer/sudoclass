58419f73ca60f9d5f39c7693dfed5d0db18668fe
Alucsky <samuelmolinari8@gmail.com>
2024-11-05 00:37:28 -0300

    style(list-teachers): ajust borders and removing desnecessary overflow-hidden
    n

--- hosting/src/routes/list-teachers.tsx
+++ hosting/src/routes/list-teachers.tsx
@@ -11,21 +11,21 @@ export function TeacherList() {
   const { data: teachersList } = TeachersSchemaQuery()
   return (
     <div className="p-2">
-      <div className="w-full border overflow-hidden">
-        <div className="flex justify-between items-center ml-2 p-4 text-[#B5B7C0]">
+      <div className="w-full border ">
+        <div className="flex justify-between items-center p-4 border text-[#B5B7C0]">
           <span>Professores cadastrados</span>
           <Button variant="blueButton" size="large">
             Cadastrar novo professor +
           </Button>
         </div>
-        <div className="flex items-center border-t p-4">
+        <div className="flex items-center  p-4">
           <img src={avatarLogo} alt="Avatar Logo" className="w-12 h-18 ml-16" /> 
           <h1 className="ml-16 text-xl font-semibold">Jane Cooper</h1> 
         </div>
         {teachersList?.map(({ fullName }, index) => (
           <div
             key={index}
-            className={`flex items-center p-4 ${index !== teachersList.length - 1 ? 'border-b' : ''}`}
+            className={`flex items-center p-4 ${index !== teachersList.length - 1 ? 'border' : ''}`}
           >
             <img src={avatarLogo} alt="Avatar" className="w-12 h-18 ml-16" /> 
             <h1 className="ml-16 text-xl font-semibold">{fullName}</h1> 
