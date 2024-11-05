b4dd349885e754731109da2bdebbb932922e4361
Yuosssef <yuossefpolidorio@gmail.com>
2024-11-04 21:10:26 -0300

    fix(frequencyschema): removed commit

--- hosting/src/models/frequency-scherma.ts
+++ hosting/src/models/frequency-scherma.ts
@@ -1,20 +1,10 @@
-// import { z } from "zod";
-
-// export const frequencySchema = z.object({
-//   IDclass: z.string().min(2, { message: 'O nome da classe deve ter pelo menos 2 caracteres' }),
-//   data: z.string().min(2, { message: 'O nome da classe deve ter pelo menos 2 caracteres' }),
-//   status: z.enum(["present", "absent", "late"], { required_error: 'O campo deve ser um status válido' }),
-// });
-
-// export type Frequency = z.infer<typeof frequencySchema>;
-
 import { z } from "zod";
-import { Timestamp } from "firebase/firestore"; // Certifique-se de importar o Timestamp
+import { Timestamp } from "firebase/firestore"; 
 
 export const frequencySchema = z.object({
   IDclass: z.string().min(2, { message: 'O nome da classe deve ter pelo menos 2 caracteres' }),
   data: z.instanceof(Timestamp).refine((data) => {
-    return data instanceof Timestamp; // Verifica se o campo date é uma instância de Timestamp
+    return data instanceof Timestamp; 
   }, {
     message: 'A data deve ser um Timestamp válido do Firestore',
   }),
