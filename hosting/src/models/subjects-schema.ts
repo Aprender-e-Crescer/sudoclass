20fe5dca46973e75da16e64a66352f9b09567b64
enzoguis <enzog0315@gmail.com>
2024-11-05 19:55:50 -0300

    fix(subjects-query and home-list-subject.tsx): fixing the subjects query and zod for subjects
    fixing the error when subjects query returns undefined

--- hosting/src/models/subjects-schema.ts
+++ hosting/src/models/subjects-schema.ts
@@ -1,13 +1,32 @@
-import { z } from 'zod';
+import { DocumentReference, Timestamp } from 'firebase/firestore'
+import { z } from 'zod'
 
 export const subjectsSchema = z.object({
+  id: z.string(),
   name: z.string().min(2, { message: 'O nome da matéria não pode ser inferior a 2 letras' }),
   description: z.string().min(2, { message: 'A descrição da matéria não pode ser inferior a 2 letras' }),
-  endDate: z.string().min(1, { message: 'Insira uma data valida' }),
-  startDate: z.string().min(1, { message: 'Insira uma data valida' }),
-  workload: z.string().min(1, { message: 'Insira uma hora valida' }),
-  teacher: z.string().min(1, {message: 'insira um nome valido'})
-});
+  endDate: z
+    .preprocess((value) => {
+      if (value instanceof Timestamp) {
+        return value.toDate()
+      }
+      return value
+    }, z.date())
+    .optional(),
+  startDate: z
+    .preprocess((value) => {
+      if (value instanceof Timestamp) {
+        return value.toDate()
+      }
+      return value
+    }, z.date())
+    .optional(),
+  workload: z.number().min(1, { message: 'Insira uma hora valida' }).optional(),
+  teacher: z
+    .any()
+    .refine((teacher: object): teacher is DocumentReference => teacher instanceof DocumentReference)
+    .optional(),
+  menu: z.object({ objective: z.string(), methodology: z.string() }).optional(),
+})
 
-
-export type Subject = z.infer<typeof subjectsSchema>;
+export type Subject = z.infer<typeof subjectsSchema>
--- hosting/src/queries/use-list-subjects-query.ts
+++ hosting/src/queries/use-list-subjects-query.ts
@@ -1,7 +1,7 @@
-import { Subject, subjectsSchema } from '@/models/subjects-schema';
-import { firestore } from '@/services/firebase';
-import { useQuery } from '@tanstack/react-query';
-import { collection, getDocs, QueryDocumentSnapshot } from 'firebase/firestore';
+import { Subject, subjectsSchema } from '@/models/subjects-schema'
+import { firestore } from '@/services/firebase'
+import { useQuery } from '@tanstack/react-query'
+import { collection, getDocs, QueryDocumentSnapshot } from 'firebase/firestore'
 
 export function useListSubjectsQuery(schoolMatricesId: string) {
   return useQuery<Subject[]>({
@@ -9,13 +9,11 @@ export function useListSubjectsQuery(schoolMatricesId: string) {
     queryFn: async () => {
       const subjectRefs = collection(firestore, 'schoolMatrices', schoolMatricesId, 'subjects').withConverter({
         toFirestore: (subject: Subject) => subject,
-        fromFirestore: (snapshot: QueryDocumentSnapshot) => {
-          return subjectsSchema.parse(snapshot.data());
-        },
-      });
+        fromFirestore: (snapshot) => subjectsSchema.parse({ id: snapshot.id, ...snapshot.data() }),
+      })
 
-      const snapshot = await getDocs(subjectRefs);
-      return snapshot.docs.map((doc) => doc.data());
+      const snapshot = await getDocs(subjectRefs)
+      return snapshot.docs.map((doc) => doc.data())
     },
-  });
+  })
 }
