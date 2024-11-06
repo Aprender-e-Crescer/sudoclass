60c35a2ec2f14ee5bdc395086c3915ee30e3a800
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2024-11-05 21:29:29 -0300

    fix(teachers-schema): fixing schema

--- hosting/src/models/teachers-schema.ts
+++ hosting/src/models/teachers-schema.ts
@@ -35,4 +35,23 @@ export const registerSchema = z.object({
   ),
 });
 
-export type RegisterRequests = z.infer<typeof registerSchema>;
+export const listTeacherSchema = z.object({
+  birthCity: z.string(),
+  birthStatus: z.string(),
+  cpf: z.string(),
+  email: z.string(),
+  fullName: z.string(),
+  municipality: z.string(),
+  neighborhood: z.string(),
+  number: z.number().or(z.string()),
+  password: z.string(),
+  profilePhoto: z.string(),
+  rgDispatchDate: z.string(),
+  rgDispatchStatus: z.string(),
+  rgNumber: z.number(),
+  state: z.string(),
+  street: z.string(),
+  telephone: z.number(),
+})
+
+export type RegisterRequests = z.infer<typeof registerSchema>
