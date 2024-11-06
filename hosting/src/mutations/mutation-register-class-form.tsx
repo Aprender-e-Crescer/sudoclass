cc7212816d858a0bcc0b93da3eb085cc22e2473d
enzo-savaris <savarisenzo2@gmail.com>
2024-11-05 21:46:38 -0300

    feat(mutation-register-class-form.tsx): passing values to mutation

--- hosting/src/mutations/mutation-register-class-form.tsx
+++ hosting/src/mutations/mutation-register-class-form.tsx
@@ -4,12 +4,22 @@ import { useMutation } from '@tanstack/react-query'
 import { collection, doc, setDoc } from 'firebase/firestore'
 
 export function useRegisterClassMutation() {
-  const docRef = doc(collection(firestore, 'cities'))
+  const docRef = doc(collection(firestore, 'classes'))
 
   return useMutation({
     mutationKey: ['register-class'],
     mutationFn: (values: CreationClass) => {
-      return setDoc(docRef, { values })
+      return setDoc(docRef, {
+        class: values.class,
+        shift: values.shift,
+        startForecast: values.startForecast,
+        endPrediction: values.endPrediction,
+        registrationEndlDate: values.registrationFinalDate,
+        numberOfHours: values.quantityHours,
+        totalVacancies: values.totalVacancies,
+        // completed: values.
+        // released: values.
+      })
     },
   })
 }
