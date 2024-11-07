8aa8f7e3496a7af1480e63e7db08b5269f939698
JoaoPMBsp <joaopedromoraesbonkoski@gmail.com>
2024-11-06 21:20:00 -0300

    fix(change-password-request, student-document-schema, student-schema, teachers-schema): fixed typess

--- hosting/src/models/student-document-schema.ts
+++ hosting/src/models/student-document-schema.ts
@@ -2,16 +2,18 @@ import { DocumentReference, Timestamp } from 'firebase/firestore'
 import { z } from 'zod'
 
 export const documentSchema = z.object({
-  createdby: z
-    .any()
-    .refine((createdby: object): createdby is DocumentReference => createdby instanceof DocumentReference),
+  createdby: z.unknown().refine(
+    (createdby: unknown): createdby is DocumentReference => createdby instanceof DocumentReference
+  ),
   creationDate: z.preprocess((value) => {
     if (value instanceof Timestamp) {
       return value.toDate()
     }
     return value
   }, z.date()),
-  student: z.any().refine((student: object): student is DocumentReference => student instanceof DocumentReference),
+  student: z.unknown().refine(
+    (student: unknown): student is DocumentReference => student instanceof DocumentReference
+  ),
   name: z.string(),
 })
 
--- hosting/src/models/student-schema.ts
+++ hosting/src/models/student-schema.ts
@@ -15,9 +15,9 @@ export const studentSchema = z.object({
   dateOfBirth: z.string(),
   email: z.string().email(),
   name: z.string(),
-  responsible: z
-    .any()
-    .refine((responsable: object): responsable is DocumentReference => responsable instanceof DocumentReference),
+  responsible: z.unknown().refine(
+    (responsable: unknown): responsable is DocumentReference => responsable instanceof DocumentReference
+  ),
   rg: z.string(),
   shippingDate: z.string(),
   shippingStatus: z.string(),
