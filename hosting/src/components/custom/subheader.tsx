ef2a084b9af20873694eb5da18261659013581e8
Yuosssef <yuossefpolidorio@gmail.com>
2024-11-12 20:22:10 -0300

    fix(subheader): add a the variant pedagogue to the subheader component

--- hosting/src/components/custom/subheader.tsx
+++ hosting/src/components/custom/subheader.tsx
@@ -1,7 +1,7 @@
 import { Link } from '@tanstack/react-router';
 
 interface SubHeaderProps {
-  hasPrivilege?: 'teacher' | 'student';
+  hasPrivilege?: 'teacher' | 'student' | 'pedagogue';
 }
 
 export function SubHeader({ hasPrivilege = 'student' }: SubHeaderProps) {
@@ -15,61 +15,97 @@ export function SubHeader({ hasPrivilege = 'student' }: SubHeaderProps) {
   return (
     <div className="flex pb-2">
       <ul className="flex space-x-6 overflow-x-auto whitespace-nowrap scrollbar-hide">
-        <li>
-          <Link
-            to="/"
-            onClick={handleClick}
-            className="menu-link cursor-pointer text-gray-500"
-          >
-            Mural
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/"
-            onClick={handleClick}
-            className="menu-link cursor-pointer text-gray-500"
-          >
-            Atividades
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/"
-            onClick={handleClick}
-            className="menu-link cursor-pointer text-gray-500"
-          >
-            Notas
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/"
-            onClick={handleClick}
-            className="menu-link cursor-pointer text-gray-500"
-          >
-            Plano de Aula
-          </Link>
-        </li>
-        <li>
-          {hasPrivilege === 'teacher' ? (
-            <Link
-              to="/"
-              onClick={handleClick}
-              className="menu-link cursor-pointer text-gray-500"
-            >
-              Chamada
-            </Link>
-          ) : (
-            <Link
-              to="/"
-              onClick={handleClick}
-              className="menu-link cursor-pointer text-gray-500"
-            >
-              Frequência
-            </Link>
-          )}
-        </li>
+        {/* Exibir links comuns se o privilégio não for 'pedagogue' */}
+        {hasPrivilege !== 'pedagogue' ? (
+          <>
+            <li>
+              <Link
+                to="/"
+                onClick={handleClick}
+                className="menu-link cursor-pointer text-gray-500"
+              >
+                Mural
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                onClick={handleClick}
+                className="menu-link cursor-pointer text-gray-500"
+              >
+                Atividades
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                onClick={handleClick}
+                className="menu-link cursor-pointer text-gray-500"
+              >
+                Notas
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                onClick={handleClick}
+                className="menu-link cursor-pointer text-gray-500"
+              >
+                Plano de Aula
+              </Link>
+            </li>
+            <li>
+              {hasPrivilege === 'teacher' ? (
+                <Link
+                  to="/"
+                  onClick={handleClick}
+                  className="menu-link cursor-pointer text-gray-500"
+                >
+                  Chamada
+                </Link>
+              ) : (
+                <Link
+                  to="/"
+                  onClick={handleClick}
+                  className="menu-link cursor-pointer text-gray-500"
+                >
+                  Frequência
+                </Link>
+              )}
+            </li>
+          </>
+        ) : (
+          // Exibir links específicos para 'pedagogue' apenas
+          <>
+            <li>
+              <Link
+                to="/aluno"
+                onClick={handleClick}
+                className="menu-link cursor-pointer text-gray-500"
+              >
+                Aluno
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/professor"
+                onClick={handleClick}
+                className="menu-link cursor-pointer text-gray-500"
+              >
+                Professor
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/pedagogo"
+                onClick={handleClick}
+                className="menu-link cursor-pointer text-gray-500"
+              >
+                Pedagogo
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
