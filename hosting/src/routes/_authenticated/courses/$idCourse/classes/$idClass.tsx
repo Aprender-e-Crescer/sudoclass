c971681159ac39f65ad2429646a8bd5c24f8b8a9
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2024-11-12 22:36:47 -0300

    feature(routes): adding path params

---
+++ hosting/src/routes/_authenticated/courses/$idCourse/classes/$idClass.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute, Outlet } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/_authenticated/courses/$idCourse/classes/$idClass')({
+    component: Outlet,
+})
