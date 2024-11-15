0c183593ad79c6ddf5efe77882d3a8de48e765fc
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-11-15 16:40:43 -0300

    feat(routes): adding outlet in layouts and comment validation

--- hosting/src/routes/_authenticated/_matter.tsx
+++ hosting/src/routes/_authenticated/_matter.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated/_matter')({
   component: Matter,
 })
 
 export function Matter() {
-  return 'Hello /_authenticated/_matter!'
+  return <Outlet />
 }
