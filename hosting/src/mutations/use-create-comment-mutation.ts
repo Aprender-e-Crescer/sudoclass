3bfddceb1995c9c6a4251dc4ce7fdb418abe3fbf
enzoguis <enzog0315@gmail.com>
2024-11-19 21:24:43 -0300

    feat(comments mutation): reorganizing mutation for comments

--- hosting/src/mutations/use-create-comment-mutation.ts
+++ hosting/src/mutations/use-create-comment-mutation.ts
@@ -1,26 +1,28 @@
-import { collection, addDoc, Timestamp } from 'firebase/firestore'
 import { firestore } from '@/services/firebase'
-import { useState } from 'react'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
+import { useMutation } from '@tanstack/react-query'
 
-export const useCreateCommentMutation = () => {
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState<string | null>(null)
-
-  const createComment = async (activityID: string, commentData: { message: string; sentBy: string }) => {
-    setLoading(true)
-    setError(null)
-    try {
-      const commentsRef = collection(firestore, 'activities', activityID, 'comments')
-      await addDoc(commentsRef, {
-        ...commentData,
-        timestamp: Timestamp.now(), // Adiciona um timestamp ao comentário
-      })
-    } catch (err) {
-      setError('Erro ao criar comentário.')
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  return { createComment, loading, error }
+export function useCreateCommentMutation(courseID: string, classID: string, subjectID: string, activityID: string) {
+  return useMutation({
+    mutationKey: ['createComment', courseID, classID, subjectID, activityID],
+    mutationFn: (commentData: { message: string; sentBy: string }) =>
+      addDoc(
+        collection(
+          firestore,
+          'courses',
+          courseID,
+          'classes',
+          classID,
+          'subjects',
+          subjectID,
+          'activities',
+          activityID,
+          'comments',
+        ),
+        {
+          ...commentData,
+          timestamp: Timestamp.now(), // Adiciona o timestamp ao comentário
+        },
+      ),
+  })
 }
