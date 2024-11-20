02ce7d8997efca81835f57171ee8c78e35d3d756
enzoguis <enzog0315@gmail.com>
2024-11-19 21:27:45 -0300

    refactor(grade mutation): changing the mutation name

--- hosting/src/mutations/use-add-grade-mutation.ts
+++ hosting/src/mutations/use-add-grade-mutation.ts
@@ -2,11 +2,22 @@ import { firestore } from '@/services/firebase'
 import { collection, addDoc } from 'firebase/firestore'
 import { useMutation } from '@tanstack/react-query'
 
-
 export function useAddGradeMutation(schoolMatriceId: string, subjectId: string, activityId: string) {
-    return useMutation({
-      mutationKey: ['createSchoolMatrice', schoolMatriceId, subjectId, activityId  ],
-      mutationFn: (values: any) => addDoc(collection(firestore, 'schoolMatrices', schoolMatriceId, 'subjects', subjectId, 'activities', activityId, 'correction'), values)
-    })
-  }
-  
+  return useMutation({
+    mutationKey: ['createGrade', schoolMatriceId, subjectId, activityId],
+    mutationFn: (values: any) =>
+      addDoc(
+        collection(
+          firestore,
+          'schoolMatrices',
+          schoolMatriceId,
+          'subjects',
+          subjectId,
+          'activities',
+          activityId,
+          'correction',
+        ),
+        values,
+      ),
+  })
+}
