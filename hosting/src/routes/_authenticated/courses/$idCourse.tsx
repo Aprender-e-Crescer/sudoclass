c126ecbd542585c46bef2078a4a623163acc0e9d
Samuel Molinari <samuelmolinari8@gmail.com>
2024-11-22 20:41:17 -0300

    create student

--- hosting/src/routes/_authenticated/courses/$idCourse.tsx
+++ hosting/src/routes/_authenticated/courses/$idCourse.tsx
@@ -2,4 +2,5 @@ import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated/courses/$idCourse')({
     component: Outlet,
+    
 })
--- hosting/src/routes/_authenticated/register/_register/students/index.tsx
+++ hosting/src/routes/_authenticated/register/_register/students/index.tsx
@@ -1,9 +1,271 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { InputFile } from '@/components/custom/file-input'
+import { InputForm } from '@/components/custom/text-input'
+import { Button } from '@/components/ui/button'
+import { useRegisterStudentController } from '@/controllers/student-register-controller'
+import { studentSchema } from '@/models/student-schema'
+import { useStudentsListQuery } from '@/queries/use-students-list-query'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { Form, Formik } from 'formik'
+import { When } from 'react-if'
+import { z } from 'zod'
+import { toFormikValidationSchema } from 'zod-formik-adapter'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import avatar from '@/assets/avatar.png'
+
+const validateSearch = z.object({
+  action: z.enum(['create', 'edit']).optional(),
+  idStudent: z.string().optional(),
+})
 
 export const Route = createFileRoute(
   '/_authenticated/register/_register/students/',
-)({
-  component: () => (
-    <div>Hello /_authenticated/register/_register/students/!</div>
-  ),
+)( {
+  component: StudentsListing,
+  validateSearch,
 })
+
+const initialValues = {
+  address: {
+    city: '',
+    neighborhood: '',
+    state: '',
+    street: '',
+    streetNumber: '',
+  },
+  cityOfBirth: '',
+  cpf: '',
+  dateOfBirth: '',
+  email: '',
+  name: '',
+  responsible: '', 
+  rg: '',
+  shippingDate: '',
+  shippingStatus: '',
+  stateOfBirth: '',
+  telephone: '',
+}
+
+function useLogic() {
+  const { registerStudent } = useRegisterStudentController()
+  const { data: registerRequests } = useStudentsListQuery()
+  const { idStudent, action } = Route.useSearch()
+
+  const handleOnStudentSubmit = (values: typeof initialValues) => {
+    registerStudent(values)
+  }
+
+  return { handleOnStudentSubmit, registerRequests, action }
+}
+
+export function StudentsListing() {
+  const { handleOnStudentSubmit, registerRequests, action } = useLogic()
+
+  return (
+    <>
+      <div className="flex flex-col flex-1">
+        <div className="flex sm:flex-row flex-col justify-between items-center">
+          <h1 className="text-2xl font-bold">Estudantes</h1>
+          <Link to="/register/students" search={{ action: 'create' }}>
+            <Button variant="blueButton" size="large">
+              Cadastrar novo estudante
+            </Button>
+          </Link>
+        </div>
+
+        <div className="flex sm:flex-row flex-col">
+          <div className="flex flex-1 flex-col p-3 data-[isAction=true]:w-2/6" data-isAction={!!action}>
+            {registerRequests?.map(({ name, email, cpf }, index) => (
+              <div key={index} className="flex justify-between items-start">
+                <Link to="/register/students" search={{ action: 'edit', idStudent: cpf }} className="flex flex-col flex-1">
+                  <div className="flex gap-x-4 my-2 items-center border p-3 cursor-pointer rounded-sm">
+                    <Avatar>
+                      <AvatarImage src={avatar} />
+                      <AvatarFallback>carregando...</AvatarFallback>
+                    </Avatar>
+                    <p>{name}</p>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+
+          <When condition={!!action}>
+            <Formik
+              initialValues={initialValues}
+              onSubmit={handleOnStudentSubmit}
+              validationSchema={toFormikValidationSchema(studentSchema)}
+            >
+              <Form className="p-1">
+                <div className="flex flex-col flex-1 p-2 rounded-sm border-2">
+                  {/* Name Field */}
+                  <InputForm
+                    title="Nome completo"
+                    placeholder="Nome completo"
+                    id="name"
+                    name="name"
+                    label="name"
+                    customStyleInput="rounded-lg border-2 p-[6px]"
+                  />
+                  
+                  {/* Email Field */}
+                  <InputForm
+                    title="Email"
+                    placeholder="estudante@gmail.com"
+                    id="email"
+                    name="email"
+                    label="email"
+                    customStyleInput="rounded-lg border-2 p-[6px]"
+                  />
+                  
+                  {/* Telephone Field */}
+                  <InputForm
+                    title="Telefone"
+                    placeholder="(99) 99999-9999"
+                    id="telephone"
+                    name="telephone"
+                    label="telephone"
+                    customStyleInput="rounded-lg border-2 p-[6px]"
+                  />
+
+                  {/* Address Fields */}
+                  <div className="flex gap-5 flex-wrap">
+                    <InputForm
+                      title="Estado"
+                      placeholder="PR"
+                      id="state"
+                      name="address.state"
+                      label="state"
+                      customStyleInput="rounded-lg border-2 p-[6px]"
+                    />
+                    <InputForm
+                      title="Municipio"
+                      placeholder="Seu Municipio"
+                      id="municipality"
+                      name="address.city"
+                      label="municipality"
+                      customStyleInput="rounded-lg border-2 p-[6px]"
+                    />
+                  </div>
+
+                  <InputForm
+                    title="Rua"
+                    placeholder="Rua"
+                    id="street"
+                    name="address.street"
+                    label="street"
+                    customStyleInput="rounded-lg border-2 p-[6px]"
+                  />
+                  
+                  {/* Neighborhood & House Number */}
+                  <div className="flex sm:gap-5 sm:flex-row flex-col">
+                    <InputForm
+                      title="Bairro"
+                      placeholder="Bairro"
+                      id="neighborhood"
+                      name="address.neighborhood"
+                      label="neighborhood"
+                      customStyleInput="rounded-lg border-2 p-[6px]"
+                    />
+                    <InputForm
+                      title="Numero"
+                      placeholder="ex: 77"
+                      id="houseNumber"
+                      name="address.streetNumber"
+                      label="houseNumber"
+                      customStyleInput="rounded-lg border-2 p-[6px]"
+                    />
+                  </div>
+
+                  {/* Date of Birth, CPF & RG */}
+                  <div className="flex gap-5 max-sm:gap-1 flex-wrap">
+                    <InputForm
+                      title="Data de nascimento"
+                      placeholder="00/00/0000"
+                      id="DateOfBirth"
+                      name="dateOfBirth"
+                      label="DateOfBirth"
+                      type="date"
+                      customStyleInput="rounded-lg border-2 p-[6px]"
+                    />
+                    <InputForm
+                      title="CPF"
+                      placeholder="000.000.000-00"
+                      id="cpf"
+                      name="cpf"
+                      label="cpf"
+                      customStyleInput="rounded-lg border-2 p-[6px]"
+                    />
+                    <InputForm
+                      title="RG"
+                      placeholder="00.000.000-0"
+                      id="rg"
+                      name="rg"
+                      label="rg"
+                      customStyleInput="rounded-lg border-2 p-[6px]"
+                    />
+                  </div>
+
+                  {/* RG Issue Date and Status */}
+                  <InputForm
+                    title="Data de expedição RG"
+                    placeholder="data de expedição"
+                    id="shippingDate"
+                    name="shippingDate"
+                    label="shippingDate"
+                    customStyleInput="rounded-lg border-2 p-[6px]"
+                  />
+                  <InputForm
+                    title="Estado de expedição RG"
+                    placeholder="estado de expedição"
+                    id="shippingStatus"
+                    name="shippingStatus"
+                    label="shippingStatus"
+                    customStyleInput="rounded-lg border-2 p-[6px]"
+                  />
+
+                  {/* State & City of Birth */}
+                  <InputForm
+                    title="Estado de nascimento"
+                    placeholder="Estado"
+                    id="stateOfBirth"
+                    name="stateOfBirth"
+                    label="stateOfBirth"
+                    customStyleInput="rounded-lg border-2 p-[6px]"
+                  />
+                  <InputForm
+                    title="Cidade de nascimento"
+                    placeholder="Cidade"
+                    id="cityOfBirth"
+                    name="cityOfBirth"
+                    label="cityOfBirth"
+                    customStyleInput="rounded-lg border-2 p-[6px]"
+                  />
+
+                  {/* File Upload */}
+                  <InputFile
+                    title="Anexar arquivos"
+                    placeholder="ImagemDocumentoAnexado.png 90kb"
+                    id="attachDocuments"
+                    name="attachDocuments"
+                    label="attachDocuments"
+                  />
+
+                  <div className="flex justify-center gap-5">
+                    <Link to="/register/students">
+                      <Button variant="ghostBlack" size="large" className="w-64">
+                        Cancelar
+                      </Button>
+                    </Link>
+                    <Button variant="blueButton" size="large" className="w-64">
+                      Cadastrar
+                    </Button>
+                  </div>
+                </div>
+              </Form>
+            </Formik>
+          </When>
+        </div>
+      </div>
+    </>
+  )
+}
