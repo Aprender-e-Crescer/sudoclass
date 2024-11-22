843db4712bb71dabf8b8ed2696dce4fff6bd6ed8
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-11-22 19:51:51 -0300

    feat(register/index.tsx): adding sub header e leftmenu

--- hosting/src/routes/_authenticated/register/_register.tsx
+++ hosting/src/routes/_authenticated/register/_register.tsx
@@ -1,8 +1,10 @@
+import LeftMenu from '@/components/custom/left-menu'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated/register/_register')({
   component: () => (
-    <div>
+    <div className="flex flex-1">
+      <LeftMenu type="AdminPortal" />
       <Outlet />
     </div>
   ),
