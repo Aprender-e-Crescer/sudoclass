d95f0fec4582fe14cc1e860e1f4c525efb3926f7
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-11-22 19:59:54 -0300

    fix(_register.tsx): fixing remove left menu

--- hosting/src/routes/_authenticated/register/_register.tsx
+++ hosting/src/routes/_authenticated/register/_register.tsx
@@ -1,10 +1,8 @@
-import LeftMenu from '@/components/custom/left-menu'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated/register/_register')({
   component: () => (
     <div className="flex flex-1">
-      <LeftMenu type="AdminPortal" />
       <Outlet />
     </div>
   ),
