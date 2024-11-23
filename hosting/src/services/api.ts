e65e8d9036057067b06d4ee979955a01d01db828
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2024-11-23 20:54:43 -0300

    fix(get-full-user): fixing pedagogue query

--- hosting/src/services/api.ts
+++ hosting/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { auth } from './firebase';
 
 const api = axios.create({
-    baseURL: 'https://us-central1-sudo-class-staging.cloudfunctions.net/api/',
+    baseURL: 'http://127.0.0.1:5001/sudo-class-staging/us-central1/api',
 });
 
 api.interceptors.request.use(async function (config) {
