0b113d720750a6b71258f20d7471febb0627548a
Jonatas Fiorentin <jonatasfiorentin@gmail.com>
2024-11-24 11:56:50 -0300

    feat(authenticated): adding type left menu

--- hosting/src/routes/_authenticated.tsx
+++ hosting/src/routes/_authenticated.tsx
@@ -1,6 +1,6 @@
 import LeftMenu from '@/components/custom/left-menu'
 import { useCurrentUserQuery } from '@/queries/use-current-user-query'
-import { useGetUser } from '@/queries/use-get-user'
+import { useGetUserQuery } from '@/queries/use-get-user-query'
 import { auth } from '@/services/firebase'
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
@@ -13,15 +13,21 @@ export const Route = createFileRoute('/_authenticated')({
   component: Authenticated,
 })
 
-export function Authenticated() {
-  // const currentUser = useCurrentUserQuery()
-  // const { data: User } = useGetUser(currentUser?.uid)
+type TypeMenu = 'TeacherClassroom' | 'StudentPortal' | 'AdminPortal'
 
-  // console.log(User)
+export function Authenticated() {
+  const currentUser = useCurrentUserQuery()
+  const { data: User } = useGetUserQuery(currentUser?.data?.uid)
+
+  let type: TypeMenu = 'TeacherClassroom'
+
+  User?.type == 'professor' ? (type = 'TeacherClassroom') : ''
+  User?.type == 'aluno' ? (type = 'StudentPortal') : ''
+  User?.type == 'pedagogo' ? (type = 'AdminPortal') : ''
 
   return (
     <div className="flex">
-      <LeftMenu type="StudentPortal" />
+      <LeftMenu type={type} />
       <div className="flex-1 ml-5">
         <Outlet />
       </div>
