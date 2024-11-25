3c974e0a133b7dcb509301f7a0ec4b7f9cb638fc
enzoguis <enzog0315@gmail.com>
2024-11-24 21:42:18 -0300

    refactor(warning-query-and-mutation): changing parameters

--- hosting/src/queries/use-warning-wall-query.ts
+++ hosting/src/queries/use-warning-wall-query.ts
@@ -4,11 +4,11 @@ import { useQuery } from '@tanstack/react-query'
 import { z } from 'zod'
 
 export const WARNING_WALL_QUERY = ['getWarnings']
-export function useListWarningsQuery(idSubject: string) {
+export function useListWarningsQuery(subjectId: string) {
   return useQuery({
-    queryKey: [...WARNING_WALL_QUERY, idSubject],
+    queryKey: [...WARNING_WALL_QUERY, subjectId],
     queryFn: async () => {
-      const { data } = await api.get(`/warnings/${idSubject}`)
+      const { data } = await api.get(`/warnings/${subjectId}`)
       const warnings = z.array(warningSchema).parse(data)
 
       return warnings
