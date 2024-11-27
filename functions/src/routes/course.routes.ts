a989c429cfe444d46feb7e94149c74995e4c0d6e
augustofav <augustofavetti159@gmail.com>
2024-11-26 23:48:20 -0300

    att course

--- functions/src/routes/course.routes.ts
+++ functions/src/routes/course.routes.ts
@@ -4,10 +4,11 @@ import courseController from '../controllers/course.controller';
 const router = Router();
 
 
-router.post('/course', courseController.createCurso);
-router.delete('/course/:id', courseController.deleteCourse);
-router.put('/course/:id', courseController.updateCourse);
-router.get('/course/:id?', courseController.listarCursos);
+router.post('/courso', courseController.createCurso);
+router.delete('/courso/:id', courseController.deleteCourse);
+router.put('/courso/:id', courseController.updateCourse);
+router.get('/courso/:id?', courseController.listarCursos);
+router.get('/courso/:id/cursosAlunos', courseController.AlunosnoCurso)
 
 
 
