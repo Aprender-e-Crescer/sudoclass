deb1ee556434c3e791912ab9c9824e259a594dd8
Raul <rauldondonimachado09@gmail.com>
2024-12-04 19:44:57 -0300

    refactor(usuario.routes.ts): uncommenting route

--- functions/src/routes/usuario.routes.ts
+++ functions/src/routes/usuario.routes.ts
@@ -9,5 +9,5 @@ router.get('/usuario/:id_usuario', usuarioController.getUser);
 router.delete('/usuario/:id_usuario', usuarioController.deleteUser);
 router.get('/trocasenha/usuario', usuarioController.requestPasswordList);
 router.put('/trocar-senha/usuario/:id_usuario', usuarioController.requestDenied);
-/* patch caso nao der certo */
+
 export default router;
