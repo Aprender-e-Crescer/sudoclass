5d562697f12f0a73cb6ddf95cb729914187dfb7c
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-12-05 12:56:28 -0300

    fix(alunos-backend): fixing students backend

--- functions/src/controllers/alunos.controller.ts
+++ functions/src/controllers/alunos.controller.ts
@@ -1,155 +1,267 @@
-import { Request, Response } from 'express';
-import { alunoService } from '../services/alunos.service';
+import { Request, Response } from "express";
+import { alunoService } from "../services/alunos.service";
 
 const alunosController = {
-    createAlunos: async (req: Request, res: Response): Promise<void> => {
-        const {
-            nomeCompleto,
-            email,
-            estadodeexpedicaorg,
-            estado,
-            municipio,
-            rua,
-            bairro, 
-            numero,
-            dataDeNascimento, 
-            cpf,
-            rg,
-            dataExpedicaoRg, 
-            estadoDeNascimento,
-            cidadeDeNascimeto} = req.body;
-            console.log(req.body);
-            
-        
-        try {
-            const retorno = await alunoService.createAluno(
-                nomeCompleto,
-                email,
-                estadodeexpedicaorg,
-                estado,
-                municipio,
-                rua,
-                bairro, 
-                numero,
-                dataDeNascimento, 
-                cpf,
-                rg,
-                dataExpedicaoRg, 
-                estadoDeNascimento,
-                cidadeDeNascimeto);
-            if (!retorno) {
-                res.status(500).send('Não foi possível cadastrar o aluno.');
-            } else {
-                res.status(200).send(retorno);
-            }
-        } catch (error) {
-            console.error('Erro ao cadastrar aluno:', error);
-            res.status(500).send('Ocorreu um erro no servidor ao tentar cadastrar o aluno.');
-        }
-    },
 
-    updateAlunos: async (req: Request, res: Response): Promise<void> => {
-        const {nomeCompleto,
-            email,
-            telefone,
-            estado,
-            municipio,
-            rua,
-            bairro, 
-            numero,
-            dataDeNascimento, 
-            cpf,
-            rg,
-            dataExpedicaoRg, 
-            estadoDeNascimento,
-            cidadeDeNascimeto} = req.body;
-        const id = req.params.id; 
+      getnoteStudent: async (req: Request, res: Response): Promise<void> => {
+        const {id_turma} = req.params;
         try {
-            const ret = await alunoService.updateAluno(
-                id,
-                nomeCompleto,
-                email,
-                telefone,
-                estado,
-                municipio,
-                rua,
-                bairro, 
-                numero,
-                dataDeNascimento, 
-                cpf,
-                rg,
-                dataExpedicaoRg, 
-                estadoDeNascimento,
-                cidadeDeNascimeto);
-            if (!ret) {
-                res.status(500).send('Não foi possível atualizar o aluno.');
-            } else {
-                res.status(200).send('Atualização realizada com sucesso');
-            }
-        } catch (error) {
-            console.error('Erro ao atualizar aluno:', error);
-            res.status(500).send('Ocorreu um erro no servidor ao tentar atualizar o aluno.');
-        }
-    },
-
-    deleteStudent: async (req: Request,  res: Response): Promise<void> => {
-        const id = req.params.id; 
-        try {
-            const ret = await alunoService.deleteStudent(id);
-            if (!ret) {
-                res.status(500).send('Não foi possível deletar o aluno.');
-            } else {
-                res.status(200).send('Aluno delatado com sucesso');
-            }
-        } catch (error) {
-            console.error('Erro ao deltar o aluno:', error);
-            res.status(500).send('Ocorreu um erro no servidor ao tentar deletar o aluno.');
-        }
-    },
-    
-        getStudent: async (req: Request,  res: Response): Promise<void> => {
-        const id = req.params.id;
-        try {
-            const ret = await alunoService.getStudent(id);
-            if (!ret) {
-                res.status(500).send('Não foi possível deletar o aluno.');
-            } else {
-                res.status(200).send(ret);
-            }
-        } catch (error) {
-            console.error('Erro ao deltar o aluno:', error);
-            res.status(500).send('Ocorreu um erro no servidor ao tentar deletar o aluno.');
-        }
-    },
-    getAllStudent: async (req: Request, res: Response): Promise<void> => {
-        try {
-            const ret = await alunoService.getAllStudents();
-            if (!ret) {
-                res.status(500).send('Não foi possível listar os alunos.')
-            } else {
-                res.status(200).send(ret);
-            }
-        } catch (error) {
-            console.error('Erro ao listar alunos:', error);
-            res.status(500).send('Ocorreu um erro no servidor ao tentar listar os alunos.');
-        }
-    },
-    getDocStudent: async (req: Request, res: Response): Promise<void> => {
-        const id = req.params.id;
-        try {
-          const ret = await alunoService.getDocStudent(id);
+          const ret = await alunoService.noteStudent(id_turma);
           if (!ret) {
-            res.status(500).send("Não foi possível buscar os documentos.");
+            res.status(500).send("Não foi possível buscar os alunos.");
           } else {
             res.status(200).send(ret);
           }
         } catch (error) {
-          console.error("Erro ao buscar documentos:", error);
+          console.error("Erro ao buscar alunos:", error);
           res
             .status(500)
             .send("Ocorreu um erro no servidor ao tentar buscar os alunos.");
         }
       },
+
+      getpresenceStudent: async (req: Request, res: Response): Promise<void> => {
+        const {id_turma} = req.params;
+        try {
+          const ret = await alunoService.presenceStudent(id_turma);
+          if (!ret) {
+            res.status(500).send("Não foi possível buscar os alunos.");
+          } else {
+            res.status(200).send(ret);
+          }
+        } catch (error) {
+          console.error("Erro ao buscar alunos:", error);
+          res
+            .status(500)
+            .send("Ocorreu um erro no servidor ao tentar buscar os alunos.");
+        }
+      },
+
+      getpresenceMateria: async (req: Request, res: Response): Promise<void> => {
+        const {id_turma} = req.params;
+        try {
+          const ret = await alunoService.presenceMateria(id_turma);
+          if (!ret) {
+            res.status(500).send("Não foi possível buscar os alunos.");
+          } else {
+            res.status(200).send(ret);
+          }
+        } catch (error) {
+          console.error("Erro ao buscar alunos:", error);
+          res
+            .status(500)
+            .send("Ocorreu um erro no servidor ao tentar buscar os alunos.");
+        }
+      },
+
+      getpresenceTurma: async (req: Request, res: Response): Promise<void> => {
+        const {id_turma} = req.params;
+        try {
+          const ret = await alunoService.presenceMateria(id_turma);
+          if (!ret) {
+            res.status(500).send("Não foi possível buscar os alunos.");
+          } else {
+            res.status(200).send(ret);
+          }
+        } catch (error) {
+          console.error("Erro ao buscar alunos:", error);
+          res
+            .status(500)
+            .send("Ocorreu um erro no servidor ao tentar buscar os alunos.");
+        }
+      },
+      createstudent: async (req: Request, res: Response): Promise<any> => {
+        const {
+          id_aluno,
+          nome,
+          email,
+          data_nasc,
+          estado,
+          municipio,
+          rua,
+          bairro,
+          numero,
+          cpf,
+          rg,
+          datadeexpedicaorg,
+          estadodeexpedicaorg,
+          estadonascimento,
+          cidadenascimento,
+    
+          id_responsavel,
+          tipo_responsavel,
+          nome_responsavel,
+          email_responsavel,
+          telefone_responsavel,
+          estado_responsavel,
+          municipio_responsavel,
+          rua_responsavel,
+          bairro_responsavel,
+          numero_responsavel,
+          dataNascimento_responsavel,
+          cpf_responsavel,
+          rg_responsavel,
+          documentos_responsavel,
+        } = req.body;
+    
+        try {
+          const retorno = await alunoService.createStudent(
+            id_aluno,
+            nome,
+            email,
+            data_nasc,
+            estado,
+            municipio,
+            rua,
+            bairro,
+            numero,
+            cpf,
+            rg,
+            datadeexpedicaorg,
+            estadodeexpedicaorg,
+            estadonascimento,
+            cidadenascimento,
+        id_responsavel,
+        tipo_responsavel,
+        nome_responsavel,
+        email_responsavel,
+        telefone_responsavel,
+        estado_responsavel,
+        municipio_responsavel,
+        rua_responsavel,
+        bairro_responsavel,
+        numero_responsavel,
+        dataNascimento_responsavel,
+        cpf_responsavel,
+        rg_responsavel,
+        documentos_responsavel
+      );
+      if (!retorno) {
+        res.status(500).send("Não foi possível cadastrar o aluno.");
+      } else {
+        res.status(200).send(retorno);
+      }
+    } catch (error) {
+      console.error("Erro ao cadastrar aluno:", error);
+      res
+        .status(500)
+        .send("Ocorreu um erro no servidor ao tentar cadastrar o aluno.");
+    }
+  },
+
+  updateStudent: async (req: Request, res: Response): Promise<void> => {
+    const {
+      nome,
+      email,
+      data_nasc,
+      estado,
+      municipio,
+      rua,
+      bairro,
+      numero,
+      cpf,
+      rg,
+      datadeexpedicaorg,
+      estadodeexpedicaorg,
+      estadonascimento,
+      cidadenascimento,
+    } = req.body;
+    const { id } = req.params;
+    try {
+      const ret = await alunoService.updateStudent(
+        id,
+        nome,
+        email,
+        data_nasc,
+        estado,
+        municipio,
+        rua,
+        bairro,
+        numero,
+        rg,
+        datadeexpedicaorg,
+        estadodeexpedicaorg,
+        estadonascimento,
+        cidadenascimento,
+        cpf
+      );
+      if (!ret) {
+        res.status(500).send("Não foi possível atualizar o aluno.");
+      } else {
+        res.status(200).send(ret);
+      }
+    } catch (error) {
+      console.error("Erro ao atualizar aluno:", error);
+      res
+        .status(500)
+        .send("Ocorreu um erro no servidor ao tentar atualizar o aluno.");
+    }
+  },
+
+  deleteStudent: async (req: Request, res: Response): Promise<void> => {
+    const id = req.params.id;
+    try {
+      const ret = await alunoService.deleteStudent(id);
+      if (!ret) {
+        res.status(500).send("Não foi possível deletar o aluno.");
+      } else {
+        res.status(200).send("Aluno delatado com sucesso");
+      }
+    } catch (error) {
+      console.error("Erro ao deltar o aluno:", error);
+      res
+        .status(500)
+        .send("Ocorreu um erro no servidor ao tentar deletar o aluno.");
+    }
+  },
+
+  getStudent: async (req: Request, res: Response): Promise<void> => {
+    const id = req.params.id;
+    try {
+      const ret = await alunoService.getStudent(id);
+      if (!ret) {
+        res.status(500).send("Não foi possível deletar o aluno.");
+      } else {
+        res.status(200).send(ret);
+      }
+    } catch (error) {
+      console.error("Erro ao deltar o aluno:", error);
+      res
+        .status(500)
+        .send("Ocorreu um erro no servidor ao tentar deletar o aluno.");
+    }
+  },
+  getAllStudent: async (req: Request, res: Response): Promise<void> => {
+    try {
+      const ret = await alunoService.getAllStudent();
+      if (!ret) {
+        res.status(500).send("Não foi possível buscar os alunos.");
+      } else {
+        res.status(200).send(ret);
+      }
+    } catch (error) {
+      console.error("Erro ao buscar alunos:", error);
+      res
+        .status(500)
+        .send("Ocorreu um erro no servidor ao tentar buscar os alunos.");
+    }
+  },
+  getDocStudent: async (req: Request, res: Response): Promise<void> => {
+    const id = req.params.id;
+    try {
+      const ret = await alunoService.getDocStudent(id);
+      if (!ret) {
+        res.status(500).send("Não foi possível buscar os documentos.");
+      } else {
+        res.status(200).send(ret);
+      }
+    } catch (error) {
+      console.error("Erro ao buscar documentos:", error);
+      res
+        .status(500)
+        .send("Ocorreu um erro no servidor ao tentar buscar os alunos.");
+    }
+  },
 };
 
-export default alunosController; 
+export default alunosController;
