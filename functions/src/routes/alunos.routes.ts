2bad8d5d92a8ccd7f38d5413551e265a956319cc
Augusto favetti <122417437+augustofav@users.noreply.github.com>
2024-12-06 10:30:27 -0300

    Update alunos.routes.ts

--- functions/src/routes/alunos.routes.ts
+++ functions/src/routes/alunos.routes.ts
@@ -5,10 +5,15 @@ const router = Router();
 
 router.post("/alunos", alunosController.createAlunos);
 
+router.put("/alunos/:id", alunosController.updateStudent);
 router.get('/alunos/:id', alunosController.getStudent)
 router.delete('/alunos/:id', alunosController.deleteStudent)
 router.get('/alunos', alunosController.getAllStudent)
 router.get('/alunos/documents/:id', alunosController.getDocStudent)
+router.get("/alunos/nota/:id_turma", alunosController.getnoteStudent);
+router.get("/alunos/presenca/:id_turma", alunosController.getpresenceStudent);
+router.get("/alunos/presenca-turma/:id_turma", alunosController.getpresenceTurma);
+router.get("/alunos/presenca-materia/:id_turma", alunosController.getpresenceMateria);
 
 
 
