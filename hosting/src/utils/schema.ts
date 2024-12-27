6ae6f256e4af7e11123283ab75967da9b0ba93aa
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2024-12-27 13:41:07 -0300

    refactor(utils): fixing schema date parser

--- hosting/src/utils/schema.ts
+++ hosting/src/utils/schema.ts
@@ -1,3 +1,9 @@
 import { z } from "zod";
 
-export const datePreprocessedSchema = z.preprocess(data => new Date(data), z.date())
+export const datePreprocessedSchema = z.preprocess(data => {
+    if (data instanceof Date) return data;
+    
+    if (typeof data !== "string" && typeof data !== "number") throw new Error("Invalid date format");
+
+    return new Date(data);
+}, z.date())
