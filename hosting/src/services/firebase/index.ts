52678dd0bc631122654521ccd6cafc75c0aa60ba
Alucsky <samuelmolinari8@gmail.com>
2025-01-27 14:30:33 -0300

    fix(index.ts): changing firebase sdk

--- hosting/src/services/firebase/index.ts
+++ hosting/src/services/firebase/index.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, setPersistence, indexedDBLocalPersistence } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
@@ -18,6 +19,7 @@ export const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
 export const storage = getStorage(app);
 export const functions = getFunctions(app);
+const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
 
