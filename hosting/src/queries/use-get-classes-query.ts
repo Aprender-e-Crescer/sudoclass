57e367d3f392a8ce377140ea79c461a1c8a4b0c3
enzoguis <enzog0315@gmail.com>
2025-01-28 20:25:28 -0300

    feat(get class query prototype): initing query and screen

---
+++ hosting/src/queries/use-get-classes-query.ts
@@ -0,0 +1,55 @@
+import { Student } from '@/models/student-schema'
+import { Teacher } from '@/models/teacher-schema'
+import { firestore } from '@/services/firebase'
+import { role } from '@/types/user'
+import { queryOptions, useQuery } from '@tanstack/react-query'
+import { collection, query, where, getDocs, documentId } from 'firebase/firestore'
+import { Class, classSchema } from '@/models/class-schema'
+
+export const getClassesQueryOptions = (
+  courseId: string,
+  role: role | undefined,
+  studentClasses: Student['classes'] | undefined,
+  teacherSubjects: Teacher['subjects'] | undefined,
+) =>
+  queryOptions({
+    queryKey: ['getClasses', courseId, role, studentClasses, teacherSubjects],
+    queryFn: async () => {
+      if (!role) throw new Error('Role is not defined')
+
+      let classRefs
+
+      if (role === 'teacher') {
+        if (!teacherSubjects) throw new Error('Subjects are not defined')
+        classRefs = teacherSubjects.map((subjectRef) => subjectRef.parent.parent)
+      }
+
+      if (role === 'student' || role === 'responsible') {
+        if (!studentClasses) throw new Error('Classes are not defined')
+        classRefs = studentClasses.map((classRef) => classRef)
+      }
+
+      const classCollectionRef = collection(firestore, `courses/${courseId}/classes`).withConverter({
+        toFirestore: (classItem: Class) => classItem,
+        fromFirestore: (snapshot, options) => {
+          const data = snapshot.data(options)
+          return classSchema.parse({ ...data, id: snapshot.id })
+        },
+      })
+
+      const conditions = role === 'admin' ? [] : [where(documentId(), 'in', classRefs)]
+
+      const queryRef = query(classCollectionRef, ...conditions)
+      const querySnapshot = await getDocs(queryRef)
+
+      return querySnapshot.docs.map((doc) => doc.data())
+    },
+  })
+export function useGetClassesQuery(
+  courseId: string,
+  role: role | undefined,
+  studentClasses: Student['classes'] | undefined,
+  teacherSubjects: Teacher['subjects'] | undefined,
+) {
+  return useQuery(getClassesQueryOptions(courseId, role, studentClasses, teacherSubjects))
+}
---
+++ hosting/src/routes/_authenticated/courses/$idCourse/classes/index.tsx
@@ -0,0 +1,22 @@
+import { CardComponent } from '@/components/custom/card-bolletin-board'
+import { useGetFullUser } from '@/hooks/use-get-full-user'
+import { useGetClassesQuery } from '@/queries/use-get-classes-query'
+import { useStudentPersonalClasses } from '@/queries/use-student-personal-classes-query'
+import { useTeacherPersonalSubjects } from '@/queries/use-teacher-personal-subjects-query'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/_authenticated/courses/$idCourse/classes/')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  const fullUser = useGetFullUser()
+  const idCourse = Route.useParams()
+  const { data: student } = useStudentPersonalClasses(fullUser?.role, fullUser?.roleRef)
+  const { data: teacher } = useTeacherPersonalSubjects(fullUser?.role, fullUser?.roleRef)
+  const { data: classes } = useGetClassesQuery(String(idCourse), fullUser?.role, student?.classes, teacher?.subjects)
+  console.log(classes)
+  console.log(student)
+
+  return <div></div>
+}
