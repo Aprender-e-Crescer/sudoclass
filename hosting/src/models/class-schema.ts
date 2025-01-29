65c3e3144fdd0a9ba4960aad45e01fe48e13a835
Alucsky <samuelmolinari8@gmail.com>
2025-01-29 12:51:17 -0300

    refactor(class-schema): merging with feature/class-list

--- hosting/src/models/class-schema.ts
+++ hosting/src/models/class-schema.ts
@@ -1,12 +1,14 @@
 import { docRefSchema } from '@/utils/schema'
 import { z } from 'zod'
+import { Timestamp } from 'firebase/firestore'
 
 export const classSchema = z.object({
   name: z.string(),
+  color: z.string(),
   shift: z.enum(['morning', 'afternoon', 'night']),
-  startDate: z.date(),
-  endDate: z.date(),
-  subscriptionEndDate: z.date(),
+  startDate: z.instanceof(Timestamp).transform((ts) => ts.toDate()),
+  endDate: z.instanceof(Timestamp).transform((ts) => ts.toDate()),
+  subscriptionEndDate: z.instanceof(Timestamp).transform((ts) => ts.toDate()),
   workload: z.number(),
   availableVacancies: z.number(),
   students: z.array(docRefSchema),
