775d88404c1696557d30e4288c4d53643b283193
Alucsky <samuelmolinari8@gmail.com>
2025-01-30 13:50:13 -0300

    feat(card-bolletin): Adding a course name in props

--- hosting/src/components/custom/card-bolletin-board.tsx
+++ hosting/src/components/custom/card-bolletin-board.tsx
@@ -1,17 +1,17 @@
 interface CardComponentProps {
   name: string
-  description: string
+  courseName: string
   color: string
 }
 
-export function CardComponent({ name, description, color }: CardComponentProps) {
+export function CardComponent({ name, courseName, color }: CardComponentProps) {
   return (
     <div
       className="text-white w-full max-w-[1068px] h-[258px] rounded-lg p-8 flex flex-col justify-end"
       style={{ backgroundColor: color }}
     >
       <h1 className="text-3xl sm:text-4xl font-bold">{name}</h1>
-      <p className="text-base sm:text-lg text-gray-300">{description}</p>
+      <p className="text-base sm:text-lg text-gray-300">{courseName}</p>
     </div>
   )
 }
