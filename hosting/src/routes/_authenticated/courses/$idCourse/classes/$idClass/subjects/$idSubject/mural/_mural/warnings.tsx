da900cbb5e1609a8dc16b37e20eeb1954986e81e
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2025-01-30 23:12:20 -0300

    refactor(warnings): adding controller

--- hosting/src/routes/_authenticated/courses/$idCourse/classes/$idClass/subjects/$idSubject/mural/_mural/warnings.tsx
+++ hosting/src/routes/_authenticated/courses/$idCourse/classes/$idClass/subjects/$idSubject/mural/_mural/warnings.tsx
@@ -1,13 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
 import { CardComponent } from '@/components/custom/card-bolletin-board'
 import { CustomLoading } from '@/components/custom/custom-loading'
-import { useGetWarningsQuery } from '@/queries/use-get-warnings-query'
 import { Warning } from '@/components/custom/warning'
-import { useGetSubjectByIdQuery } from '@/queries/use-get-subject-by-id-query'
-import { useGetCourseById } from '@/queries/use-get-course-by-id'
-import { useSentByProfilesQueries } from '@/queries/use-sent-by-profiles-queries'
+import { useWarningController } from '@/controllers/use-warnings-controller'
+import { createFileRoute } from '@tanstack/react-router'
+import avatarPlaceholder from '@/assets/user.png'
 import { SendHorizonal } from 'lucide-react'
-import { useGetFullUser } from '@/hooks/use-get-full-user'
+import { When } from 'react-if'
 
 export const Route = createFileRoute(
   '/_authenticated/courses/$idCourse/classes/$idClass/subjects/$idSubject/mural/_mural/warnings',
@@ -17,36 +15,9 @@ export const Route = createFileRoute(
 
 export function WallSubjects() {
   const { idCourse, idClass, idSubject } = Route.useParams()
-  const { data: warnings, isLoading } = useGetWarningsQuery(idCourse, idClass, idSubject)
-  const sentByProfiles = useSentByProfilesQueries(warnings)
-  const { data: subject } = useGetSubjectByIdQuery(idCourse, idClass, idSubject)
-  const { data: course } = useGetCourseById(idCourse)
-  const fullUser = useGetFullUser()
-
-  const warningsWithSentByProfiles = warnings?.map(({ id, message, sentDate, sentByProfile }) => {
-    const sentByProfileData = sentByProfiles.find(({ data: currentSentByProfile }) => {
-      if (!currentSentByProfile) return false
-
-      if (currentSentByProfile.id === sentByProfile.id) return true
-
-      return false
-    })
-
-    const author = sentByProfileData?.data
-      ? {
-          name: sentByProfileData.data.displayName,
-          profilePhotoSrc: sentByProfileData.data.photoUrl,
-        }
-      : undefined
-
-    return {
-      key: id,
-      id: id,
-      date: sentDate,
-      message: message,
-      author: author,
-    }
-  })
+  const { isLoading, subject, course, fullUser, warningsWithSentByProfiles } = useWarningController({ idCourse, idClass, idSubject })
+  
+  const hasPermissionToSendWarning = fullUser?.role === 'teacher' || fullUser?.role === 'admin'
 
   if (isLoading) {
     return (
@@ -58,17 +29,19 @@ export function WallSubjects() {
     )
   }
 
+  if (!subject || !course || !fullUser) throw new Error('Unexpected error')
+
   return (
     <div className="bg-white w-full min-h-screen flex flex-col items-center justify-start">
       <div className="w-full max-w-screen-lg p-4 sm:p-6 flex flex-col gap-5">
         <div className="my-2 mx-auto w-full sm:max-w-md lg:max-w-full">
-          <CardComponent name={subject?.name} courseName={course?.name} color={subject?.color} />
+          <CardComponent name={subject.name} courseName={course.name} color={subject.color} />
         </div>
-
-        {fullUser?.role === 'teacher' || fullUser?.role === 'admin' ? (
+        
+        <When condition={hasPermissionToSendWarning}>
           <div>
             <div className="flex items-center border-2 p-7 rounded-lg shadow-xl">
-              <img src={fullUser?.photoURL} alt="Icon" className="w-12 h-12 mr-2 rounded-full " />
+              <img src={fullUser.photoURL ?? avatarPlaceholder} alt="Icon" className="w-12 h-12 mr-2 rounded-full " />
               <input
                 type="text"
                 placeholder="Escreva um aviso para sua turma"
@@ -79,7 +52,7 @@ export function WallSubjects() {
               </button>
             </div>
           </div>
-        ) : null}
+        </When>
 
         <div className="flex flex-col p-5 gap-5">
           {warningsWithSentByProfiles?.map(({ author, date, id, key, message }) => (
