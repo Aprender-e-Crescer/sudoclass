280b17dba675a97166298160fe84f91cae899c74
enzoguis <enzog0315@gmail.com>
2025-02-03 03:36:35 -0300

    refactor(missings-query): refactoring the query

--- hosting/src/queries/use-get-missings-query.ts
+++ hosting/src/queries/use-get-missings-query.ts
@@ -1,51 +1,30 @@
 import { Missing, missingSchema } from '@/models/missing-schema'
 import { firestore } from '@/services/firebase'
-import { useQuery } from '@tanstack/react-query'
-import { collection, getDocs, getDoc } from 'firebase/firestore'
+import { queryOptions } from '@tanstack/react-query'
+import { collection, getDocs } from 'firebase/firestore'
 
 export const MISSINGS_QUERY_KEY = ['get-missings']
 
-export function useGetMissingsQuery(courseId: string, classId: string, subjectId: string, lessonPlanId: string) {
-  return useQuery({
-    queryKey: [...MISSINGS_QUERY_KEY, courseId, classId, subjectId, lessonPlanId],
-    queryFn: async () => {
-      const missingRef = collection(
-        firestore,
-        'courses',
-        courseId,
-        'classes',
-        classId,
-        'subjects',
-        subjectId,
-        'lessonPlannings',
-        lessonPlanId,
-        'missings',
-      ).withConverter({
-        toFirestore: (missing: Missing) => missing,
-        fromFirestore: (snapshot, options) => {
-          const data = snapshot.data(options)
-          return missingSchema.parse({ ...data, id: snapshot.id })
-        },
-      })
-
-      const missingSnapshot = await getDocs(missingRef)
-
-      const missings = await Promise.all(
-        missingSnapshot.docs.map(async (doc) => {
-          const missingData = doc.data()
-          const studentRef = missingData.studentProfile
-
-          let studentProfile = null
-          if (studentRef) {
-            const studentDoc = await getDoc(studentRef)
-            studentProfile = studentDoc.exists() ? { id: studentDoc.id, ...(studentDoc.data() ?? {}) } : null
-          }
-
-          return { id: doc.id, ...missingData, studentProfile }
-        }),
-      )
-
-      return missings
-    },
+export const getMissinsFirestoreQuery = (courseId: string, classId: string, subjectId: string, lessonPlanId: string) =>
+  collection(
+    firestore,
+    'courses',
+    courseId,
+    'classes',
+    classId,
+    'subjects',
+    subjectId,
+    'lessonPlannings',
+    lessonPlanId,
+    'missings',
+  ).withConverter({
+    toFirestore: (missing: Missing) => missing,
+    fromFirestore: (snapshot) => missingSchema.parse({ id: snapshot.id, ...snapshot.data() }),
   })
-}
+
+export const getMissingsQueryOptions = (courseId: string, classId: string, subjectId: string, lessonPlanId: string) =>
+  queryOptions({
+    queryKey: ['get-missings'],
+    queryFn: () => getDocs(getMissinsFirestoreQuery(courseId, classId, subjectId, lessonPlanId)),
+    select: (snapshot) => snapshot.docs.map((doc) => missingSchema.parse({ id: doc.id, ...doc.data() })),
+  })
