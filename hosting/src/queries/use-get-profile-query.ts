0bd037ce1872262a5737966871e6adb3c7fe8fce
enzoguis <enzog0315@gmail.com>
2025-02-03 21:37:13 -0300

    refactor(get-profile-query): adding return from profileRef

--- hosting/src/queries/use-get-profile-query.ts
+++ hosting/src/queries/use-get-profile-query.ts
@@ -1,32 +1,16 @@
 import { Profile, profileSchema } from '@/models/profile-schema'
-import { queryOptions, useQuery } from '@tanstack/react-query'
+import { queryOptions } from '@tanstack/react-query'
 import { DocumentData, DocumentReference, getDoc } from 'firebase/firestore'
+
 export const getProfileFirestoreQuery = (profileRef: DocumentReference<DocumentData, DocumentData>) =>
   profileRef.withConverter({
-    fromFirestore: (snapshot) => profileSchema.parse({ id: snapshot.id, ...snapshot.data() }),
+    fromFirestore: (snapshot) => profileSchema.parse({ id: snapshot.id, profileRef, ...snapshot.data() }),
     toFirestore: (profile: Profile) => profile,
   })
 
 export const getProfileQueryOptions = (profileRef: DocumentReference<DocumentData, DocumentData>) =>
   queryOptions({
     queryKey: ['get-profile', profileRef],
-    queryFn: async () => {
-      if (!profileRef) throw new Error('Referência de perfil não encontrada')
-
-      const profileSnapshot = await getDoc(
-        profileRef.withConverter({
-          fromFirestore: (snapshot) => profileSchema.parse({ id: snapshot.id, profileRef, ...snapshot.data() }),
-          toFirestore: (profile: Profile) => profile,
-        }),
-      )
-
-      if (!profileSnapshot.exists()) throw new Error('Perfil não encontrado')
-
-      return profileSnapshot.data()
-    },
-    enabled: !!profileRef,
+    queryFn: () => getDoc(getProfileFirestoreQuery(profileRef)),
+    select: (snapshot) => snapshot.data(),
   })
-
-export function useGetProfileQuery(profileRef: DocumentReference<DocumentData, DocumentData>) {
-  return useQuery(getProfileQueryOptions(profileRef))
-}
