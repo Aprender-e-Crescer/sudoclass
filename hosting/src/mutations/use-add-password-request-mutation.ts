ad401130cca1dee8a5600f349bcf36427c94767e
enzoguis <enzog0315@gmail.com>
2025-02-12 21:29:46 -0300

    feat(password-reset): adding controller

--- hosting/src/mutations/use-add-password-request-mutation.ts
+++ hosting/src/mutations/use-add-password-request-mutation.ts
@@ -2,15 +2,21 @@ import { firestore } from '@/services/firebase'
 import { useMutation } from '@tanstack/react-query'
 import { addDoc, collection, DocumentData, DocumentReference } from 'firebase/firestore'
 
+interface AddPasswordRequestInput {
+  onError: (error: Error) => void
+  onSuccess: () => void
+}
 interface AddPasswordRequestData {
   profileRef: DocumentReference<DocumentData, DocumentData>
 }
-export function useAddPasswordRequestMutation() {
+export function useAddPasswordRequestMutation({ onError, onSuccess }: AddPasswordRequestInput) {
   return useMutation({
     mutationKey: ['addPasswordRequest'],
     mutationFn: ({ profileRef }: AddPasswordRequestData) => {
       const passwordRequestRef = collection(firestore, 'requestsChangePassword')
-      return addDoc(passwordRequestRef, profileRef)
+      return addDoc(passwordRequestRef, { profileRef: profileRef })
     },
+    onError,
+    onSuccess,
   })
 }
