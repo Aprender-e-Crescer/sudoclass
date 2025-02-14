3d3a0446cf1d4136d415b705e15fb5104ac83bfa
Alucsky <samuelmolinari8@gmail.com>
2025-02-14 16:02:27 -0300

    feat(body): add a props buttonsNextTo to changing style of buttons

--- hosting/src/components/custom/form/body.tsx
+++ hosting/src/components/custom/form/body.tsx
@@ -6,9 +6,10 @@ import { PropsWithChildren } from 'react'
 
 interface Props {
     cancelTo: string
+    buttonsNextTo?: boolean
 }
 
-export function FormBody({ cancelTo, children }: PropsWithChildren<Props>) {
+export function FormBody({ cancelTo, buttonsNextTo, children }: PropsWithChildren<Props>) {
     const { isSubmitting } = useFormikContext()
 
     return (
@@ -16,13 +17,12 @@ export function FormBody({ cancelTo, children }: PropsWithChildren<Props>) {
             <div className="flex flex-col border border-gray-300 p-4 rounded-lg gap-2">
                 {children}
             </div>
-            <div className="flex flex-1 gap-x-3 justify-center">
-                <Link to={cancelTo}>
-                    <Button variant="outline" className="px-[72px] sm:px-[108px]" type="button">Cancelar</Button>
+            <div className={`flex ${buttonsNextTo ? 'flex-col gap-2' : 'flex-row'} flex-1 gap-x-3 justify-center`}>
+                <Link to={cancelTo} className={buttonsNextTo ? 'w-full' : ''}>
+                    <Button variant="outline" className={`${buttonsNextTo ? 'w-full' : 'px-[72px] sm:px-[108px]'}`} type="button">Cancelar</Button>
                 </Link>
-                <button className="pl-4 pr-10 sm:pl-12 sm:pr-20 flex gap-1 items-center rounded bg-blue-500 hover:bg-blue-600 active:bg-blue-700 disabled:bg-gray-400 text-sm font-bold text-white" type='submit' disabled={isSubmitting}>
-                    <Loader2 className='invisible data-[is-submitting=true]:visible w-6 h-6 animate-spin text-gray-200' data-is-submitting={isSubmitting} />
-                    Cadastrar
+                <button className={`${buttonsNextTo ? 'w-full p-1' : 'w-auto px-[72px] sm:px-[108px]'} flex justify-center items-center rounded bg-blue-500 hover:bg-blue-600 active:bg-blue-700 disabled:bg-gray-400 text-sm font-bold text-white`} type='submit' disabled={isSubmitting}>
+                    {isSubmitting ? <Loader2 className='w-6 h-6 animate-spin text-gray-200' /> : "Cadastrar"}
                 </button>
             </div>
         </Form>
