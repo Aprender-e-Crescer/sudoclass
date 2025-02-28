e525fb6d91f9b39beb1f1269c76d5eef5dd78e18
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2025-02-28 08:11:01 -0300

    fix(schema): fixing types

--- functions/src/utils/schema.ts
+++ functions/src/utils/schema.ts
@@ -1,7 +1,9 @@
 const getFirestoreLib = () => {
   try {
+    // @ts-expect-error
     return typeof process !== undefined ? require("firebase-admin/firestore") : import("firebase/firestore");
   } catch (error) {
+    // @ts-expect-error
     return import("firebase/firestore")
   }
 }
@@ -11,12 +13,14 @@ import { z } from "zod";
 import { isValidCPF } from "./isValidCPF";
 
 export const docRefSchema = z.any().refine(
+    // @ts-expect-error
     (documentReference: object): documentReference is DocumentReference => documentReference instanceof DocumentReference,
   )
 
 export const datePreprocessedSchema = z.preprocess(data => {
   if (!(data instanceof Timestamp)) throw new Error("Invalid date format");
 
+  // @ts-expect-error
   return data.toDate();
 }, z.date())
 
