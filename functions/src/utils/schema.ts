ac54ad5907e836ce8b7f915883f81137fe49ccbc
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2025-02-28 07:38:26 -0300

    fix(schema): fixing importing on web

--- functions/src/utils/schema.ts
+++ functions/src/utils/schema.ts
@@ -1,15 +1,23 @@
-import { DocumentReference, Timestamp } from "firebase-admin/firestore";
+const getFirestoreLib = () => {
+  try {
+    return typeof process !== undefined ? require("firebase-admin/firestore") : import("firebase/firestore");
+  } catch (error) {
+    return import("firebase/firestore")
+  }
+}
+
+const { DocumentReference, Timestamp } = getFirestoreLib();
 import { z } from "zod";
 import { isValidCPF } from "./isValidCPF";
+
+export const docRefSchema = z.any().refine(
+    (documentReference: object): documentReference is DocumentReference => documentReference instanceof DocumentReference,
+  )
 
 export const datePreprocessedSchema = z.preprocess(data => {
   if (!(data instanceof Timestamp)) throw new Error("Invalid date format");
 
   return data.toDate();
 }, z.date())
-
-export const docRefSchema = z.any().refine(
-    (documentReference: object): documentReference is DocumentReference => documentReference instanceof DocumentReference,
-  )
 
 export const cpfSchema = z.string().refine(isValidCPF, "Inválido")
