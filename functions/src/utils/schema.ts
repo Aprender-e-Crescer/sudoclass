1987ce637aa97c8e4eee695bb6a1130e95fc3cf1
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2025-03-05 23:03:29 -0300

    fix(functions-schema): fixing date schema

--- functions/src/utils/schema.ts
+++ functions/src/utils/schema.ts
@@ -9,6 +9,7 @@ const getFirestoreLib = () => {
 const { DocumentReference, Timestamp } = getFirestoreLib();
 import { z } from "zod";
 import { isValidCPF } from "./isValidCPF";
+import { isDate } from 'date-fns'
 
 export const docRefSchema = z.any().refine(
     // @ts-expect-error
@@ -16,10 +17,17 @@ export const docRefSchema = z.any().refine(
   )
 
 export const datePreprocessedSchema = z.preprocess(data => {
-  if (!(data instanceof Timestamp)) throw new Error("Invalid date format");
+  if (data instanceof Date) return data;
+
+  if (typeof data === 'string' && isDate(new Date(data))) {
+    const parsedDate = new Date(data);
+    return parsedDate;
+  }
 
   // @ts-expect-error
-  return data.toDate();
+  if (typeof data?.toDate === 'function') return data.toDate();
+    
+  throw new Error("Invalid date format");  
 }, z.date())
 
 export const cpfSchema = z.string().refine(isValidCPF, "Inválido")
